cbe08f3cd84c42623e3c6d9450121b43a65de50c
Akhil Rajan <rajanakhil07@gmail.com>
2022-01-10 20:51:27 +0530

    Time: 24 ms (13.52%), Space: 15.9 MB (49.76%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -15,20 +15,26 @@ public:
         
         for(int i=0; i<n; i++){
             
+            // if the current nums elements matches our pme1, then increase the count of c1
             if(nums[i] == m1) {
                 c1++;
             }
+            // if the current nums elements matches our pme2, then increase the count of c2
             else if(nums[i] == m2){
                 c2++;
             }
+            // if the count of c1 is 0, then make current element as m1 and increase the count of c1 to 1
             else if(c1 == 0){
                 m1 = nums[i];
                 c1=1;
             }
+            // if the count of c2 is 0, then make current element as m2 and increase the count of c2 to 1
             else if(c2 == 0){
                 m2 = nums[i];
                 c2=1;
             }
+            // if the current elements doesnt match with any of our potential majority elements
+            // decrease both the count 
             else{
                 c1--;
                 c2--;
@@ -38,6 +44,7 @@ public:
         int freq1=0; // frequency of pme1
         int freq2=0; // frequency of pme2
         
+        // count the occurences of both the pme1 and pme2
         for(int i=0; i<n; i++){
             if(nums[i] == m1){
                 freq1++;
@@ -47,9 +54,11 @@ public:
             }
         }
         
-        vector<int> ans;
-        cout<<freq1<<" "<<freq2<<endl;
-        if(freq1 > n/3) ans.push_back(m1);
+        vector<int> ans; // vector array to store our results
+        
+        // check if frequencies of both pme1 and pme2 are greater than n/3 if yes, push them to
+        // our result array
+        if(freq1 > n/3) ans.push_back(m1); 
         if(freq2 > n/3) ans.push_back(m2);
         
         return ans;
