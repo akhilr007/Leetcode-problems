27010d1d1f8f413b2c22ba115712d5c0ba7ca404
Akhil Rajan <rajanakhil07@gmail.com>
2022-01-16 14:11:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (15.43%) - LeetHub

---
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        
+        if(name.length() > typed.length()){
+            return false;
+        }
+        
+        int i=0; // name pointer
+        int j=0; // typed pointer
+        
+        while(j<typed.length()){
+            
+            if(i<name.length() && name[i] == typed[j]){
+                i++;
+                j++;
+            }
+            
+            else if(j>0 && typed[j] == typed[j-1]){
+                j++;
+            }
+            
+            else{
+                return false;
+            }
+        }
+        
+       return i==name.length();
+        
+    }
+};
