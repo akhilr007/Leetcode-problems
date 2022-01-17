bce72bf658fdce1b10f68b6d0e8acf3d75a23629
Akhil Rajan <rajanakhil07@gmail.com>
2022-01-17 21:30:37 +0530

    Time: 12 ms (96.82%), Space: 19.6 MB (88.09%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,42 +2,31 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        // gonna use moore voting algorithm
-        // let us assume our majority element to be first element of our array
-        int potential_majority_element = nums[0];
-        // increase the count to 1
-        int count=1;
+        int ele=-1;
+        int count=0;
         
-        int n = nums.size();
-        for(int i=1; i<n; i++){
+        for(int i=0; i<nums.size(); i++){
             
-            if(nums[i] == potential_majority_element){
+            if(count==0){
+                count=1;
+                ele=nums[i];
+            }
+            else if(nums[i] == ele){
                 count++;
             }
             else{
                 count--;
             }
-            
-            // if at any stage my count becomes zero, then make the next element to be 
-            // potential majority element
-            if(count == 0){
-                potential_majority_element=nums[i];
-                count=1;
+        }
+        
+        count=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == ele){
+                count++;
             }
         }
         
-        // now check if the potential majority eleement is actually the majority element
-        // check if the count is greater than n/2 times
-        
-        int freq=0; // frequency of majority element
-        for(int i=0; i<n; i++){
-            if(nums[i] == potential_majority_element){
-                freq++;
-            }
-        }
-        
-        if(freq > n/2) return potential_majority_element;
+        if(count > nums.size()/2) return ele;
         else return -1;
-        
     }
 };
