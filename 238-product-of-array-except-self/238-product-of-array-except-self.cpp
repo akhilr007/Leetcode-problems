e23c86db0e712f8509a0209b445a208c331bbad5
Akhil Rajan <rajanakhil07@gmail.com>
2022-01-17 20:21:02 +0530

    Time: 48 ms (11.58%), Space: 25 MB (33.52%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -2,62 +2,47 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        int n = nums.size();
+        /*
+        worst case
+        vector<int> res(nums.size(), 1);
         
-        vector<int> res(n);
-        
-        int left=1;
-        
-        for(int i=0; i<n; i++){
-            
-            if(i>0){
-                left *= nums[i-1];
-            }
-            res[i] = left;
-        }
-        
-        int right=1;
-        for(int i=n-1; i>=0; i--){
-            if(i<n-1){
-                right = right * nums[i+1];
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                
+                if(i==j) continue;
+                res[i] *= nums[j];
             }
             
-            res[i] *= right;
         }
         
         return res;
+        */
         
-//         vector<int> res(n);
+        // O(n)
         
-//         // prefix of left product array
-//         vector<int> left(n);
-//         left[0] = nums[0];
+        int n=nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
         
-//         for(int i=1; i<n; i++){
-//             left[i] = left[i-1] * nums[i];
-//         }
+        left[0] = nums[0];
         
-//         //prefix of right product array
-//         vector<int> right(n);
-//         right[n-1] = nums[n-1];
+        for(int i=1; i<n; i++){
+            left[i] = left[i-1]*nums[i];
+        }
         
-//         for(int i=n-2; i>=0; i--){
-//             right[i] = right[i+1] * nums[i];
-//         }
+        right[n-1] = nums[n-1];
         
-//         for(int i=0; i<n; i++){
-//             if(i==0){
-//                 res[i] = right[i+1];
-//                 continue;
-//             }
-//             if(i==n-1){
-//                 res[i] = left[i-1];
-//                 continue;
-//             }
-            
-//             res[i] = left[i-1] * right[i+1];
-//         }
+        for(int i=n-2; i>=0; i--){
+            right[i] = right[i+1]*nums[i];
+        }
         
-//         return res;
+        vector<int> res(n);
+        
+        for(int i=0; i<n; i++){
+            int ans = (i==0 ? 1 : left[i-1]) * (i==n-1?1:right[i+1]);
+            res[i] = ans;
+        }
+        
+        return res;
     }
 };
