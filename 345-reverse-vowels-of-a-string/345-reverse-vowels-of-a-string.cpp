cc47094f29916b29b72d4f5a6fe02eeeb93a67dd
Akhil Rajan <rajanakhil07@gmail.com>
2022-01-17 22:48:05 +0530

    Time: 14 ms (17.65%), Space: 7.8 MB (63.37%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    bool isVowel(char ch){
+    bool isVowel(char s){
         
-        if(ch == 'A' || ch =='E' || ch == 'I' || ch =='O' || ch =='U' || ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u'){
+        if(s=='A'||s=='E'||s=='I'||s=='O'||s=='U' || s=='a' || s=='e' || s=='i' || s=='o' || s=='u'){
             return true;
         }
         
@@ -11,23 +11,19 @@ public:
     }
     string reverseVowels(string s) {
         
-        int i = 0;
-        int j = s.length()-1;
+        int i=0;
+        int j=s.length()-1;
         
-        //
         while(i<j){
             
-            // increment i till we find vowels from left side
-            while(i<j && isVowel(s[i]) == false){
+            while(i<j && isVowel(s[i])==false){
                 i++;
             }
             
-            // decrement j till we find vowels from right side
-            while(i<j && isVowel(s[j])== false){
+            while(i<j && isVowel(s[j]) == false){
                 j--;
             }
             
-            // if both the pointers are standing on vowels, then swap them
             swap(s[i], s[j]);
             i++;
             j--;
