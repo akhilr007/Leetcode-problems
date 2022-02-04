ac465a6168f801f9a32b8c3284d4db2c9a073470
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-04 22:14:20 +0530

    Time: 88 ms (72.41%), Space: 59.1 MB (38.86%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    int maxArea(vector<int>& arr) {
+    int maxArea(vector<int>& height) {
         
-//         int maxArea=0;
-//         for(int i=0; i<height.size(); i++){
-//             for(int j=i+1; j<height.size(); j++){
-                
-//                 int currArea = (j-i)*min(height[i], height[j]);
-                
-//                 if(currArea > maxArea){
-//                     maxArea = currArea;
-//                 }
-                
-//             }
-//         }
-        
-//         return maxArea;
-        
-        int i=0;
-        int j=arr.size()-1;
-        
-        int maxArea=0;
-        
-        while(i<j){
-            
-            // compare b/w heights of i and j
-            int minHeight = min(arr[i], arr[j]);
-            int currArea = (j-i)*minHeight;
-            
-            if(currArea > maxArea){
-                maxArea = currArea;
-            }
-            
-            // if height of arr[i] is smaller , then increment i otherwise decrement j'
-            if(arr[i]<arr[j]){
-                i++;
-            }
-            else{
-                j--;
-            }
+    int n = height.size();
+    
+    int maximumArea = INT_MIN;
+
+    int i=0;
+    int j=n-1;
+
+    while(i<j){
+
+        int width = (j-i);
+        int heights = min(height[i], height[j]);
+
+        int currArea = width*heights;
+
+        maximumArea = max(maximumArea, currArea);
+
+        if(height[i] < height[j]){
+            i++;
+        } 
+        else{
+            j--;
         }
-        
-        return maxArea;
+    }
+
+    return maximumArea;
     }
 };
