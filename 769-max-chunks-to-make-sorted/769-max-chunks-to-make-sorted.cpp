865016c83119edc338e16fc6ef0f639d87b8a42c
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-07 14:10:19 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (27.52%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         
-        int count=0;
-        int maxR =0;
+        int maxImpact = INT_MIN;
+        int countChunks=0;
         
         for(int i=0; i<arr.size(); i++){
+            // max impact of current element
+            maxImpact = max(maxImpact, arr[i]);
             
-            maxR = max(arr[i], maxR);
-            
-            if(i == maxR){
-                count++;
+            // if the idx is equal to maxImpact idx then we can make partition
+            if(maxImpact == i){
+                countChunks++;
             }
         }
         
-        return count;
+        return countChunks;
+        
     }
 };
