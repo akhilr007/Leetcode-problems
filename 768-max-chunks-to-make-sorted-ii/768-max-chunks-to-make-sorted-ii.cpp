cc08f964889c93ae8458dba1885ecab737110136
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-07 15:17:19 +0530

    Time: 10 ms (71.05%), Space: 12.7 MB (18.65%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
@@ -4,30 +4,28 @@ public:
         
         int n = arr.size();
         
-        vector<int> left_max(n);
-        left_max[0] = arr[0];
+        vector<int> lmax(n);
+        lmax[0] = arr[0];
         
         for(int i=1; i<n; i++){
-            left_max[i] = max(arr[i], left_max[i-1]);
+            lmax[i] = max(lmax[i-1], arr[i]);
         }
         
-        vector<int> right_min(n+1);
-        right_min[n] = INT_MAX;
+        vector<int> rmin(n+1);
+        rmin[n] = INT_MAX;
         
         for(int i=n-1; i>=0; i--){
-            
-            right_min[i] = min(arr[i], right_min[i+1]);
-            
+            rmin[i] = min(rmin[i+1], arr[i]);
         }
         
-        int chunks=0;
+        int countChunks=0;
         for(int i=0; i<n; i++){
-            
-            if(left_max[i] <= right_min[i+1]){
-                chunks++;
+            if(lmax[i] <= rmin[i+1]){
+                countChunks++;
             }
         }
         
-        return chunks;
+        return countChunks;
+        
     }
 };
