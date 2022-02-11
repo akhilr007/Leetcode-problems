822a71455b7f1942a0ba8d4f29c3d4188bf44fbd
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-11 13:21:30 +0530

    Time: 231 ms (72.06%), Space: 76.9 MB (84.37%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    static bool compare(const pair<int, double>& a, const pair<int, double>& b){
+    
+    static bool compare(const pair<int, double> &a, const pair<int, double>&b){
         return a.first <= b.first;
     }
     
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         
-        int n = position.size();
-        
-        vector<pair<int, double>> cardata(n);
-        // first -> position of car
-        // second -> time taken to reach destination
+        int n=position.size();
+        vector<pair<int, double>> carData(n);
         
         for(int i=0; i<n; i++){
-            cardata[i].first = position[i];
-            cardata[i].second = (double)(target-position[i])/speed[i];
+            carData[i].first = position[i];
+            carData[i].second = (double)(target-position[i])/speed[i];
         }
         
-        sort(cardata.begin(), cardata.end(), compare);
+        sort(carData.begin(), carData.end(), compare);
         
-        int no_of_fleet=1;
-        double tt = cardata[n-1].second;
+        int nof=1;
+        double tt = carData[n-1].second;
         
         for(int i=n-2; i>=0; i--){
-            if(cardata[i].second > tt){
-                no_of_fleet++;
-                tt = cardata[i].second;
+            if(carData[i].second > tt){
+                nof++;
+                tt = carData[i].second;
             }
         }
         
-        return no_of_fleet;
-        
-        
+        return nof;
     }
 };
