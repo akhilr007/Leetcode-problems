69c0a484a86382b3fe2294a927b7e4da74e2f302
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-15 15:01:35 +0530

    Time: 4 ms (24.91%), Space: 6.2 MB (99.13%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int i) {
+        
+        vector<int> res ;
+        
+        long long int val=1;
+        for(int j=0; j<=i; j++){
+            
+            res.push_back(val);
+            val = (val * (i-j))/(j+1);
+        }
+        
+        return res;
+    }
+};
