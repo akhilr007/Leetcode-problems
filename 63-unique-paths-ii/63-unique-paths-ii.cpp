22c16ea2a37a8ee90d8926bd076acd4704a267e5
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-20 15:31:56 +0530

    Time: 3 ms (79.72%), Space: 7.7 MB (56.78%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -19,8 +19,32 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
         
-        return f(n-1, m-1, grid, dp);
+        // return f(n-1, m-1, grid, dp);
+        
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                
+                else if(i==0 && j==0) dp[i][j] = 1; 
+                
+                else{
+                    
+                    int up = 0;
+                    int left =0;
+                    
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
 };
