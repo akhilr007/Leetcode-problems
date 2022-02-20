7010efb8772d79885478295d1035b40e317140f4
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-20 15:19:17 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (49.81%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -19,23 +19,28 @@ public:
         // return f(m-1, n-1, dp);
         
         // for tabulation
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        // for space optimization
+        vector<int> prev(n, 0);
         
         for(int i=0; i<m; i++){
+            vector<int> cur(n, 0);
             for(int j=0; j<n; j++){
-                if(i==0 && j==0) dp[i][j] = 1;
+                if(i==0 && j==0) cur[j] = 1;
                 
                 else{
                     int up=0, left=0;
-                   if(i>0)  up = dp[i-1][j];
-                   if(j>0)  left = dp[i][j-1];
+                   if(i>0)  up = prev[j];
+                   if(j>0)  left = cur[j-1];
                     
-                    dp[i][j] = up+left;
+                    cur[j] = up+left;
                 }
             }
+            prev = cur;
         }
         
-        return dp[m-1][n-1];
+        return prev[n-1];
         
     }
 };
