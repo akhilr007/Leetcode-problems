c6f4430f3d0e452b72fb0fd12aaeb2604b0a9d14
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-20 22:33:33 +0530

    Time: 15 ms (40.83%), Space: 9.9 MB (51.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -23,25 +23,29 @@ public:
         // vector<vector<int>> dp(n, vector<int>(m, -1));
         // return f(n-1, m-1, grid, dp);
         
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        // vector<vector<int>> dp(n, vector<int>(m, 0));
+        
+        vector<int> prev(m, 0);
         
         for(int i=0; i<n; i++){
+            vector<int> cur(m, 0);
             for(int j=0; j<m; j++){
                 
-                if(i==0 && j==0) dp[i][j] = grid[i][j];
+                if(i==0 && j==0) cur[j] = grid[i][j];
                 
                 else{
                     
                     int up =1e8, left = 1e8;
                     
-                    if(i>0) up = grid[i][j] + dp[i-1][j];
-                    if(j>0) left = grid[i][j] + dp[i][j-1];
+                    if(i>0) up = grid[i][j] + prev[j];
+                    if(j>0) left = grid[i][j] + cur[j-1];
                     
-                    dp[i][j] = min(up, left);
+                    cur[j] = min(up, left);
                 }
             }
+            prev = cur;
         }
         
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
