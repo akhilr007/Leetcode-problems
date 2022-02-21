f4c51d2eb1c04f1944f27f09ba3b10d9ea0617e5
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-21 13:04:14 +0530

    Time: 4 ms (93.14%), Space: 8.8 MB (39.61%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -20,22 +20,26 @@ public:
         // vector<vector<int>> dp(n, vector<int>(n, -1));
         // return f(0, 0, n, m, triangle, dp);
         
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        // vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        vector<int> front(n, 0);
         
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            front[j] = triangle[n-1][j];
         }
         
         for(int i=n-2; i>=0; i--){
+            vector<int> cur(n, 0);
             for(int j=i; j>=0; j--){
                 
-                int down = triangle[i][j] + dp[i+1][j];
-                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + front[j];
+                int diagonal = triangle[i][j] + front[j+1];
                 
-                dp[i][j] = min(down, diagonal);
+                cur[j] = min(down, diagonal);
             }
+            front = cur;
         }
         
-        return dp[0][0];
+        return front[0];
     }
 };
