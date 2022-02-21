1e035cc7c0ca2a7818cb889a005f30d33c4c8d97
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-21 13:02:04 +0530

    Time: 8 ms (64.76%), Space: 9.2 MB (9.49%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -17,7 +17,25 @@ public:
         int n=triangle.size();
         int m=triangle[0].size();
         
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, 0, n, m, triangle, dp);
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        // return f(0, 0, n, m, triangle, dp);
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
