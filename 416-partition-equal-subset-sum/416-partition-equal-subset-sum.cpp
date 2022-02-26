0cb34d7e969fb28d0314720b3d3f256a386a0943
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-26 13:26:36 +0530

    Time: 236 ms (47.46%), Space: 75.2 MB (21.25%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     
-    bool f(int idx, int target, vector<int>& nums, vector<vector<int>>& dp){
+    bool f(int idx, vector<int>& nums, int tar, vector<vector<int>>& dp){
         
-        if(target == 0) return true;
-        if(idx==0) return nums[idx] == target;
+        if(tar==0) return true;
+        if(idx==0) return nums[idx] == tar;
         
-        if(dp[idx][target] != -1) return dp[idx][target];
+        if(dp[idx][tar] != -1) return dp[idx][tar];
         
-        bool notTake = f(idx-1, target, nums, dp);
+        bool notTake = f(idx-1, nums, tar, dp);
         bool take = false;
+        if(nums[idx] <= tar) take = f(idx-1, nums, tar-nums[idx], dp);
         
-        if(nums[idx]<=target) take = f(idx-1, target-nums[idx], nums, dp);
-        
-        return dp[idx][target] = notTake|take;
+        return dp[idx][tar] = notTake | take;
     }
     
     bool canPartition(vector<int>& nums) {
@@ -21,36 +20,13 @@ public:
         int n=nums.size();
         
         int totalSum=0;
-        for(auto val: nums) totalSum += val;
+        for(auto val : nums) totalSum += val;
         
         if(totalSum%2) return false;
         
-        int target = totalSum/2;
+        int tar = totalSum/2;
         
-        // vector<vector<bool>> dp(n, vector<bool> (target+1, 0));
-        // return f(n-1, target, nums, dp);
-        
-        vector<int> prev(target+1, 0), cur(target+1, 0);
-        
-        prev[0] = cur[0] = true;
-        
-        if(nums[0] <= target) prev[nums[0]] = true;
-        
-        // for(int i=0; i<n; i++) dp[i][0] = true;
-        // if(nums[0] <= target) dp[0][nums[0]] = true;
-        
-        for(int i=1; i<n; i++){
-            for(int t=1; t<=target; t++){
-                
-                bool notTake = prev[t];
-                bool take = false;
-                if(nums[i] <= t) take = prev[t-nums[i]];
-                
-                cur[t] = notTake | take;
-            }
-            prev = cur;
-        }
-        
-        return prev[target];
+        vector<vector<int>> dp(n, vector<int> (tar+1, -1));
+        return f(n-1, nums, tar, dp);
     }
 };
