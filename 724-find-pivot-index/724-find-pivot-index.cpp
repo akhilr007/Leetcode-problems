312d25bd34b4225e508578e03f30a6d9a29f0061
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-28 18:54:37 +0530

    Time: 26 ms (74.68%), Space: 31.2 MB (28.60%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,34 +2,52 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         
+//         int n=nums.size();
+        
+//         vector<int> lsum(n);
+//         vector<int> rsum(n);
+        
+//         for(int i=0; i<n; i++){
+//             if(i==0) lsum[i]=nums[i];
+//             else lsum[i] = lsum[i-1] + nums[i];
+//         }
+        
+//         for(int i=n-1; i>=0; i--){
+//             if(i==n-1) rsum[i] = nums[i];
+//             else rsum[i] = rsum[i+1] + nums[i];
+//         }
+        
+//         int pivotIndex=-1;
+        
+//         for(int i=0;i<n; i++){
+            
+//             if(i==0 && rsum[i+1]==0) return i;
+            
+//             else if(i==n-1 && lsum[i-1]==0) return i;
+            
+//             else if( (i!=0 && i!=n-1) && (lsum[i-1]==rsum[i+1]) ) {
+//                 return i;
+//             }
+//         }
+        
+//         return pivotIndex;
+        
         int n=nums.size();
         
-        vector<int> lsum(n);
-        vector<int> rsum(n);
+        int sum=0;
+        for(auto val : nums) sum += val;
+        
+        int lsum=0;
+        int rsum=sum;
         
         for(int i=0; i<n; i++){
-            if(i==0) lsum[i]=nums[i];
-            else lsum[i] = lsum[i-1] + nums[i];
+            rsum = rsum-nums[i];
+            
+            if(lsum == rsum) return i;
+            
+            lsum += nums[i];
         }
         
-        for(int i=n-1; i>=0; i--){
-            if(i==n-1) rsum[i] = nums[i];
-            else rsum[i] = rsum[i+1] + nums[i];
-        }
-        
-        int pivotIndex=-1;
-        
-        for(int i=0;i<n; i++){
-            
-            if(i==0 && rsum[i+1]==0) return i;
-            
-            else if(i==n-1 && lsum[i-1]==0) return i;
-            
-            else if( (i!=0 && i!=n-1) && (lsum[i-1]==rsum[i+1]) ) {
-                return i;
-            }
-        }
-        
-        return pivotIndex;
+        return -1;
     }
 };
