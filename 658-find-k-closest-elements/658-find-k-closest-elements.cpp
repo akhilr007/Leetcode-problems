3c0e407c64206f0c6a3e2fab98ace413dfe4d9fd
Akhil Rajan <rajanakhil07@gmail.com>
2022-02-28 20:32:27 +0530

    Time: 68 ms (57.31%), Space: 31.6 MB (65.50%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,47 +1,22 @@
-class compare{
-public:
-    bool operator()(pair<int, int>&a, pair<int ,int>& b){
-        
-        if(a.second==b.second) return a.first<b.first;
-        else return a.second < b.second;
-    }
-};
-
 class Solution {
 public:
-    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         
-        // we can have a priority queue
-        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq; // first -> val , second -> diff with value
+        int lo = 0;
+        int hi = arr.size()-1;
         
-        for(int i=0; i<arr.size(); i++){
+        while(hi-lo >= k){
             
-            int gap = abs(arr[i]-x);
-            
-            if(pq.size()<k){
-                pq.push({arr[i], gap});
-            }
-            else{
-                if(pq.top().second > gap){
-                    pq.pop();
-                    pq.push({arr[i], gap});
-                }
-            }
-            
+            if(abs(arr[lo]-x) > abs(arr[hi]-x)) lo++;
+            else hi--;
         }
         
         vector<int> res;
-        
-        while(pq.size()>0) {
-            int val = pq.top().first;
-            pq.pop();
-            res.push_back(val);
+        for(int i=lo; i<=hi; i++){
+            res.push_back(arr[i]);
         }
         
-        sort(res.begin(), res.end());
         return res;
-        
         
     }
 };
