9a6467c25c45d2504430d008a79017a33a1d2a9d
Akhil Rajan <rajanakhil07@gmail.com>
2022-03-11 22:28:22 +0530

    Time: 12 ms (78.62%), Space: 8.3 MB (57.54%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,18 +5,34 @@ public:
         int len=0;
         
         
-        for(int i=0; i<s.length(); i++){
-            unordered_map<char, int> mp;
-            for(int j=i; j<s.length(); j++){
+//         for(int i=0; i<s.length(); i++){
+//             unordered_map<char, int> mp;
+//             for(int j=i; j<s.length(); j++){
                 
-                if(mp.find(s[j]) == mp.end()){
-                    mp[s[j]]++;
-                    len = max(len, j-i+1);
-                }
-                else{
-                    break;
-                }
+//                 if(mp.find(s[j]) == mp.end()){
+//                     mp[s[j]]++;
+//                     len = max(len, j-i+1);
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return len;
+        
+        int i=0, j=0;
+        unordered_map<char, int> mp;
+        
+        while(j<s.length()){
+            
+            if(mp.find(s[j]) != mp.end()){
+                i = max(i, mp[s[j]] + 1);
             }
+            
+            mp[s[j]]=j;
+            len = max(len, j-i+1);
+            j++;
         }
         
         return len;
