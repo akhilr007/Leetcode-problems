6579760d26e56e6af8e4d1e61324dc99ecbe1ebc
Akhil Rajan <rajanakhil07@gmail.com>
2022-03-11 21:54:08 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.cpp
@@ -12,25 +12,56 @@ class Solution{
     int longestKSubstr(string s, int k) {
     // your code here
         
+        // int len=0;
+        // for(int i=0; i<s.length(); i++){
+        //     int count=0;
+        //     vector<int> v(27, 0);
+        //     for(int j=i; j<s.length(); j++){
+        //         if(v[s[j] - 'a'] == 0){
+        //             v[s[j]-'a'] = 1;
+        //             count++;
+        //         }
+                
+        //         if(count > k) break;
+                
+        //         if(count == k){
+        //             len = max(len, j-i+1);
+        //         }
+        //     }
+            
+        // }
+        // return len == 0 ? -1 : len;
+        
+        int i=0, j=0;
         int len=0;
-        for(int i=0; i<s.length(); i++){
-            int count=0;
-            vector<int> v(27, 0);
-            for(int j=i; j<s.length(); j++){
-                if(v[s[j] - 'a'] == 0){
-                    v[s[j]-'a'] = 1;
-                    count++;
-                }
-                
-                if(count > k) break;
-                
-                if(count == k){
-                    len = max(len, j-i+1);
-                }
+        
+        unordered_map<char, int> mp;
+        
+        while(j<s.length()){
+            
+            mp[s[j]]++;
+            int count = mp.size();
+            
+            if(count < k) j++;
+            
+            else if(count == k){
+                len = max(len, j-i+1);
+                j++;
             }
             
+            else if(count > k){
+                
+                while(mp.size() > k){
+                    mp[s[i]]--;
+                    if(mp[s[i]] == 0) mp.erase(s[i]);
+                    i++;
+                }
+                
+                j++;
+            }
         }
-        return len == 0 ? -1 : len;
+        
+        return len==0 ? -1:len;
     }
 };
 
