2624cdd8e5e6d05d5f1aeb5ccdf7913f4de4b28b
Akhil Rajan <rajanakhil07@gmail.com>
2022-03-22 23:44:16 +0530

    Time: 812 ms (12.78%), Space: 558.5 MB (10.56%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,9 +1,9 @@
 struct Node{
     Node* links[26];
     bool flag = false;
-        
+    
     bool containsKey(char ch){
-        return links[ch-'a'] != NULL;
+        return links[ch -'a'] != NULL;
     }
     
     void put(char ch, Node* node){
@@ -11,6 +11,7 @@ struct Node{
     }
     
     Node* get(char ch){
+        
         return links[ch-'a'];
     }
     
@@ -22,38 +23,34 @@ struct Node{
         return flag;
     }
     
-    bool find(string word, Node* node, int index){
+    bool find(string& word, Node* node, int ind){
         
-        if(index == word.size()){
-            return node->isEnd();
-        }
+        if(ind == word.size()) return node->isEnd();
         
-        if(word[index] == '.'){
+        if(word[ind] == '.'){
             
             for(int j=0; j<26; j++){
-                if(node->links[j] != NULL && find(word, node->links[j], index+1)){
+                if(node->links[j] != NULL && find(word, node->links[j], ind+1)){
                     return true;
                 }
+                
             }
             return false;
-            
         }
+        
         else{
-            if(node->containsKey(word[index])){
-                node = node->get(word[index]);
-                return find(word,node,index+1);
+            
+            if(node->containsKey(word[ind])){
+                node = node->get(word[ind]);
+                return find(word, node, ind+1);
             }
-            else{
-                return false;
-            }
+            else return false;
         }
     }
-    
 };
 
 class WordDictionary {
 private : Node* root;
-    
 public:
     WordDictionary() {
         root = new Node();
@@ -64,19 +61,20 @@ public:
         Node* node = root;
         
         for(int i=0; i<word.length(); i++){
+            
             if(!node->containsKey(word[i])){
                 node->put(word[i], new Node());
             }
             node = node->get(word[i]);
         }
-        
         node->setEnd();
-        
     }
     
     bool search(string word) {
-        Node* node= root;
-        return root->find(word,node, 0);
+        
+        Node* node = root;
+        
+        return root->find(word, node, 0);
     }
 };
 
