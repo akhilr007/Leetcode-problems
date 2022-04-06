4b7163f373d87d1239e75a08ee1a2266bf605a28
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-06 12:56:13 +0530

    Time: 24 ms (77.21%), Space: 10.7 MB (47.62%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    long mod = (int)1e9+7;
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        
+        unordered_map<int, long> mp;
+        for(auto el : arr){
+            mp[el]++;
+        }
+        
+        long ans=0;
+        for(auto i: mp){
+            for(auto j : mp){
+                int el1 = i.first;
+                int el2 = j.first;
+                int el3 = target - el1 -el2;
+                
+                if(mp.find(el3) != mp.end()){
+                    
+                    long f1 = mp[el1];
+                    long f2 = mp[el2];
+                    long f3 = mp[el3];
+                    
+                    // case 1 if all the elements are same
+                    if(el1 == el2 and el1 == el3){
+                        // nc3
+                        ans += ((f1)*(f1-1)*(f1-2))/6;
+                    }
+                    else if(el1 == el2 and el1 != el3){
+                        // nc2 * nc1
+                        ans += ((f1)*(f1-1))/2 * f3;
+                    }
+                    else if(el1 < el2 and el2 < el3){
+                        ans += f1*f2*f3;
+                    }
+                    ans = ans%mod;
+                }
+                
+            }
+        }
+        
+        return (int)ans;
+    }
+};
