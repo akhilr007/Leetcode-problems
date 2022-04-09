bdbeea22cb3318fe9617d0f6abab111846432ea3
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-09 13:15:13 +0530

    Time: 0 ms (100.00%), Space: 9 MB (84.04%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -4,20 +4,12 @@ public:
         
         sort(arr.begin(), arr.end());
         
-        unordered_set<int> s; // delta -> index;
         int delta = arr[1]-arr[0];
-        s.insert(delta);
         
         for(int i=2; i<arr.size(); i++){
             
-            delta = arr[i] - arr[i-1];
-            
-            if(s.find(delta) != s.end()){
-                continue;
-            }
-            else{
-                return false;
-            }
+            int deltaNew = arr[i] - arr[i-1];
+            if(delta != deltaNew) return false;
         }
         
         return true;
