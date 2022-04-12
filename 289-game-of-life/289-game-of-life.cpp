ac8bc3ad260527c74681938b4eae255319342ab2
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-12 12:27:58 +0530

    Time: 0 ms (100.00%), Space: 7 MB (20.27%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,56 +1,62 @@
 class Solution {
+private:
+    int dir[8][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 public:
+    int getActiveNeighbour(vector<vector<int>>& board, int i, int j){
+        
+        int activeNbr=0;
+        
+        for(int R=0; R<8; R++){
+            int r = i + dir[R][0];
+            int c = j + dir[R][1];
+            
+            if(r >= 0 && c >= 0 && r < board.size() && c < board[0].size() && (board[r][c] == 1 || board[r][c] == -2)){
+                activeNbr++;
+            }
+        }
+        
+        return activeNbr;
+    }
+    
+    void updateBoard(vector<vector<int>>& board){
+        
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+                
+                if(board[i][j] == -2){
+                    board[i][j] = 0;
+                }
+                else if(board[i][j] == 3){
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+    
     void gameOfLife(vector<vector<int>>& board) {
         
         int n = board.size();
         int m = board[0].size();
         
-        // top left down right top-dia-left top-dia-right bott-dia-left bott-dia-right
-        int dir[8][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-        
-        vector<vector<int>> res(n, vector<int> (m, 0));
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 
-                int life=0;
-                
                 if(board[i][j] == 1){
-                    
-                    for(int k=0; k<8; k++){
-                        int r = i + dir[k][0];
-                        int c = j + dir[k][1];
-                        
-                        if(r >=0 && c >= 0 && r < board.size() && c < board[0].size()){
-                            if(board[r][c] == 1){
-                                life++;
-                            }
-                        }
+                    int activeNbr = getActiveNeighbour(board, i, j);
+                    if(activeNbr < 2 || activeNbr > 3){
+                        board[i][j] = -2; // 0
                     }
-                    
-                    if(life < 2) res[i][j]=0;
-                    else if(life == 2 || life == 3) res[i][j] = 1;
-                    else if(life > 3) res[i][j] = 0;
-                    
                 }
-                else{
-                    
-                    for(int k=0; k<8; k++){
-                        int r = i + dir[k][0];
-                        int c = j + dir[k][1];
-                        
-                        if(r >=0 && c >= 0 && r < board.size() && c < board[0].size()){
-                            if(board[r][c] == 1){
-                                life++;
-                            }
-                        }
+                else if(board[i][j] == 0){
+                    int activeNbr = getActiveNeighbour(board, i, j);
+                    if(activeNbr == 3){
+                        board[i][j] = 3; // 1
                     }
-                    
-                    if(life == 3) res[i][j] = 1;
                 }
             }
         }
         
-        board = res;
+        updateBoard(board);
+        return;
     }
 };
