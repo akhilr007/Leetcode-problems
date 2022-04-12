faaa240825525e588e88ee32926ef25e35fd333a
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-12 22:32:46 +0530

    Time: 72 ms (55.63%), Space: 21.2 MB (50.80%) - LeetHub

---
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+void dfs(int i, int j, vector<vector<int>>&grid, queue<pair<int, int>>& q){
+    
+    if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != 1){
+        return;
+    }
+    
+    grid[i][j]=2;
+    q.push(make_pair(i, j));
+    
+    dfs(i-1, j, grid, q);
+    dfs(i, j-1, grid, q);
+    dfs(i+1, j, grid, q);
+    dfs(i, j+1, grid, q);
+}
+
+int shortestBridge(vector<vector<int>>& grid){
+    
+    queue<pair<int, int>> q;
+    bool flag=false;
+    
+    for(int i=0; i<grid.size()&& !flag; i++){
+        for(int j=0; j<grid[0].size()&& !flag; j++){
+            if(grid[i][j]==1){
+                dfs(i, j, grid, q);
+                flag=true;
+            }
+        }
+    }
+    
+    int level=0;
+    int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    
+    while(q.size()>0){
+        
+        int size = q.size();
+        
+        for(int i=0; i<size; i++){
+            
+            auto rem = q.front(); q.pop();
+            
+            int row = rem.first;
+            int col = rem.second;
+            
+            for(int d=0; d<4; d++){
+                
+                int r = row + dir[d][0];
+                int c = col + dir[d][1];
+                
+                if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size()){
+                    if(grid[r][c] == 0){
+                        q.push({r, c});
+                        grid[r][c]=2;
+                    }
+                    else if(grid[r][c] == 1){
+                        return level;
+                    }
+                }
+            }
+        }
+        level++;
+    }
+    
+    return -1;
+}
+};
