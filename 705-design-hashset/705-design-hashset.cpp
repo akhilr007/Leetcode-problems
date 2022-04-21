098281d640d963c9d91ff67a751b291b89ce1a6a
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-21 11:46:41 +0530

    Time: 80 ms (96.38%), Space: 53.8 MB (30.51%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,7 +1,7 @@
 class MyHashSet {
 private:
     int numBuckets;
-    vector<list<int>> buckets;
+    vector<list<int>> bucket;
     
     int hashFunction(int key){
         return key%numBuckets;
@@ -9,32 +9,36 @@ private:
     
 public:
     MyHashSet() {
-        numBuckets=15000;
-        buckets = vector<list<int>> (numBuckets, list<int>{});
+        numBuckets = 15000;
+        bucket = vector<list<int>> (numBuckets, list<int>{});
     }
     
     void add(int key) {
-        int idx = hashFunction(key);
         
-        if(find(buckets[idx].begin(), buckets[idx].end(), key) == buckets[idx].end()){
-            buckets[idx].push_back(key);
+        int i = hashFunction(key);
+        
+        if(find(bucket[i].begin(), bucket[i].end(), key) == bucket[i].end()){
+            bucket[i].push_back(key);
         }
     }
     
     void remove(int key) {
-        int idx = hashFunction(key);
-        auto it = find(buckets[idx].begin(), buckets[idx].end(), key);
-        if(it != buckets[idx].end()){
-            buckets[idx].erase(it);
+        
+        int i = hashFunction(key);
+        
+        auto it = find(bucket[i].begin(), bucket[i].end(), key);
+        if(it != bucket[i].end()){
+            bucket[i].erase(it);
         }
     }
     
     bool contains(int key) {
-        int idx = hashFunction(key);
-        if(find(buckets[idx].begin(), buckets[idx].end(), key) == buckets[idx].end()){
+        
+        int i = hashFunction(key);
+        
+        if(find(bucket[i].begin(), bucket[i].end(), key) == bucket[i].end()){
             return false;
         }
-        
         return true;
     }
 };
