8e70fca7c7997cc709b065c194236773108406ff
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-25 18:25:27 +0530

    Time: 27 ms (12.00%), Space: 20.3 MB (65.39%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -15,35 +15,20 @@ private:
         
         if(root == NULL) return -1;
         
-        int left = height(root->left);
-        int right = height(root->right);
-        
-        return max(left, right)+1;
+        return max(height(root->left), height(root->right))+1;
     }
-    
 public:
-    pair<int, int> diameterFast(TreeNode* root){
-        
-        if(root == NULL){
-            pair<int, int> ans = make_pair(0, -1);
-            return ans;
-        }
-        
-        pair<int, int> left = diameterFast(root->left);
-        pair<int, int> right = diameterFast(root->right);
-        
-        int op1 = left.first;
-        int op2 = right.first;
-        int op3 = left.second + right.second + 2;
-        
-        pair<int, int> ans;
-        ans.first = max(op1, max(op2, op3));
-        ans.second = max(left.second, right.second)+1;
-        
-        return ans;
-    }
     int diameterOfBinaryTree(TreeNode* root) {
         
-        return diameterFast(root).first;
+        if(root == NULL){
+            return 0;
+        }
+        
+        int op1 = diameterOfBinaryTree(root->left);
+        int op2 = diameterOfBinaryTree(root->right);
+        
+        int op3 = height(root->left) + height(root->right) + 2;
+        
+        return max(max(op1, op2), op3);
     }
 };
