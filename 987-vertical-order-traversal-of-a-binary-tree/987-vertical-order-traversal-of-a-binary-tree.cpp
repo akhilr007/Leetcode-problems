ebdafe905d03a05cc205ce360d98daa0032e8d9b
Akhil Rajan <rajanakhil07@gmail.com>
2022-04-28 17:16:12 +0530

    Time: 3 ms (91.06%), Space: 13.7 MB (33.68%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -16,41 +16,46 @@ public:
         vector<vector<int>> ans;
         if(root == NULL) return ans;
         
-        // hd -> [level, nodes]
-        map<int, map<int, multiset<int> > > nodes;
-        // node, hd, level
-        queue<pair<TreeNode*, pair<int, int> > > q;
-        q.push({root, {0, 0} });
+        map<int, map<int, multiset<int>>> m;
+        // [hd ->[[level]->nodes]]
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});
         
-        while(q.size() > 0){
+        while(q.size()>0){
             
-            auto rem = q.front();
-            q.pop();
+            auto rem = q.front(); q.pop();
             
             TreeNode* node = rem.first;
             int hd = rem.second.first;
-            int lvl = rem.second.second;
+            int level = rem.second.second;
             
-            nodes[hd][lvl].insert(node->val);
+            m[hd][level].insert(node->val);
             
-            if(node->left != NULL){
-                q.push({node->left, {hd-1, lvl+1} });
+            if(node->left){
+                q.push({node->left, {hd-1, level+1}});
             }
-            if(node->right != NULL){
-                q.push({node->right, {hd+1, lvl+1} });
+            if(node->right){
+                q.push({node->right, {hd+1, level+1}});
             }
         }
         
-        for(auto i : nodes){
+        
+        for(auto i : m){
+            
             vector<int> res;
+            
             for(auto j : i.second){
                 for(auto k : j.second){
                     res.push_back(k);
                 }
             }
+            
             ans.push_back(res);
         }
         
         return ans;
+        
+        
+        
     }
 };
