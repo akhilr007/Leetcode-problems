834991170faf49f3a204b42eeceaa2d88e918081
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-03 14:28:30 +0530

    Time: 11 ms (85.66%), Space: 10.2 MB (97.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -26,21 +26,39 @@ public:
         // vector<vector<int>> dp(n, vector<int> (n+1, -1));
         // return f(0, -1, nums, n, dp); // ind prev_index arr size of arr
         
-        vector<int> dp(n, 1);
-        int maxi=0;
+//         vector<int> dp(n, 1);
+//         int maxi=0;
         
-        for(int ind=0; ind<n; ind++){
+//         for(int ind=0; ind<n; ind++){
             
-            for(int prev_ind=0; prev_ind<ind; prev_ind++){
+//             for(int prev_ind=0; prev_ind<ind; prev_ind++){
                 
-                if(nums[prev_ind] < nums[ind]){
+//                 if(nums[prev_ind] < nums[ind]){
                     
-                    dp[ind] = max(dp[ind], 1 + dp[prev_ind]);
-                }
+//                     dp[ind] = max(dp[ind], 1 + dp[prev_ind]);
+//                 }
+//             }
+//             maxi = max(maxi, dp[ind]);
+//         }
+        
+//         return maxi;
+        
+        if(nums.size()==0) return 0;
+        
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=1; i<nums.size(); i++){
+            
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
             }
-            maxi = max(maxi, dp[ind]);
+            else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
         }
         
-        return maxi;
+        return temp.size();
     }
 };
