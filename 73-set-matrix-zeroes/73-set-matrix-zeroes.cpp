a70187ffdf351566fc618cc66775dad361110a65
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-05 22:24:40 +0530

    Time: 23 ms (31.97%), Space: 13.3 MB (54.67%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,25 +4,29 @@ public:
         
         int n=grid.size();
         int m=grid[0].size();
-        
-        vector<int> rows(n, 1), cols(m, 1);
+        int col0=1;
         
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+            
+            if(grid[i][0]==0) col0=0;
+            
+            for(int j=1; j<m; j++){
                 
                 if(grid[i][j] == 0){
-                    rows[i]=0;
-                    cols[j]=0;
+                    grid[i][0] = grid[0][j] = 0;
                 }
             }
         }
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(rows[i]==0 or cols[j]==0){
+        for(int i=n-1; i>=0; i--){
+            
+            for(int j=m-1; j>=1; j--){
+                if(grid[i][0] == 0 || grid[0][j] == 0){
                     grid[i][j]=0;
                 }
             }
+            
+            if(col0==0) grid[i][0] = 0;
         }
     }
 };
