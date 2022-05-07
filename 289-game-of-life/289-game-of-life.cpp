7d0c53cdf8516403e85110abbb2f6ee8a793facc
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-08 00:03:02 +0530

    Time: 3 ms (43.41%), Space: 6.9 MB (85.85%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,52 +1,51 @@
-class Solution {
+
+class Solution{
 private:
     int dir[8][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
 public:
     int getActiveNeighbour(vector<vector<int>>& board, int i, int j){
-        
+
         int activeNbr=0;
-        
-        for(int R=0; R<8; R++){
-            int r = i + dir[R][0];
-            int c = j + dir[R][1];
-            
+
+        for(int d=0; d<8; d++){
+
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
             if(r >= 0 && c >= 0 && r < board.size() && c < board[0].size() && (board[r][c] == 1 || board[r][c] == -2)){
                 activeNbr++;
             }
         }
-        
+
         return activeNbr;
     }
-    
+
     void updateBoard(vector<vector<int>>& board){
-        
+
         for(int i=0; i<board.size(); i++){
             for(int j=0; j<board[0].size(); j++){
-                
-                if(board[i][j] == -2){
-                    board[i][j] = 0;
-                }
-                else if(board[i][j] == 3){
-                    board[i][j] = 1;
-                }
+
+                if(board[i][j] == -2) board[i][j] = 0;
+                else if(board[i][j] == 3) board[i][j] = 1;
             }
         }
     }
-    
-    void gameOfLife(vector<vector<int>>& board) {
-        
-        int n = board.size();
-        int m = board[0].size();
-        
+
+    void gameOfLife(vector<vector<int>>& board){
+
+        int n=board.size();
+        int m=board[0].size();
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                
                 if(board[i][j] == 1){
                     int activeNbr = getActiveNeighbour(board, i, j);
                     if(activeNbr < 2 || activeNbr > 3){
-                        board[i][j] = -2; // 0
+                        board[i][j] = -2; // 0;
                     }
                 }
+
                 else if(board[i][j] == 0){
                     int activeNbr = getActiveNeighbour(board, i, j);
                     if(activeNbr == 3){
@@ -55,7 +54,7 @@ public:
                 }
             }
         }
-        
+
         updateBoard(board);
         return;
     }
