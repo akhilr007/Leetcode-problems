cf0cbe0cafe8852754b549000303087dc246e4e0
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-07 21:31:24 +0530

    Time: 759 ms (43.98%), Space: 147.4 MB (43.05%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,15 +1,14 @@
-class Solution {
+class Solution{
 public:
     int count;
-    
+
     vector<int> merge(vector<int>& left, vector<int>& right){
-        
-        int i=0;
-        int j=0;
+
         int n=left.size(), m=right.size();
-        
+        int i=0, j=0;
+
         while(i<n and j<m){
-            if(left[i] > 2LL *right[j]){
+            if(left[i] > 2LL * right[j]){
                 count += (n-i);
                 j++;
             }
@@ -17,59 +16,57 @@ public:
                 i++;
             }
         }
-        
-        
-        i=0;
-        j=0;
+
+        i=0, j=0;
         int k=0;
-        
         vector<int> res(n+m);
-        
+
+        // merge them
         while(i<n and j<m){
-            
+
             if(left[i] < right[j]){
                 res[k] = left[i];
                 i++;
-                k++;
             }
             else{
                 res[k] = right[j];
                 j++;
-                k++;
             }
+            k++;
         }
-        
+
         while(i<n){
             res[k] = left[i];
             i++;
             k++;
         }
-        
+
         while(j<m){
             res[k] = right[j];
             j++;
             k++;
         }
-        
+
         return res;
-        
+
     }
-    
+
     vector<int> mergeSort(vector<int>& nums, int lo, int hi){
-        
-        if(lo==hi){
+
+        if(lo == hi){
             vector<int> ba(1);
             ba[0] = nums[lo];
             return ba;
         }
-        
+
         int mid = (lo+hi)/2;
         vector<int> left = mergeSort(nums, lo, mid);
         vector<int> right = mergeSort(nums, mid+1, hi);
         vector<int> arr = merge(left, right);
         return arr;
     }
-    int reversePairs(vector<int>& nums) {
+
+    int reversePairs(vector<int>& nums){
         count=0;
         mergeSort(nums, 0, nums.size()-1);
         return count;
