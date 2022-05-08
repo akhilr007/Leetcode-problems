96500f4656b07d7458d0fec4c2e8d238ca591b21
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-08 17:41:08 +0530

    Time: 409 ms (24.84%), Space: 115.7 MB (5.41%) - LeetHub

--- 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,76 +1,89 @@
-class RandomizedCollection {
+class RandomizedCollection{
 private:
     vector<int> v;
     unordered_map<int, unordered_set<int>> m;
-    
+
 public:
-    RandomizedCollection() {
-        
+    RandomizedCollection(){
+
     }
-    
-    bool insert(int val) {
-        bool flag;
+
+    bool insert(int val){
+
         unordered_set<int> s;
+        bool flag;
         if(m.find(val) != m.end()){
             flag = false;
             s = m[val];
         }
         else{
             flag = true;
-            m[val] = s;
         }
-        
+
         v.push_back(val);
         s.insert(v.size()-1);
         m[val] = s;
+
         return flag;
     }
-    
-    bool remove(int val) {
-        
+
+    bool remove(int val){
+
         unordered_set<int> s;
-        
+
+        // if val not found in map return false
         if(m.find(val) == m.end()){
             return false;
         }
         else{
+            // if fount, get the set of the val
             s = m[val];
         }
-        
-        int remidx = -1;
-        for(int i: s){
+
+        int remidx;
+        // pick the first element from set
+        for(int i : s){
             remidx = i;
             break;
         }
-        
+
+        // remove the remidx from set
         s.erase(remidx);
-        
+        // update the m[val]
         m[val] = s;
+
+        // if m[val]-> set list size is 0 then erase val from m
         if(m[val].size() == 0){
             m.erase(val);
         }
-        
+
+        // now remove the element at remidx from vector
+        // if remidx is at the last index of vector then simply remove it
+
         if(remidx == v.size()-1){
             v.pop_back();
         }
         else{
             v[remidx] = v.back();
             v.pop_back();
-            
+
             int newval = v[remidx];
+
+            // change the index of newval
             unordered_set<int> temp = m[newval];
-            
+
             temp.erase(v.size());
             temp.insert(remidx);
-            
+
             m[newval] = temp;
         }
-        
+
         return true;
+
     }
-    
-    int getRandom() {
-        return v[rand()%v.size()];
+
+    int getRandom(){
+        return v[rand() % v.size()];
     }
 };
 
