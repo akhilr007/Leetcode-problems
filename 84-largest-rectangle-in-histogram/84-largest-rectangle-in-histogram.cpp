4f826df0797a4fd932a09b01b33862038d7bd555
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-08 18:00:19 +0530

    Time: 265 ms (35.84%), Space: 81.4 MB (41.47%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,75 +1,78 @@
-class Solution {
+class Solution{
 public:
-    
-    vector<int> findNextSmallerLeft(vector<int>& heights){
-        
+    vector<int> nextSmallerToLeft(vector<int>& heights){
+
         int n=heights.size();
         vector<int> nsl(n);
-        
+
         stack<int> st;
-        nsl[0]=-1;
+
+        nsl[0] = -1;
         st.push(0);
-        
+
         for(int i=1; i<heights.size(); i++){
-            
+
             while(st.size()>0 and heights[st.top()] >= heights[i]){
                 st.pop();
             }
-            
-            if(st.size() ==0){
-                nsl[i]=-1;
+
+            if(st.size() == 0){
+                nsl[i] = -1;
             }
             else{
                 nsl[i] = st.top();
             }
-            
+
             st.push(i);
         }
-        
+
         return nsl;
     }
-    
-    vector<int> findNextSmallerRight(vector<int>& heights){
-        
+
+    vector<int> nextSmallerToRight(vector<int>& heights){
+
         int n=heights.size();
         vector<int> nsr(n);
-        
+
         stack<int> st;
-        nsr[n-1]=n;
+
+        nsr[n-1] = n;
         st.push(n-1);
-        
+
         for(int i=n-2; i>=0; i--){
-            
+
             while(st.size()>0 and heights[st.top()] >= heights[i]){
                 st.pop();
             }
-            
-            if(st.size() ==0){
-                nsr[i]=n;
+
+            if(st.size() == 0){
+                nsr[i] = n;
             }
             else{
                 nsr[i] = st.top();
             }
-            
+
             st.push(i);
         }
-        
+
         return nsr;
     }
-    
-    int largestRectangleArea(vector<int>& heights) {
-        
-        vector<int> nsl = findNextSmallerLeft(heights);
-        vector<int> nsr = findNextSmallerRight(heights);
-        
-        int area=0;
+
+    int largestRectangleArea(vector<int>& heights){
+
+        vector<int> nsl = nextSmallerToLeft(heights);
+        vector<int> nsr = nextSmallerToRight(heights);
+
+        int maxArea=0;
         for(int i=0; i<heights.size(); i++){
-            int width = (nsr[i]-nsl[i])-1;
-            int height = heights[i];
             
-            area = max(area, width*height);
+            int height = heights[i];
+            int width = nsr[i] - nsl[i] - 1;
+
+            maxArea = max(height*width, maxArea);
         }
-        
-        return area;
+
+        return maxArea;
+
     }
 };
