c30c1fa77c234785a440b9c20ea0410de71c2f84
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-11 21:59:40 +0530

    Added solution - LeetHub

--- Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.cpp
@@ -6,59 +6,64 @@ using namespace std;
  // } Driver Code Ends
 class Solution
 {
-    public:
-        vector <int> search(string p, string t)
-        {
-            long power = 31;
-            long p_ = 31;
-            long pHash = p[0]-'a'+1;
-            long mod = 1e9+7;
-            
-            for(int i=1; i<p.length(); i++){
-                pHash = (pHash + (p[i]-'a'+1)*power)%mod;
-                power = (power*p_)%mod;
-            }
-            
-            //rabin karp alogorithm implementation
-            vector<long> dp(t.length());
-            vector<long> pi(t.length()); // storing power like p0, p1, p2
-            
-            power = p_;
-            dp[0] = t[0]-'a'+1;
-            pi[0] = 1;
-            
-            for(int i=1; i<t.length(); i++){
-                
-                dp[i] = (dp[i-1] + (t[i]-'a'+1)*power)%mod;
-                pi[i] = power;
-                power = (power*p_)%mod;
-            }
-            
-            vector<int> ans;
-            int sp=0;
-            int ep=p.length()-1;
-            
-            while(ep<t.length()){
-                long curWin = dp[ep];
-                if(sp>0) curWin = (curWin - dp[sp-1] + mod)%mod;
-                if(curWin == (pHash*pi[sp])%mod){
-                    ans.push_back(sp+1);
-                }
-                sp++;
-                ep++;
-            }
-            
-            if(ans.size()==0){
-                vector<int> v;
-                v.push_back(-1);
-                return v;
-            }
-            
-            return ans;
-        }
-     
-};
+public:
+    vector <int> search(string pat, string txt)
+    {
 
+
+        long power = 31; // prime number
+        long p=31;
+        long patHash = pat[0]-'a'+1;
+        long mod = 1e9+7;
+
+        for(int i=1; i<pat.length(); i++){
+            patHash = (patHash + (pat[i]-'a'+1)*power)%mod;
+            power = (power*p)%mod;
+        }
+
+        // rabin karp algo starts
+        power = p;
+        vector<long> dp(txt.length());
+        vector<long> pi(txt.length());
+
+        dp[0] = txt[0]-'a'+1;
+        pi[0] = 1;
+
+        for(int i=1; i<txt.length(); i++){
+            dp[i] = (dp[i-1] + (txt[i]-'a'+1)*power)%mod;
+            pi[i] = power;
+            power = (power*p)%mod;
+        }
+
+        vector<int> ans;
+        int sp=0;
+        int ep=pat.length()-1;
+
+        while(ep<txt.length()){
+            long curWindowHashValue = dp[ep];
+
+            if(sp>0){
+                curWindowHashValue = (curWindowHashValue - dp[sp-1] + mod) % mod;
+            }
+
+            if(curWindowHashValue == (patHash*pi[sp])%mod){
+                ans.push_back(sp+1);
+            }
+            sp++;
+            ep++;
+        }
+
+        if(ans.size()==0){
+            vector<int> v(1);
+            v[0] = -1;
+            return v;
+        }
+
+        return ans;
+
+     }
+};
+        
 // { Driver Code Starts.
 int main()
 {
