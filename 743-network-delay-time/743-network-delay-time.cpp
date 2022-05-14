771993b29b62617258cbbc30cc596544ae79e294
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-14 15:23:10 +0530

    Time: 152 ms (60.12%), Space: 40 MB (71.55%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -14,7 +14,6 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
         vector<Edge> graph[n+1];
-        
         for(int i=0; i<times.size(); i++){
             int src = times[i][0];
             int nbr = times[i][1];
@@ -23,40 +22,37 @@ public:
             graph[src].push_back(Edge(nbr, wt));
         }
         
-        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-        pq.push({0, k});
+        queue<pair<int, int>> q;
+        q.push(make_pair(k, 0)); // vertex->weight
         
-        vector<bool> vis(n+1, false);
-        vector<int> time(n+1, INT_MAX);
+        vector<int> signal(n+1, INT_MAX);
+        signal[k]=0;
         
-        while(pq.size()>0){
+        while(q.size()>0){
             
-            auto rem = pq.top();
-            pq.pop();
+            auto rem = q.front();
+            q.pop();
             
-            int v = rem.second;
-            int wsf = rem.first;
+            int src = rem.first;
+            //int wt = rem.second;
             
-            if(vis[v] == true) continue;
-            vis[v] = true;
-            
-            time[v] = wsf;
-            
-            for(Edge edge : graph[v]){
-                int nbr = edge.nbr;
-                int wt = edge.wt;
+            for(Edge edge : graph[src]){
                 
-                if(vis[nbr] == false){
-                    int weight = wsf + wt;
-                    pq.push({weight, nbr});
+                int nbr = edge.nbr;
+                int time = edge.wt;
+                
+                int arrivalTime = signal[src] + time;
+                if(signal[nbr] > arrivalTime){
+                    signal[nbr] = arrivalTime;
+                    q.push(make_pair(nbr, time));
                 }
             }
         }
         
-        int ans;
-        for(int i=1; i<n+1; i++){
-            if(time[i] == INT_MAX) return -1;
-            ans = max(ans, time[i]);
+        int ans=0;
+        for(int i=1; i<=n; i++){
+            if(signal[i]==INT_MAX) return -1;
+            ans = max(ans, signal[i]);
         }
         
         return ans;
