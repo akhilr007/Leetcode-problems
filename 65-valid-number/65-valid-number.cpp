d0eba110a5aebfdfa8c57d3646e94c710abcaf84
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-15 22:48:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (76.77%) - LeetHub

--- 65-valid-number/65-valid-number.cpp
+++ 65-valid-number/65-valid-number.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     bool isNumber(string s) {
         
+        /*
+        -ve cases:
+        1. decimal point after decimal seen in the past
+        2. decimal point after the power valur .i.e, after e
+        3. e again after e seen in the past
+        4. e without a prefixed number
+        5. +/- will only occur at index 0 or index e+1
+        6. see any other char other than 0-9+-eE.
+        */
+        
         if(s.length()==0) return false;
         
         bool numberSeen = false;
