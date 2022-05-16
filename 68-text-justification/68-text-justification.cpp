b01ad887125a77092f66b8a3e6ffda497325f2ad
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-16 19:30:09 +0530

    Time: 7 ms (11.01%), Space: 7.3 MB (83.27%) - LeetHub

--- 68-text-justification/68-text-justification.cpp
+++ 68-text-justification/68-text-justification.cpp
@@ -1,46 +1,44 @@
-class Solution {
+class Solution{
 public:
-    vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        
+    vector<string> fullJustify(vector<string>& words, int maxWidth){
+
         vector<string> result;
-        
+
         int i=0;
+
         while(i<words.size()){
-            
+
             int wordCharCount = words[i].length();
-            int j=i+1;
-            
-            int normalSpaces=0;
-            while(j<words.size() and (wordCharCount+words[j].length()+1 <= maxWidth)){
-                wordCharCount += words[j].length()+1;
+            int j = i+1; // next word index;
+
+            int normalSpaces=0; // normal spaces used
+            while(j<words.size() and (wordCharCount + words[j].length() + 1 <= maxWidth)){
+                wordCharCount += words[j].length() + 1;
                 normalSpaces++;
                 j++;
             }
-            
+
+            // calculate total spaces left if we consider only characters
             int totalSpaces = maxWidth - wordCharCount + normalSpaces;
-            int distributeSpaces = j-i-1; // i am a boy so 3 spaces are there between words
-            //cout<<totalSpaces<<" "<<distributeSpaces<<" ";
-            
+            // suppose we have a sentence
+            // i am a boy then spaces where we can add space are 3 that is total words-1
+            int distributeSpaces = j-i-1; // 
+
             int minSpace = distributeSpaces == 0 ? 0 : totalSpaces / distributeSpaces;
-            int extraSpace = distributeSpaces == 0 ? 0:  totalSpaces % distributeSpaces;
-            //cout<<minSpace<<" "<<extraSpace<<endl;
+            int extraSpace = distributeSpaces == 0 ? 0 : totalSpaces % distributeSpaces;
             
-            // for last line
             if(j == words.size()){
                 minSpace = 1;
-                extraSpace = 0;
+                extraSpace=0;
             }
-            
+
             string s="";
             for(int k=i; k<j; k++){
-                
                 s += words[k];
-               // cout<<s<<endl;
-                
-                if(k == j-1){
-                    break;
-                }
-                
+
+                // if it is the last word for the line then dont add space
+                if(k == j-1) break;
+
                 for(int e=0; e<minSpace; e++){
                     s += " ";
                 }
@@ -49,16 +47,13 @@ public:
                     extraSpace--;
                 }
             }
-            
-            // the length of each line should be equal to maxwidth
-            while(s.length() < maxWidth){
-                s += " ";
-            }
-            
+
+            while(s.length() < maxWidth) s += " ";
+
             result.push_back(s);
             i=j;
         }
-        
+
         return result;
     }
 };
