e8ea0b0704d12036a7cd939d641c80124cc5541e
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-20 15:08:16 +0530

    Time: 3 ms (36.95%), Space: 6.6 MB (99.85%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,52 +1,50 @@
 class Solution{
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix){
-
-        int n=matrix.size();
-        int m=matrix[0].size();
-
-        int top=0, left=0, right = m-1, bottom=n-1;
-
-        int dir=0;
-
         vector<int> ans;
+
+        int top=0, bottom=matrix.size()-1, left=0, right=matrix[0].size()-1;
+
+        int dir=1;
 
         while(top <= bottom and left <= right){
 
-            if(dir == 0){
-
+            if(dir == 1){
+                // left to right print
                 for(int i=left; i<=right; i++){
                     ans.push_back(matrix[top][i]);
                 }
                 top++;
-                dir=1;
+                dir = 2;
             }
 
-            else if(dir == 1){
+            else if(dir == 2){
 
+                // top to bottom
                 for(int i=top; i<=bottom; i++){
                     ans.push_back(matrix[i][right]);
                 }
                 right--;
-                dir=2;
-            }
-
-            else if(dir==2){
-
-                for(int i=right; i>=left; i--){
-                    ans.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-                dir=3;
+                dir = 3;
             }
 
             else if(dir == 3){
 
-                for(int i=bottom; i>=top; i--){
+                // right to left
+                for(int i=right; i>=left; i--){
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+                dir = 4;
+            }
+            
+            else if(dir == 4){
+                // bottom to up
+                for(int i=bottom; i>= top; i--){
                     ans.push_back(matrix[i][left]);
                 }
                 left++;
-                dir=0;
+                dir = 1;
             }
         }
 
