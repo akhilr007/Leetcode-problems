3155f56570c61f7f8b3ad93c0e7c5bfe0e3bea05
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-20 13:56:01 +0530

    Time: 53 ms (70.57%), Space: 15.8 MB (30.20%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,95 +1,95 @@
-class Solution {
+class Solution{
 public:
-    
-    vector<int> nsl(vector<int>& v){
+
+    vector<int> nextSmallerLeft(vector<int>& v){
         
         int n=v.size();
+
         stack<int> st;
         st.push(0);
-        
-        vector<int> left(n);
-        left[0] = -1;
-        
+
+        vector<int> nsl(n);
+        nsl[0] = -1;
+
         for(int i=1; i<n; i++){
-            
+
             while(st.size()>0 && v[st.top()] >= v[i]){
                 st.pop();
             }
-            
+
             if(st.size()==0){
-                left[i] = -1;
+                nsl[i] = -1;
             }
             else{
-                left[i] = st.top();
+                nsl[i] = st.top();
             }
-            
+
             st.push(i);
         }
-        
-        return left;
+
+        return nsl;
     }
-    
-    vector<int> nsr(vector<int>& v){
+
+    vector<int> nextSmallerRight(vector<int>& v){
         
         int n=v.size();
+
         stack<int> st;
         st.push(n-1);
-        
-        vector<int> right(n);
-        right[n-1] = n;
-        
+
+        vector<int> nsr(n);
+        nsr[n-1] = n;
+
         for(int i=n-2; i>=0; i--){
-            
+
             while(st.size()>0 && v[st.top()] >= v[i]){
                 st.pop();
             }
-            
+
             if(st.size()==0){
-                right[i] = n;
+                nsr[i] = n;
             }
             else{
-                right[i] = st.top();
+                nsr[i] = st.top();
             }
-            
+
             st.push(i);
         }
-        
-        return right;
+
+        return nsr;
     }
-    
-    int maxRectangle(vector<int>& v){
-        
-        vector<int> left = nsl(v);
-        vector<int> right = nsr(v);
-        
+
+    int maximumAreaHistogram(vector<int>& v){
+
+        vector<int> nsl = nextSmallerLeft(v);
+        vector<int> nsr = nextSmallerRight(v);
+
         int maxArea=0;
-        
         for(int i=0; i<v.size(); i++){
-            
             int height = v[i];
-            int width = right[i] - left[i] - 1;
-            
-            maxArea = max(maxArea, height*width);
+            int width = nsr[i]-nsl[i]-1;
+
+            int area = height*width;
+            maxArea = max(maxArea, area);
         }
-        
+
         return maxArea;
     }
     
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        
+    int maximalRectangle(vector<vector<char>>& matrix){
+
         vector<int> v;
-        
-        for(int j=0; j<matrix[0].size(); j++){
-            int val = matrix[0][j]-'0';
+        for(int i=0; i<matrix[0].size(); i++){
+            int val = matrix[0][i] - '0';
             v.push_back(val);
         }
-        
-        int mx=0;
-        mx = max(mx, maxRectangle(v));
-        
+
+        int mx = 0;
+        mx = max(mx, maximumAreaHistogram(v));
+
         for(int i=1; i<matrix.size(); i++){
             for(int j=0; j<matrix[0].size(); j++){
-                
+
                 if(matrix[i][j] == '0'){
                     v[j] = 0;
                 }
@@ -97,10 +97,10 @@ public:
                     v[j] += 1;
                 }
             }
-            
-            mx = max(mx, maxRectangle(v));
+
+            mx = max(mx, maximumAreaHistogram(v));
         }
-        
+
         return mx;
     }
 };
