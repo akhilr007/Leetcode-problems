936fe25a402ed4ef2e33ff8f1be9f1d73a6c99e7
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-20 14:11:48 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -4,37 +4,32 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution {
-
+private:
+    int dir[8][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
   public:
     void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& vis){
-        
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] == '0' || vis[i][j]==true){
-            return;
-        }
-        
+
         vis[i][j] = true;
-        
-        
-        dfs(i-1, j, grid, vis);
-        dfs(i, j-1, grid, vis);
-        dfs(i+1, j, grid, vis);
-        dfs(i, j+1, grid, vis);
-        dfs(i-1, j-1, grid, vis);
-        dfs(i-1, j+1, grid, vis);
-        dfs(i+1, j-1, grid, vis);
-        dfs(i+1, j+1, grid, vis);
-        
+
+        for(int d=0; d<8; d++){
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+
+            if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c] == '0' || vis[r][c] == true){
+                continue;
+            }
+
+            dfs(r, c, grid, vis);
+        }
     }
     // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
-        
         int n=grid.size();
         int m=grid[0].size();
-        
+
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        
+
         int islands=0;
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == '1' && vis[i][j] == false){
@@ -43,7 +38,7 @@ class Solution {
                 }
             }
         }
-        
+
         return islands;
     }
 };
