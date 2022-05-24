df44005674864afbf2f0000645aed3466cc5c7bf
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-24 12:22:42 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (42.55%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        int maxi=0;
+        stack<int> st;
+        st.push(-1);
+        
+        for(int i=0; i<s.length(); i++){
+            
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    int len = i-st.top();
+                    maxi = max(maxi, len);
+                }
+            }
+        }
+        
+        return maxi;
+    }
+};
