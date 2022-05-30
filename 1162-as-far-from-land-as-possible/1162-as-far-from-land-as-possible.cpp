c4922ae1704afa021da788819d6f9ef0e16a8e21
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-30 20:24:54 +0530

    Time: 127 ms (28.49%), Space: 19.7 MB (83.45%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -2,50 +2,45 @@ class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
         
-        int n = grid.size();
-        
         queue<pair<int, int>> q;
-        
+        int n = grid.size();
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                
                 if(grid[i][j] == 1){
-                    q.push(make_pair(i, j));
+                    q.push({i, j});
                 }
             }
         }
         
-        if(q.size() == 0 || q.size() == n*n){
-            return -1;
-        }
-        
-        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        if(q.size() == 0 || q.size() == n*n) return -1;
+
         int level=-1;
-        
+        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
         while(q.size()>0){
-            
+            int size = q.size();
             level++;
-            int size=q.size();
-            
-            for(int i=0; i<size; i++){
-                
-                auto rem = q.front(); q.pop();
-            
-                int r = rem.first;
-                int c = rem.second;
 
-                for(int i=0; i<4; i++){
-                    int row = r + dir[i][0];
-                    int col = c + dir[i][1];
+            for(int k=0; k<size; k++){
+                auto rem = q.front();
+                q.pop();
 
-                    if(row >= 0 && col >= 0 && row < n && col < n && grid[row][col] == 0){
-                        q.push({row, col});
-                        grid[row][col] = 1;
+                int i = rem.first;
+                int j = rem.second;
+
+                for(int d=0; d<4; d++){
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+
+                    if(r >= 0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c] == 0){
+                        q.push({r, c});
+                        grid[r][c] = 1;
                     }
                 }
             }
         }
-        
+
         return level;
     }
 };
