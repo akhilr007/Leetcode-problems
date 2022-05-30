2f6f0262ac8257e0b910f7cd285c104e92161a6d
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-30 21:06:26 +0530

    Time: 89 ms (43.04%), Space: 21.4 MB (47.31%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -1,70 +1,71 @@
 class Solution {
-private:
-    int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 public:
-    
-    void dfs(int i, int j, vector<vector<int>>& grid, queue<pair<int, int>>& q){
-        
+    void dfs(vector<vector<int>>& grid, int i, int j, queue<pair<int, int>>& q){
+
         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != 1){
             return;
         }
+
         grid[i][j] = 2;
-        q.push(make_pair(i, j));
-        
-        dfs(i-1, j, grid, q);
-        dfs(i, j-1, grid, q);
-        dfs(i+1, j, grid, q);
-        dfs(i, j+1, grid, q);
+        q.push({i, j});
+
+        dfs(grid, i-1, j, q);
+        dfs(grid, i, j-1, q);
+        dfs(grid, i+1, j, q);
+        dfs(grid, i, j+1, q);
+
     }
-    
+
     int shortestBridge(vector<vector<int>>& grid) {
         
         int n=grid.size();
-        
-        bool flag = false;
+        int m=grid[0].size();
+
         queue<pair<int, int>> q;
-        
+        bool flag = false;
+
         for(int i=0; i<n && !flag; i++){
-            for(int j=0; j<n && !flag; j++){
+            for(int j=0; j<m && !flag; j++){
                 if(grid[i][j] == 1){
-                    dfs(i, j, grid, q);
-                    flag=true;
+                    dfs(grid, i, j, q);
+                    flag = true;
                 }
             }
         }
-        
+
         int level=-1;
-        
-        while(q.size()>0){
-            
+        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        while(q.size() > 0){
+
             int size = q.size();
             level++;
-            
-            for(int i=0; i<size; i++){
-                
-                auto rem = q.front(); q.pop();
-                
-                int row = rem.first;
-                int col = rem.second;
-                
+
+            for(int k=0; k<size; k++){
+
+                auto rem = q.front();
+                q.pop();
+
+                int i=rem.first;
+                int j=rem.second;
+
                 for(int d=0; d<4; d++){
-                    int r = row + dir[d][0];
-                    int c = col + dir[d][1];
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
                     
-                    if(r>=0 && c>=0 && r<n && c<n){
-                        if(grid[r][c] == 0){
-                            q.push(make_pair(r, c));
-                            grid[r][c] = 2;
-                        }
-                        else if(grid[r][c] == 1){
-                            return level;
-                        }
+                    if(r<0 || c<0 || r>=n || c>=m) continue;
+                    
+                    if(grid[r][c] == 0){
+                        q.push({r, c});
+                        grid[r][c] = 2;
+                    }
+                    else if(grid[r][c] == 1){
+                        return level;
                     }
                 }
             }
-            //level++;
         }
-        
+
         return -1;
     }
 };
