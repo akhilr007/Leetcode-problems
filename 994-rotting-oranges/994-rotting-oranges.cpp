595088f1180183c6ff75d835d0d2ab3ba736dc59
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-30 15:39:31 +0530

    Time: 5 ms (83.76%), Space: 13.1 MB (49.89%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,60 +1,62 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        
-        // calculate fresh oranges and add rotten oranges into queue
-        queue<pair<int, int>> q;
-        
+    int find(vector<vector<int>>& grid, queue<pair<int, int>>& q){
+
         int fresh=0;
-        
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
-                
-                if(grid[i][j] == 2){
-                    q.push(make_pair(i, j));
-                }
-                else if(grid[i][j] == 1){
+                if(grid[i][j] == 1){
                     fresh++;
+                }
+                else if(grid[i][j] == 2){
+                    q.push({i, j});
                 }
             }
         }
+
+        return fresh;
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) {
         
-        if(fresh == 0) return 0;
-        
+        int n=grid.size();
+        int m=grid[0].size();
+
+        queue<pair<int, int>> q; // to store rotten oranges index
+        int freshOranges = find(grid, q);
+
+        if(freshOranges == 0) return 0;
+
+        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
         int level = -1;
-        
-        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}}; // top left down right
-        
-        while(q.size()>0){
-            
-            int size=q.size();
+
+        while(q.size() > 0){
+
+            int size = q.size();
             level++;
-            
+
             for(int k=0; k<size; k++){
-                
+
                 auto rem = q.front(); q.pop();
-                
-                int r = rem.first;
-                int c = rem.second;
-                
-                for(int i=0; i<4; i++){
-                    int rdash = r + dir[i][0];
-                    int cdash = c + dir[i][1];
-                    
-                    if(rdash >= 0 && cdash >=0 && rdash < grid.size() && cdash <grid[0].size() && grid[rdash][cdash] == 1){
-                        q.push(make_pair(rdash, cdash));
-                        grid[rdash][cdash]=0;
-                        fresh--;
+
+                int i = rem.first;
+                int j = rem.second;
+
+                for(int d=0; d<4; d++){
+                    int r = i + dir[d][0];
+                    int c = j + dir[d][1];
+
+                    if(r>=0 && c>=0 && r<n && c<m && grid[r][c] == 1){
+                        q.push({r, c});
+                        grid[r][c] = 0;
+                        freshOranges--;
                     }
                 }
             }
         }
-        
-        if(fresh == 0){
-            return level;
-        }
-        else{
-            return -1;
-        }
+
+        if(freshOranges==0) return level;
+        else return -1;
     }
 };
