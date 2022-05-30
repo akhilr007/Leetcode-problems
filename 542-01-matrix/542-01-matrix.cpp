9556410a50dcbc9cfb1f2e4b981c67d4346363d3
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-30 14:56:10 +0530

    Time: 161 ms (18.67%), Space: 30.1 MB (62.99%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        
+
         int n = mat.size();
         int m = mat[0].size();
-        
+      
         queue<pair<int, int>> q;
-        
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                
                 if(mat[i][j] == 0){
-                    q.push(make_pair(i, j));
+                    q.push({i, j}); // push the indexes of 0 to queue
                 }
                 else{
-                    mat[i][j] = -1;
+                    mat[i][j] = -1; // mark all the 1's unvisited by making it -ve
                 }
             }
         }
-        
+
+        // four directions
         int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        
-        while(q.size()>0){
-            
+
+        while(q.size() > 0){
+
             auto rem = q.front(); q.pop();
-            
-            int r = rem.first;
-            int c = rem.second;
-            
-            for(int i=0; i<4; i++){
-                int row = r + dir[i][0];
-                int col = c + dir[i][1];
-                
-                if(row >= 0 && col >= 0 && row < n && col < m && mat[row][col] < 0){
-                    q.push(make_pair(row, col));
-                    mat[row][col] = mat[r][c] + 1;
+            int i = rem.first;
+            int j = rem.second;
+
+            for(int d=0; d<4; d++){
+                int r = i + dir[d][0];
+                int c = j + dir[d][1];
+
+                if(r >= 0 && c >=0 && r < n && c < m && mat[r][c] == -1){
+                    q.push({r, c});
+                    mat[r][c] = mat[i][j] + 1;
                 }
             }
         }
