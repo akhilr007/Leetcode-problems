257df41bd657eaa9b115dd975d9dd5c88dd77321
Akhil Rajan <rajanakhil07@gmail.com>
2022-05-30 22:26:09 +0530

    Time: 600 ms (17.16%), Space: 72.7 MB (28.59%) - LeetHub

--- 815-bus-routes/815-bus-routes.cpp
+++ 815-bus-routes/815-bus-routes.cpp
@@ -1,66 +1,63 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-
-        unordered_map<int, vector<int>> mp; // bus stop no -> buses
-
-        int n = routes.size(); // no of buses
-
+        
+        //mapping of bus_stop_no with bus_no
+        unordered_map<int, vector<int>> m;
+        int n = routes.size();
         for(int i=0; i<n; i++){
             for(int j=0; j<routes[i].size(); j++){
-                int busstopno = routes[i][j];
-                int busno = i;
-                mp[busstopno].push_back(busno);
+               int bus_stop_no = routes[i][j];
+               int bus_no = i;
+               m[bus_stop_no].push_back(bus_no);
             }
         }
 
-        queue<int> q;
-        unordered_set<int> busStopVis;
-        unordered_set<int> busNoVis;
+        unordered_set<int> bus_stop_no_vis;
+        unordered_set<int> bus_no_vis;
 
+        queue<int> q; // bus_stop_no
         q.push(source);
-        busStopVis.insert(source);
+        bus_stop_no_vis.insert(source);
 
-        int level=0;
+        int level=-1;
 
         while(q.size()>0){
 
             int size = q.size();
+            level++;
 
             for(int i=0; i<size; i++){
 
-                int rem = q.front(); q.pop();
+                auto rem = q.front();
+                q.pop();
+
+                vector<int> buses = m[rem];
 
                 if(rem == target) return level;
 
-                vector<int> buses = mp[rem];
-
                 for(int bus : buses){
 
-                    if(busNoVis.find(bus) != busNoVis.end()){
+                    if(bus_no_vis.find(bus) != bus_no_vis.end()){
                         continue;
                     }
 
                     vector<int> route = routes[bus];
 
-                    for(int busstop : route){
+                    for(int bus_stop : route){
 
-                        if(busStopVis.find(busstop) != busStopVis.end()){
+                        if(bus_stop_no_vis.find(bus_stop) != bus_stop_no_vis.end()){
                             continue;
                         }
 
-                        q.push(busstop);
-                        busStopVis.insert(busstop);
+                        q.push(bus_stop);
+                        bus_stop_no_vis.insert(bus_stop);
                     }
-
-                    busNoVis.insert(bus);
-
+                    bus_no_vis.insert(bus);
                 }
             }
-
-            level++;
         }
 
         return -1;
-}
+    }
 };
