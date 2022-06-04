969cd50a4e6e9fec5f64800676baa2e0c7a5b863
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-04 11:59:12 +0530

    Time: 684 ms (85.80%), Space: 169.6 MB (84.68%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,65 +1,54 @@
 class Solution {
 public:
-    
-    void helperUtil(vector<int> graph[], int src, int par, vector<bool>& vis, vector<int>& disc, vector<int>& low, int t, vector<vector<int>>& ans){
-        
+    void dfs(vector<int> adj[], int src, int par, vector<bool>& vis, vector<int>& disc, vector<int>& low,
+    int& t, vector<vector<int>>& servers){
+
         disc[src] = low[src] = t;
-        vis[src] = true;
         t++;
-        
-        for(int nbr : graph[src]){
-            
-            if(nbr == par){
-                continue;
-            }
-            
+
+        vis[src] = true;
+
+        for(int nbr : adj[src]){
+
+            if(par == nbr) continue;
+
             else if(vis[nbr] == true){
                 low[src] = min(low[src], disc[nbr]);
             }
-            
+
             else{
-                helperUtil(graph, nbr, src, vis, disc, low, t, ans);
+                dfs(adj, nbr, src, vis, disc, low, t, servers);
                 low[src] = min(low[src], low[nbr]);
-                
+
                 if(low[nbr] > disc[src]){
-                    vector<int> edge;
-                    edge.push_back(src);
-                    edge.push_back(nbr);
-                    ans.push_back(edge);
+                    vector<int> edges;
+                    edges = {nbr, src};
+                    servers.push_back(edges);
                 }
             }
         }
-        
     }
-    
-    vector<vector<int>> findBridge(vector<int> graph[], int n){
-        
-        vector<vector<int>> ans;
-        
-        vector<int> disc(n), low(n);
-        vector<bool> vis(n, false);
-        
-        int t=0;
-        int par=-1;
-        int src=0;
-        
-        helperUtil(graph, src, par, vis, disc, low, t, ans);
-        return ans;
-    }
-    
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        
-        vector<int> graph[n];
-        
+
+        vector<int> adj[n];
         for(int i=0; i<connections.size(); i++){
             int u = connections[i][0];
             int v = connections[i][1];
-            
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
         
-        vector<vector<int>> ans = findBridge(graph, n);
-        return ans;
+        vector<vector<int>> servers;
+
+        vector<bool> vis(n, false);
+        vector<int> disc(n, 0), low(n, 0);
+
+        int par=-1;
+        int t=0;
+
+        dfs(adj, 0, par, vis, disc, low, t, servers);
+
+        return servers;
     }
 };
