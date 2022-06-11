4b6f2a0287a755a4d22076852ca7eae56e1457b6
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-11 14:06:13 +0530

    Time: 816 ms (14.13%), Space: 168.3 MB (14.25%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        
+        int totalSum=0;
+        for(auto el : nums){
+            totalSum += el;
+        }
+        
+        int target = totalSum-x;
+        
+        unordered_map<int, int> m;
+        m[0]=-1;
+        
+        int maxLenSubArray=-1;
+        
+        int prefixSum=0;
+        for(int i=0; i<nums.size(); i++){
+            
+            prefixSum += nums[i];
+            
+            m[prefixSum] = i;
+            
+            if(m.find(prefixSum - target) != m.end()){
+                int curLenSubArray = i - m[prefixSum - target];
+                maxLenSubArray = max(maxLenSubArray, curLenSubArray);
+            }
+        }
+        
+        if(maxLenSubArray == -1) return -1;
+        
+        return nums.size() - maxLenSubArray;
+    }
+};
