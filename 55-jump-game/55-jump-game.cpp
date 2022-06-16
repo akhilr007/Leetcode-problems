801cfabed102ab9844a8117a30c22484ac7c98b7
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-16 14:07:36 +0530

    Time: 2026 ms (5.00%), Space: 51.4 MB (5.74%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    bool f(int i, vector<int>& nums, int n, vector<int>& dp){
         
-        int reachable =0;
-        for(int i=0; i<nums.size(); i++){
-            if(reachable < i) return false;
+        if(i==n-1) return true;
+        
+        if(dp[i] != -1) return dp[i];
+        
+        bool ans = false;    
+        for(int j=1; j<=nums[i] && j<n; j++){
+            ans = ans + f(i+j, nums, n, dp);
             
-            reachable = max(reachable, i+nums[i]);
+            if(ans) break;
         }
         
-        return true;
+        return dp[i] = ans;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector<int> dp(n, -1);
+        return f(0, nums, n, dp);
     }
 };
