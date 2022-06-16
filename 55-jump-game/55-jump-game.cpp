73bffeb2183e05ae66887cfb447ed30986c76661
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-16 14:09:52 +0530

    Time: 1998 ms (5.00%), Space: 51.2 MB (5.74%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,5 +1,6 @@
 class Solution {
-public:
+private:
+    // TC -> O(n) SC -> O(n) memoization
     bool f(int i, vector<int>& nums, int n, vector<int>& dp){
         
         if(i==n-1) return true;
@@ -8,13 +9,19 @@ public:
         
         bool ans = false;    
         for(int j=1; j<=nums[i] && j<n; j++){
-            ans = ans + f(i+j, nums, n, dp);
+            ans = ans | f(i+j, nums, n, dp);
             
             if(ans) break;
         }
         
         return dp[i] = ans;
     }
+/*    
+    vector<int> dp(n, -1);
+    return f(0, nums, n, dp);
+*/
+public:
+    
     
     bool canJump(vector<int>& nums) {
         
