c3cd60f867c32d9ab48895be21ec9d0fd57d097a
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-26 11:58:13 +0530

    Time: 8 ms (55.00%), Space: 7.2 MB (90.01%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
+    bool alphaNum(char c){
+        
+        if ( (c >= 'A' and c <= 'Z') ||
+             (c >= 'a' and c <= 'z') ||
+             (c >= '0' and c <= '9')){
+            return true;
+        }
+        return false;
+    }
+    
     bool isPalindrome(string s) {
         
-        string str = "";
-        for(auto c : s){
-            
-            c = tolower(c);
-            if(isdigit(c)  or (c >= 'a' && c <= 'z')){
-                str += c;
-            }
-        }
+        int l=0;
+        int r=s.length()-1;
         
-        int i=0;
-        int j=str.length()-1;
-        
-        while(i<j){
-            if(str[i] != str[j]){
-                return false;
-            }
+        while(l<r){
             
-            i++;
-            j--;
+            while(l<r and alphaNum(s[l])==false) l++;
+            while(l<r and alphaNum(s[r])==false) r--;
+            
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            l++;
+            r--;
         }
         
         return true;
