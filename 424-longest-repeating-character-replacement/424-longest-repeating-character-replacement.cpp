9b44ac20d573e176811126a019346b0e941128aa
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-27 20:58:11 +0530

    Time: 17 ms (58.32%), Space: 7.1 MB (14.49%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -15,11 +15,13 @@ public:
         unordered_map<char, int> mp;
         int res=0;
         int l=0;
+        int maxF=0;
         
         for(int r=0; r<s.length(); r++){
             mp[s[r]]++;
+            maxF = max(maxF, mp[s[r]]);
             
-            while(r-l+1 - maxCountInMap(mp) > k){
+            while(r-l+1 - maxF > k){
                 mp[s[l]]--;
                 l++;
             }
