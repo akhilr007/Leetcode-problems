96a5f2966d22d66dd6a38dbe4a7af91dad4765e7
Akhil Rajan <rajanakhil07@gmail.com>
2022-06-28 00:01:50 +0530

    Time: 249 ms (26.76%), Space: 14.7 MB (35.73%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        freq = [0]*256
+        
+        count = 0
+        for c in t:
+            if freq[ord(c) - ord("a")] == 0:
+                count += 1
+            freq[ord(c) - ord("a")] += 1
+        
+        l, r = 0, 0
+        res = 1000000
+        start, end = 0, 0
+        
+        while r < len(s):
+            
+            freq[ord(s[r]) - ord("a")] -= 1
+            
+            if freq[ord(s[r]) - ord("a")] == 0:
+                count -= 1
+              
+            if count == 0:
+                while count == 0:
+                    if res > r-l+1:
+                        res = r-l+1
+                        start = l
+                        end = start + r-l+1
+                    
+                    freq[ord(s[l]) - ord("a")] += 1
+                    if freq[ord(s[l]) - ord("a")] == 1:
+                        count += 1
+                    l += 1
+                    
+            r += 1
+        
+        print(end)
+        if res == 1000000:
+            return ""
+    
+        ans = ""
+        for i in range(start, end):
+            ans += s[i]
+
+        return ans
+        
