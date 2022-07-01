b6c53ace23bafa7fa14edd0c5d962a83fc9d85cb
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-01 20:16:30 +0530

    Time: 371 ms (16.31%), Space: 76.3 MB (77.38%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,78 +1,56 @@
-class Solution{
+class Solution {
 public:
-    vector<int> nextSmallerToLeft(vector<int>& heights){
-
-        int n=heights.size();
-        vector<int> nsl(n);
-
+    int largestRectangleArea(vector<int>& heights) {
+        
+        int n = heights.size();
+        int nsl[n];
+        
         stack<int> st;
-
-        nsl[0] = -1;
-        st.push(0);
-
-        for(int i=1; i<heights.size(); i++){
-
-            while(st.size()>0 and heights[st.top()] >= heights[i]){
+        
+        for(int i=0; i<n; i++){
+            
+            while(st.size() > 0 && heights[st.top()] >= heights[i]){
                 st.pop();
             }
-
+            
             if(st.size() == 0){
                 nsl[i] = -1;
             }
             else{
                 nsl[i] = st.top();
             }
-
             st.push(i);
         }
-
-        return nsl;
-    }
-
-    vector<int> nextSmallerToRight(vector<int>& heights){
-
-        int n=heights.size();
-        vector<int> nsr(n);
-
-        stack<int> st;
-
-        nsr[n-1] = n;
-        st.push(n-1);
-
-        for(int i=n-2; i>=0; i--){
-
-            while(st.size()>0 and heights[st.top()] >= heights[i]){
+        
+        int nsr[n];
+        while(st.size()>0) st.pop();
+        
+        for(int i=n-1; i>=0; i--){
+            
+            while(st.size() > 0 && heights[st.top()] >= heights[i]){
                 st.pop();
             }
-
+            
             if(st.size() == 0){
                 nsr[i] = n;
             }
             else{
                 nsr[i] = st.top();
             }
-
             st.push(i);
         }
-
-        return nsr;
-    }
-
-    int largestRectangleArea(vector<int>& heights){
-
-        vector<int> nsl = nextSmallerToLeft(heights);
-        vector<int> nsr = nextSmallerToRight(heights);
-
-        int maxArea=0;
-        for(int i=0; i<heights.size(); i++){
+        
+        int maxArea = 0;
+        for(int i=0; i<n; i++){
             
-            int height = heights[i];
             int width = nsr[i] - nsl[i] - 1;
-
-            maxArea = max(height*width, maxArea);
+            int length = heights[i];
+            
+            int cur_area = length * width;
+            
+            maxArea = max(maxArea, cur_area);
         }
-
+        
         return maxArea;
-
     }
 };
