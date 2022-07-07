259f459ae1e7e5a7bcaae7fbda58ac8b813ee631
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-07 14:39:37 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (54.34%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool f(int i, int j, int k, string& s1, string& s2, string& s3, 
-          vector<vector<vector<int>>>& dp){
+    bool f(int i, int j, string& s1, string& s2, string& s3, 
+           vector<vector<int>>& dp){
         
-        if(k == 0 && i == 0 && j == 0) return true;
+        if(i == 0 && j == 0) return true;
         
-        if(dp[i][j][k] != -1){
-            return dp[i][j][k];
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
         
-        if(i>0 && k>0 && s3[k-1] == s1[i-1]){
-            if(j>0 && k>0 && s3[k-1] == s2[j-1]){
-                return dp[i][j][k] = f(i-1, j, k-1, s1, s2, s3, dp) || f(i, j-1, k-1, s1, s2, s3, dp);
+        if(i>0 && s3[i+j-1] == s1[i-1]){
+            if(j>0 && s3[i+j-1] == s2[j-1]){
+                return dp[i][j] = f(i-1, j, s1, s2, s3, dp) || f(i, j-1, s1, s2, s3, dp);
             }
-            else return dp[i][j][k] = f(i-1, j, k-1, s1, s2, s3, dp);
+            else return dp[i][j] = f(i-1, j, s1, s2, s3, dp);
         }
         
-        if(j>0 && k>0 && s3[k-1] == s2[j-1]){
-            return dp[i][j][k] = f(i, j-1, k-1, s1, s2, s3, dp);
+        if(j>0 && s3[i+j-1] == s2[j-1]){
+            return dp[i][j] = f(i, j-1, s1, s2, s3, dp);
         }
         else{
-            return dp[i][j][k] = false;
+            return dp[i][j] = false;
         }
     }
     
@@ -38,7 +38,7 @@ public:
             return s1 == s3;
         }
         
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (m+1, vector<int>(o+1, -1)));
-        return f(n, m, o, s1, s2, s3, dp);
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        return f(n, m, s1, s2, s3, dp);
     }
 };
