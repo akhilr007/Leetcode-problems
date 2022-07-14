4cbbd0e287c0a1122c67262d54d2352abb5e1c96
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-14 22:48:46 +0530

    Time: 4 ms (90.36%), Space: 8 MB (31.35%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,55 +1,51 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board, int row, int n, vector<bool>& cols, vector<bool>& diagonal, vector<bool>& reverseDiagonal, vector<vector<string>>& res){
+    void solve(vector<vector<char>>& board, int row, int n, vector<bool>& cols, vector<bool>& pdiagonals, vector<bool>& rdiagonals, vector<vector<string>>& result){
         
         if(row == n){
+            
             vector<string> v;
-            for(int i=0; i<n; i++){
-                string s="";
-                for(int j=0; j<n; j++){
+            for(int i=0; i<board.size(); i++){
+                string s = "";
+                for(int j=0; j<board[0].size(); j++){
                     s += board[i][j];
                 }
                 v.push_back(s);
             }
             
-            res.push_back(v);
-            
+            result.push_back(v);
             return;
         }
         
         for(int col=0; col<n; col++){
-            if(cols[col] == false && diagonal[row+col]==false && 
-               reverseDiagonal[row-col+n-1] == false)
-            {
+            if(cols[col] == false && pdiagonals[row+col] == false && rdiagonals[row-col+n-1] == false){
                 board[row][col] = 'Q';
                 
-                cols[col]=true;
-                diagonal[row+col]=true;
-                reverseDiagonal[row-col+n-1]=true;
+                cols[col] = true;
+                pdiagonals[row + col] = true;
+                rdiagonals[row - col + n - 1] = true;
                 
-                solve(board, row+1, n, cols, diagonal, reverseDiagonal, res);
+                solve(board, row+1, n, cols, pdiagonals, rdiagonals, result);
                 
-                cols[col]=false;
-                diagonal[row+col]=false;
-                reverseDiagonal[row-col+n-1]=false;
+                cols[col] = false;
+                pdiagonals[row + col] = false;
+                rdiagonals[row - col + n - 1] = false;
                 
                 board[row][col] = '.';
-                
-            }
+             }
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
         
-        vector<vector<char>> board(n, vector<char>(n , '.'));
-        vector<vector<string>> res;
+        vector<vector<char>> board(n, vector<char> (n, '.'));
+        vector<vector<string>> result;
         
         vector<bool> cols(n, false);
-        vector<bool> diagonal(2*n-1, false);
-        vector<bool> reverseDiagonal(2*n-1, false);
+        vector<bool> pdiagonals(2*n-1, false);
+        vector<bool> rdiagonals(2*n-1, false);
         
-        solve(board, 0, n, cols, diagonal, reverseDiagonal, res);
-            
-        return res;
+        solve(board, 0, n, cols, pdiagonals, rdiagonals, result);
+        return result;
     }
 };
