aec7a3c0879eea464a42107f11d05aa89a2dc12d
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-16 13:23:43 +0530

    Time: 61 ms (65.49%), Space: 17.8 MB (72.55%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>& heights, vector<vector<bool>>& vis, int prevHeight){
+    void dfs(int r, int c, vector<vector<int>>& heights, int prevHeight, vector<vector<bool>>& vis){
         
-        if(r<0 || c<0 || r>=heights.size() || c>=heights[0].size() || heights[r][c] < prevHeight || vis[r][c]==true){
+        if(r<0 || c<0 || r>=heights.size() || c>=heights[0].size() || vis[r][c] == true || heights[r][c] < prevHeight){
             return;
         }
         
         vis[r][c] = true;
-        
-        dfs(r-1, c, heights, vis, heights[r][c]);
-        dfs(r, c-1, heights, vis, heights[r][c]);
-        dfs(r+1, c, heights, vis, heights[r][c]);
-        dfs(r, c+1, heights, vis, heights[r][c]);
+        dfs(r-1, c, heights, heights[r][c], vis);
+        dfs(r, c-1, heights, heights[r][c], vis);
+        dfs(r+1, c, heights, heights[r][c], vis);
+        dfs(r, c+1, heights, heights[r][c], vis);
         
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+        
+        vector<vector<bool>> pac(n, vector<bool>(m, false));
+        vector<vector<bool>> atl(n, vector<bool>(m, false));
+        
+        for(int r=0; r<n; r++){
+            dfs(r, 0, heights, heights[r][0], pac);
+            dfs(r, m-1, heights, heights[r][m-1], atl);
+        }
+        
+        for(int c=0; c<m; c++){
+            dfs(0, c, heights, heights[0][c], pac);
+            dfs(n-1, c, heights, heights[n-1][c], atl);
+        }
         
         vector<vector<int>> res;
-        
-        int rows = heights.size();
-        int cols = heights[0].size();
-        
-        vector<vector<bool>> pac(rows, vector<bool> (cols, false));
-        vector<vector<bool>> atl(rows, vector<bool> (cols, false));
-        
-        for(int c=0; c<cols; c++){
-            dfs(0, c, heights, pac, heights[0][c]);
-            dfs(rows-1, c, heights, atl, heights[rows-1][c]);
-        }
-        
-        for(int r=0; r<rows; r++){
-            dfs(r, 0, heights, pac, heights[r][0]);
-            dfs(r, cols-1, heights, atl, heights[r][cols-1]);
-        }
-        
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(pac[i][j]==true && atl[i][j]==true){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(pac[i][j] && atl[i][j]){
                     vector<int> v = {i, j};
                     res.push_back(v);
                 }
