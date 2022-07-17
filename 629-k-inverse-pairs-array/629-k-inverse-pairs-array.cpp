aecb2bd851f504ef767fd091a25f489b5816f344
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-17 14:53:32 +0530

    Time: 109 ms (26.92%), Space: 16.3 MB (31.41%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -42,7 +42,7 @@ public:
         for(int i=1; i<=n; i++){
             for(int j=1; j<=k; j++){
                 dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod;
-                if(j-i>=0){
+                if(j>=i){
                     dp[i][j] = (dp[i][j] - dp[i-1][j-i] + mod)%mod;
                 }
             }
