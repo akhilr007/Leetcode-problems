2cbcc2f6202ef3594f816e7e1e4c5ce92343d45b
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-19 14:49:26 +0530

    Time: 2044 ms (7.84%), Space: 27.3 MB (28.73%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        
+        queue<string> q;
+        q.push(beginWord);
+        
+        int ans=1;
+        
+        while(!q.empty()){
+            
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                
+                string word = q.front();
+                q.pop();
+                
+                if(word == endWord) return ans;
+                
+                dict.erase(word);
+                
+                for(int j=0; j<word.size(); j++){
+                    
+                    char c = word[j];
+                    
+                    for(int k=0; k<26; k++){
+                        
+                        word[j] = 'a' + k;
+                        if(dict.find(word) != dict.end()){
+                            q.push(word);
+                        }
+                    }
+                    
+                    word[j] = c;
+                }
+            }
+            
+            ans++;
+        }
+        
+        return 0;
+    }
+};
