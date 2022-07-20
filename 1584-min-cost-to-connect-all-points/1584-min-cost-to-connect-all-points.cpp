ab250292d28c31b9aff5f1fb82cb953ab6fa0c46
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-20 19:11:44 +0530

    Time: 813 ms (41.80%), Space: 58.1 MB (65.20%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,82 +1,82 @@
-class Solution {
+class UnionFind{
+private:
+    vector<int> rank, parent;
 public:
-    static bool comp(const pair<int, pair<int, int>>& a, const pair<int, pair<int, int>>& b){
-        return a.first < b.first;
-    }
-    
-    int find(int x, vector<int>& parent){
-        
-        if(x == parent[x]){
-            return x;
+    UnionFind(int size) : rank(size),  parent(size){
+        for(int i=0; i<size; i++){
+            rank[i] = 0;
+            parent[i] = i;
         }
-        
-        int sl = find(parent[x], parent);
-        parent[x] = sl;
-        return sl;
     }
     
-    bool dsu(int u, int v, vector<int>& parent, vector<int>& rank){
+    int find(int x){
         
-        int sl1 = find(u, parent);
-        int sl2 = find(v, parent);
+        if(parent[x] == x) return x;
         
-        if(sl1 != sl2){
-            cout <<u << " "<<v<<endl;
-            if(rank[sl1] < rank[sl2]){
-                parent[sl1] = sl2;
+        int temp = find(parent[x]);
+        parent[x] = temp;
+        return temp;
+    }
+    
+    bool unionSet(int x, int y){
+        
+        int slx = find(x);
+        int sly = find(y);
+        
+        if(slx != sly){
+            if(rank[slx] > rank[sly]){
+                parent[sly] = slx;
             }
-            else if(rank[sl2] < rank[sl1]){
-                parent[sl2] = sl1;
+            else if(rank[sly] > rank[slx]){
+                parent[slx] = sly;
             }
             else{
-                parent[sl1] = sl2;
-                rank[sl2]++;
+                parent[sly] = slx;
+                rank[slx]++;
             }
+            
             return true;
         }
         else{
             return false;
         }
     }
-    
+};
+
+class Solution {
+public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        // using kruskal
-        vector<pair<int, pair<int, int>>> dist;
-        // to store cost of all points
-        // like i->j where i is [0, 0] and j is [2, 2]
         
-        int n=points.size();
+        int n = points.size();
+        vector<pair<int, pair<int, int>> > dist;
         
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 
-                int cost=abs(points[j][0] - points[i][0]) + abs(points[j][1] - points[i][1]);
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
+                
+                int cost = abs(x1-x2) + abs(y1-y2);
                 dist.push_back({cost, {i, j}});
             }
         }
         
-        // sort the edges by cost
-        sort(dist.begin(), dist.end(), comp);
+        sort(dist.begin(), dist.end());
         
-        // for disjoin set union make parent and rank
-        vector<int> parent(n), rank(n, 0);
-        for(int i=0; i<n; i++){
-            parent[i]=i;
-        }
-        
+        UnionFind uf(n);
         int minCost=0;
+        
         for(int i=0; i<dist.size(); i++){
             
             int u = dist[i].second.first;
             int v = dist[i].second.second;
             int wt = dist[i].first;
             
-            if(dsu(u, v, parent, rank) == true){
+            if(uf.unionSet(u, v)){
                 minCost += wt;
             }
         }
         
         return minCost;
-        
     }
 };
