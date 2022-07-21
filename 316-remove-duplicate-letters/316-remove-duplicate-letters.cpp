b9e1ee5cf8701e2963106fa530d2eb52f8493d33
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-21 14:10:32 +0530

    Time: 4 ms (64.36%), Space: 6.9 MB (42.14%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -3,41 +3,39 @@ public:
     string removeDuplicateLetters(string s) {
         
         int n = s.length();
-        
         vector<bool> visit(26, false);
         vector<int> lastIndex(26, 0);
         
+        // 1. set last indexes for every char
         for(int i=0; i<n; i++){
-            lastIndex[s[i] - 'a'] = i;
+            lastIndex[s[i]-'a'] = i;
         }
         
         stack<char> st;
+        
+        // 2. iterate over string s
         for(int i=0; i<n; i++){
             
-            char c = s[i];
+            if(visit[s[i]-'a']) continue;
             
-            if(visit[c-'a']) continue;
+            visit[s[i] - 'a'] = true;
             
-            visit[c-'a'] = true;
-            
-            while(st.size() > 0 and (st.top() > s[i]) and lastIndex[st.top()-'a'] > i){
+            while(st.size()>0 && (st.top() > s[i]) && lastIndex[st.top()-'a'] > i){
                 char ch = st.top();
                 st.pop();
                 visit[ch-'a'] = false;
             }
             
-            st.push(c);
+            st.push(s[i]);
         }
         
         string res="";
-        
-        while(st.size()>0){
+        while(!st.empty()){
             res += st.top();
             st.pop();
         }
         
         reverse(res.begin(), res.end());
         return res;
-        
     }
 };
