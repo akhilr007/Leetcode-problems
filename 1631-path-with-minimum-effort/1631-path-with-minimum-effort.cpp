ac6615c7aa5be96b90e1392bdb271e59b2b9b1db
Akhil Rajan <rajanakhil07@gmail.com>
2022-07-28 14:04:05 +0530

    Time: 305 ms (59.37%), Space: 20 MB (79.48%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,63 +1,44 @@
 class Solution {
 public:
-    bool dfs(int i, int j, vector<vector<int>>& heights, vector<vector<bool>>& vis, int mid, int n, int m)     
-    {
-        
-        vis[i][j] = true;
-        
-        if(i==n && j==m){
-            return true;
-        }
-        
-        bool op = false;
-        
-        if(i>0 && vis[i-1][j] == false && abs(heights[i][j] - heights[i-1][j]) <= mid){
-            op = op || dfs(i-1, j, heights, vis, mid, n, m);
-        }
-        if(j>0 && vis[i][j-1] == false && abs(heights[i][j] - heights[i][j-1]) <= mid){
-            op = op || dfs(i, j-1, heights, vis, mid, n, m);
-        }
-        if(i<n && vis[i+1][j] == false && abs(heights[i][j] - heights[i+1][j]) <= mid){
-            op = op || dfs(i+1, j, heights, vis, mid, n, m);
-        }
-        if(j<m && vis[i][j+1] == false && abs(heights[i][j] - heights[i][j+1]) <= mid){
-            op = op || dfs(i, j+1, heights, vis, mid, n, m);
-        }
-        
-        return op;
-    }
-    
     int minimumEffortPath(vector<vector<int>>& heights) {
         
-        int maxi = INT_MIN;
+        int n = heights.size();
+        int m = heights[0].size();
         
-        for(int i=0; i<heights.size(); i++){
-            for(int j=0; j<heights[0].size(); j++){
-                maxi = max(maxi, heights[i][j]);
+        vector<vector<int>> efforts(n, vector<int> (m, INT_MAX));
+        efforts[0][0] = 0;
+        
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        pq.push(make_tuple(0, 0, 0)); // effort, row, col
+        
+        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        
+        while(pq.size()>0){
+            
+            auto [currEffort, row, col] = pq.top();
+            pq.pop();
+            
+            if(row == n-1 and col == m-1){
+                return currEffort;
+            }
+            
+            for(int d=0; d<4; d++){
+                int r = row + dir[d][0];
+                int c = col + dir[d][1];
+                
+                if(r >= 0 and c >= 0 and r < n and c < m){
+                    
+                    int newEffort = max(currEffort, 
+                                        abs(heights[r][c] - heights[row][col]));
+                    
+                    if(newEffort < efforts[r][c]){
+                        efforts[r][c] = newEffort;
+                        pq.push(make_tuple(newEffort, r, c));
+                    }
+                }
             }
         }
         
-        int start=0;
-        int end=maxi;
-        
-        int n=heights.size();
-        int m=heights[0].size();
-        
-        int ans=INT_MAX;
-        while(start <= end){
-            
-            vector<vector<bool>> vis(n, vector<bool>(m, false));
-            int mid = start + (end-start)/2;
-            
-            if(dfs(0, 0, heights, vis, mid, n-1, m-1)){
-                ans = mid;
-                end = mid-1;
-            }
-            else{
-                start = mid+1;
-            }
-        }
-        
-        return ans;
+        return -1;
     }
 };
