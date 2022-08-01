e28af060d5a37220aa059028fd94064a6567ce67
Akhil Rajan <rajanakhil07@gmail.com>
2022-08-01 13:50:36 +0530

    Time: 96 ms (69.59%), Space: 19.1 MB (98.28%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,30 +2,42 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         
-        if(grid[0][0] != 0) return -1;
+        int n = grid.size();
+        if(grid[0][0] == 1 or grid[n-1][n-1] == 1) return -1;
+        
         queue<pair<int, int>> q;
-        q.push(make_pair(0, 0));
-        grid[0][0]=1;
+        q.push({0, 0});
+        grid[0][0] = 2;
         
-        int dir[8][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        int dir[8][2] = {{-1, 0}, {0, -1}, {-1, -1}, {1, -1}, {1, 0}, {0, 1}, {1, 1}, {-1, 1}};
         
-        while(q.size()>0){
+        int level = 0;
+        while(!q.empty()){
             
-            auto curr = q.front(); q.pop();
-            int r=curr.first;
-            int c=curr.second;
-            
-            if(r == grid.size()-1 and c == grid.size()-1) return grid[r][c];
-            
-            for(int d=0; d<8; d++){
-                int nr = r + dir[d][0];
-                int nc = c + dir[d][1];
+            int size = q.size();
+            for(int i=0; i<size; i++){
                 
-                if(nr >=0 && nc >= 0 && nr < grid.size() &&nc<grid.size() && grid[nr][nc]==0){
-                    q.push(make_pair(nr, nc));
-                    grid[nr][nc] = grid[r][c] + 1;
+                auto head = q.front(); q.pop();
+                
+                int row = head.first;
+                int col = head.second;
+                
+                if(row == n-1 && col == n-1){
+                    return level + 1;
+                }
+                
+                for(int d=0; d<8; d++){
+                    
+                    int r = row + dir[d][0];
+                    int c = col + dir[d][1];
+                    
+                    if(r >= 0 && c>=0 && r < n && c < n && grid[r][c] == 0){
+                        q.push({r, c});
+                        grid[r][c] = 2;
+                    }
                 }
             }
+            level++;
         }
         
         return -1;
