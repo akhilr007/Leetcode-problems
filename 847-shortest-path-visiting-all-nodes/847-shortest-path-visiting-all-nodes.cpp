ab554c9ddf50e42f8ce41fb654f1089997ab4eec
Akhil Rajan <rajanakhil07@gmail.com>
2022-08-09 14:48:43 +0530

    Time: 36 ms (75.84%), Space: 12.4 MB (71.78%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
         
-        if(graph.size() == 1) return 0;
-        
-        int finalState = (1 << graph.size()) - 1;
+        int n = graph.size();
+        if(n == 1) return 0;
+        int finalState = (1 << n) - 1;
         
         queue<tuple<int, int>> q;
-        for(int i=0; i<graph.size(); i++){
-            q.push(make_tuple(i, 1<<i));
+        
+        for(int i=0; i<n; i++){
+            q.push(make_tuple(i, (1<<i)));
         }
         
-        vector<vector<int>> vis(graph.size(), vector<int>(finalState+1, 0));
+        vector<vector<int>> vis(n, vector<int> (finalState + 1, 0));
         
-        int shortestPath=0;
+        int shortestPath = 0;
         while(!q.empty()){
-            
             int size = q.size();
             shortestPath++;
+            
             for(int i=0; i<size; i++){
                 
                 auto [node, visState] = q.front();
                 q.pop();
                 
-                for(auto nbr : graph[node]){
-                    int newVisState = visState | (1 << nbr);
+                for(auto nbr: graph[node]){
+                    
+                    int newVisState = visState | (1<<nbr);
                     
                     if(vis[nbr][newVisState] == 1) continue;
                     vis[nbr][newVisState] = 1;
@@ -32,10 +34,8 @@ public:
                     if(newVisState == finalState) return shortestPath;
                     
                     q.push(make_tuple(nbr, newVisState));
-                    
                 }
             }
-            
         }
         
         return -1;
