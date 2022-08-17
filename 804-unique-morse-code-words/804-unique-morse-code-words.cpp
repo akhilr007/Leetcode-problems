c56dbc47004b592eee69c71836354adfa0803d0b
Akhil Rajan <rajanakhil07@gmail.com>
2022-08-17 12:11:52 +0530

    Time: 4 ms (71.25%), Space: 8.5 MB (88.09%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -2,39 +2,13 @@ class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
         
-        unordered_map<char, string> morseCode;
-        morseCode['a'] = ".-";
-        morseCode['b'] = "-...";
-        morseCode['c'] = "-.-.";
-        morseCode['d'] = "-..";
-        morseCode['e'] = ".";
-        morseCode['f'] = "..-.";
-        morseCode['g'] = "--.";
-        morseCode['h'] = "....";
-        morseCode['i'] = "..";
-        morseCode['j'] = ".---";
-        morseCode['k'] = "-.-";
-        morseCode['l'] = ".-..";
-        morseCode['m'] = "--";
-        morseCode['n'] = "-.";
-        morseCode['o'] = "---";
-        morseCode['p'] = ".--.";
-        morseCode['q'] = "--.-";
-        morseCode['r'] = ".-.";
-        morseCode['s'] = "...";
-        morseCode['t'] = "-";
-        morseCode['u'] = "..-";
-        morseCode['v'] = "...-";
-        morseCode['w'] = ".--";
-        morseCode['x'] = "-..-";
-        morseCode['y'] = "-.--";
-        morseCode['z'] = "--..";
+        string codes[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         
         unordered_map<string, int> codeTransformation;
-        for(int i=0; i<words.size(); i++){
+        for(auto word : words){
             string str = "";
-            for(int j=0; j<words[i].length(); j++){
-                str += morseCode[words[i][j]];
+            for(auto ch : word){
+                str += codes[ch-'a'];
             }
             codeTransformation[str]++;
         }
