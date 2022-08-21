d91accc6e7e07500ca39cb2ce10599c6b4323a5b
Akhil Rajan <rajanakhil07@gmail.com>
2022-08-21 23:12:32 +0530

    Time: 59 ms (50.00%), Space: 7.6 MB (68.03%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.cpp
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -4,44 +4,40 @@ public:
     void update(string& curr, int i, string& stamp){
         
         for(int j=0; j<stamp.length(); j++){
-            curr[j+i] = '*';
+            curr[i+j] = '*';
         }
     }
     
-    int fetchStampIndex(string& curr, string& stamp){
+    int findStampIndex(string& curr, string& stamp){
         
-        
-        for(int i=0; i<=curr.length() - stamp.length();i++){
+        for(int i=0; i<=curr.length()-stamp.length(); i++){
             
-            int j=0; // for stamp 
+            int j=0; // for stamp
             int s = i; // for curr
-            bool isNotStarChar = false;
+            bool notStarChar = false;
             
-            while(j<stamp.length() && s<curr.length() && (curr[s] == '*' || curr[s] == stamp[j])){
-                if(curr[s] != '*') isNotStarChar = true;
+            while(j < stamp.length() && s < curr.length() && (curr[s] == '*' || curr[s] == stamp[j])){
+                if(curr[s] != '*') notStarChar = true;
                 s++;
                 j++;
             }
             
-            if(j==stamp.length() && isNotStarChar){
+            if(j == stamp.length() and notStarChar){
                 return i;
             }
         }
         
         return -1;
     }
-    
     vector<int> movesToStamp(string stamp, string target) {
         
         vector<int> res;
-        
         string curr = target;
         string targetStr(target.length(), '*');
         
-        
         while(curr != targetStr){
             
-            int stampIndex = fetchStampIndex(curr, stamp);
+            int stampIndex = findStampIndex(curr, stamp);
             if(stampIndex < 0){
                 vector<int> v;
                 return v;
