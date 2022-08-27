0ba2901b116bfc198349769b2842f9171e9e89b9
Akhil Rajan <rajanakhil07@gmail.com>
2022-08-27 14:52:09 +0530

    Time: 2314 ms (12.46%), Space: 293.9 MB (34.00%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -3,8 +3,8 @@ public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
         
         int res = INT_MIN;
-        int rows = matrix.size();
         int cols = matrix[0].size();
+        int rows = matrix.size();
         
         for(int l=0; l<cols; l++)
         {
@@ -19,15 +19,13 @@ public:
                 set<int> s = {0};
                 int cum_sum = 0;
                 
-                for(auto sum : sums)
+                for(int sum : sums) 
                 {
                     cum_sum += sum;
                     
                     auto it = s.lower_bound(cum_sum - k);
-                    if(it != s.end()){
-                        res = max(res, cum_sum - *it);
-                    }
-                    
+                
+                    if(it != s.end()) res = max(res, cum_sum - *it);
                     s.insert(cum_sum);
                 }
             }
