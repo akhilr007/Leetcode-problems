74c025c7b745889a20846b2a7e945ced105c7882
Akhil Rajan <rajanakhil07@gmail.com>
2022-09-10 13:59:53 +0530

    Time: 323 ms (62.30%), Space: 0B (100.00%) - LeetHub

---
+++ 596-classes-more-than-5-students/596-classes-more-than-5-students.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+WITH cte AS (SELECT class, COUNT(student) AS student_count
+FROM Courses
+GROUP BY class)
+
+SELECT class 
+FROM cte
+WHERE student_count >= 5;
