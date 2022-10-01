d1e8a168d737cc076cfcd52ab07c8465391ebfa5
Akhil Rajan <rajanakhil07@gmail.com>
2022-10-01 13:13:35 +0530

    Time: 4 ms (47.86%), Space: 6.3 MB (28.47%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int helper(string& s, int n, vector<int>& dp){
+        
+        if(n == 0){
+            return 1;
+        }
+        
+        if(dp[n-1] != -1) return dp[n];
+        
+        int count = 0;
+        if(s[n-1] != '0'){
+            count += helper(s, n-1, dp);
+        }
+        
+        if(n>1 && stoi(s.substr(n-2, 2)) >= 10 && stoi(s.substr(n-2, 2)) <= 26){
+            count += helper(s, n-2, dp);
+        }
+        
+        return dp[n] = count;
+    }
+    
+    int numDecodings(string s) {
+        
+        int n = s.length();
+        vector<int> dp(n+1, -1);
+        
+        return helper(s, n, dp);
+        
+    }
+};
