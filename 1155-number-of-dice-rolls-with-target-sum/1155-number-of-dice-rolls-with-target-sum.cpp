fd74f49affdb1cfee33765a98ed5312f442a50e9
Akhil Rajan <rajanakhil07@gmail.com>
2022-10-02 13:47:30 +0530

    Time: 136 ms (12.50%), Space: 8.5 MB (45.97%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -34,10 +34,32 @@ public:
         return dp[dice][target] = ans % MOD;
     }
     
+    int tabulation(int d, int f, int t){
+        
+        vector<vector<int>> dp(d+1, vector<int>(t+1, 0));
+        // base case -> if(dice == 0 and target == 0) return 1
+        dp[0][0] = 1;
+        
+        for(int dice=1; dice<=d; dice++){
+            for(int target=1; target<=t; target++){
+                
+                int ans=0;
+                for(int i=1; i<=f; i++){
+                    if(target-i >= 0)
+                        ans = ((ans % MOD) + dp[dice-1][target-i] % MOD) % MOD;
+                }
+                
+                dp[dice][target] = ans;
+            }
+        }
+        
+        return dp[d][t] % MOD;
+    }
+    
     int numRollsToTarget(int n, int k, int target) {
         
         // two states changing -> dice and target
-        vector<vector<int>> dp(n+1, vector<int> (target+1, -1));
-        return fMemoization(n, k, target, dp);
+        //vector<vector<int>> dp(n+1, vector<int> (target+1, -1));
+        return tabulation(n, k, target);
     }
 };
