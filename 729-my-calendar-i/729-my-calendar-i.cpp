d493d2d898cfafc049b12f075bfe28774a227a74
Akhil Rajan <rajanakhil07@gmail.com>
2022-10-07 17:59:07 +0530

    Time: 244 ms (25.57%), Space: 38.9 MB (45.58%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,29 +1,61 @@
-class MyCalendar {
+template<typename Key, typename Value>
+using MapIterator = typename std::map<Key,Value>::const_iterator;
+
+template<typename Key, typename Value>
+Key floorKey(const std::map<Key,Value>& input, const Key& key){
+    /*
+    map::upper_bound(>): 
+    Returns an iterator pointing to the first element in the container 
+    whose key is considered to go after k.
     
-    private TreeMap<Integer, Integer> bookings;
-    public MyCalendar() {
-        bookings = new TreeMap<>();   
+    If k is larger than any key in the map, then it returns map::end
+    */
+    MapIterator<Key, Value> it = input.upper_bound(key);
+    if (it != input.begin()) {
+        return (--it)->first;
+    } else {
+        //assume Key is int
+        return -1;
+    }
+}
+
+template<typename Key, typename Value>
+Key ceilingKey(const std::map<Key,Value>& input, const Key& key){
+	/*
+	map::lower_bound(>=):
+	Returns an iterator pointing to the first element 
+	in the container whose key is not considered to go before k 
+	(i.e., either it is equivalent or goes after).
+	*/
+    MapIterator<Key, Value> it = input.lower_bound(key);
+    if (it != input.end()) {
+        return it->first;
+    } else {
+        //assume Key is int
+        return -1;
+    }
+}
+
+class MyCalendar {
+private:
+    map<int, int> bookings;
+public:
+    MyCalendar() {
+        
     }
     
-    public boolean book(int start, int end) {
+    bool book(int start, int end) {
         
-        // prev booking start time of curr booking
-        Integer prevBooking = bookings.floorKey(start);
-        // next booking start time of curr booking
-        Integer nextBooking = bookings.ceilingKey(start);
+        int prevBooking = floorKey(bookings, start);
+        int nextBooking = ceilingKey(bookings, start);
         
-        if((prevBooking == null || bookings.get(prevBooking) <= start) &&
-          (nextBooking == null || end <= nextBooking)){
-            bookings.put(start, end);
+        if((prevBooking == -1 || bookings[prevBooking] <= start) && 
+        (nextBooking == -1 || end <= nextBooking)){
+            bookings[start] = end;
             return true;
         }
         
         return false;
+        
     }
-}
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
+};
