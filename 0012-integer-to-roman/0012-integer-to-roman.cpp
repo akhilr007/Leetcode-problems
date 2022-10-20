eb6048ef49bd74aec0d78a6e22d6fef90e415a94
Akhil Rajan <rajanakhil07@gmail.com>
2022-10-20 12:13:11 +0530

    Time: 15 ms (55.72%), Space: 6.1 MB (47.60%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+        string units[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string thousands[] = {"", "M", "MM", "MMM"};
+
+        string result = "";
+
+        result += thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + units[num%10];
+        return result;
+
+    }
+};
