ec86e9980a4a2c1ce01e5a2fca715d34d178bc80
Akhil Rajan <rajanakhil07@gmail.com>
2022-11-29 13:04:18 +0530

    Time: 73 ms (30.11%), Space: 115 MB (11.67%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,28 +1,43 @@
 class RandomizedSet {
     
-    private HashSet<Integer> set;
+    private List<Integer> nums;
+    private Map<Integer, Integer> indexMap;
+
     public RandomizedSet() {
-        set = new HashSet<>();
+        nums = new ArrayList<>();
+        indexMap = new HashMap<>();
     }
     
     public boolean insert(int val) {
-        if(set.contains(val)==true) return false;
-        set.add(val);
+        
+        if(indexMap.containsKey(val) == true) return false;
+        
+        nums.add(val);
+        indexMap.put(val, nums.size()-1);
         return true;
     }
     
     public boolean remove(int val) {
-        if(set.contains(val)==false) return false;
-        set.remove(val);
+        
+        if(indexMap.containsKey(val) == false) return false;
+        
+        int idx = indexMap.get(val);
+        int lastElement = nums.get(nums.size()-1);
+        nums.set(nums.size()-1, val);
+        
+        nums.set(idx, lastElement);
+        indexMap.put(lastElement, idx);
+        
+        nums.remove(nums.size()-1);
+        indexMap.remove(val);
         return true;
     }
     
     public int getRandom() {
         
-        Integer[] arr = set.toArray(new Integer[set.size()]);
-        Random rand = new Random();
-        int randomNumber = rand.nextInt(set.size());
-        return arr[randomNumber];
+        Random random = new Random();
+        int randomIndex = random.nextInt(nums.size());
+        return nums.get(randomIndex);
     }
 }
 
