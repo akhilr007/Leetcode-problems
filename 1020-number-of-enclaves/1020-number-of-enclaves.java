9848819bed211ba76d10e8a7224141b7f8ec3de3
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-11 13:28:33 +0530

    Time: 52 ms (5.00%), Space: 75.5 MB (43.91%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,4 +1,41 @@
 class Solution {
+    private void bfs(Queue<int[]> q, int[][] grid, int[][] vis, int[][] delta){
+        
+        int N = grid.length;
+        int M = grid[0].length;
+        
+        while(q.size()>0){
+            
+            int row = q.peek()[0];
+            int col = q.peek()[1];
+            q.remove();
+            
+            for(int i=0; i<4; i++){
+                int nrow = row + delta[i][0];
+                int ncol = col + delta[i][1];
+                
+                if(nrow >= 0 && ncol >= 0 && nrow < N && ncol < M && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+                    q.add(new int[] {nrow, ncol});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+    }
+    
+    private void findEnclavesInGrid(int[][] grid, int[][] vis, int[] cnt){
+        
+        int N = grid.length;
+        int M = grid[0].length;
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                    cnt[0]++;
+                }
+            }
+        }
+    }
+    
     public int numEnclaves(int[][] grid) {
         
         int N = grid.length;
@@ -34,32 +71,10 @@ class Solution {
         }
         
         int[][] delta = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        while(q.size()>0){
-            
-            int row = q.peek()[0];
-            int col = q.peek()[1];
-            q.remove();
-            
-            for(int i=0; i<4; i++){
-                int nrow = row + delta[i][0];
-                int ncol = col + delta[i][1];
-                
-                if(nrow >= 0 && ncol >= 0 && nrow < N && ncol < M && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
-                    q.add(new int[] {nrow, ncol});
-                    vis[nrow][ncol] = 1;
-                }
-            }
-        }
+        bfs(q, grid, vis, delta);
         
-        int cnt=0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0){
-                    cnt++;
-                }
-            }
-        }
-        
-        return cnt;
+        int[] cnt = {0};
+        findEnclavesInGrid(grid, vis, cnt);
+        return cnt[0];
     }
 }
