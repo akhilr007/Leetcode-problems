9e79126e21597eac2f59eaacd86a176442024406
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-12 11:57:51 +0530

    Time: 3 ms (34.80%), Space: 6.1 MB (38.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -12,6 +12,7 @@ public:
         return oneStep + twoStep;
     }
     
+    // time complexity -> O(n) space complexity -> O(n) + O(n) recursion stack space
     int solveMem(int n, vector<int>& dp){
         
         if(n == 1) return 1;
@@ -24,9 +25,22 @@ public:
         return dp[n] = oneStep + twoStep;
     }
     
+    int solveTab(int n){
+        
+        if(n == 1) return 1;
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+    
     int climbStairs(int n) {
         
-        vector<int> dp(n+1, -1);
-        return solveMem(n, dp);
+        return solveTab(n);
     }
 };
