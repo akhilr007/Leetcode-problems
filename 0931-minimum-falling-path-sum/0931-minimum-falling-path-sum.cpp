5daa16d6339770418ab11cce405be95292a312fd
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-13 12:07:44 +0530

    Time: 36 ms (42.30%), Space: 10.4 MB (27.68%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // time complexity -> O(n * 3^n)
+    // space complexity -> O(n)
     int solveRec(int row, int col, vector<vector<int>>& matrix){
         
         if(row<0 || row>=matrix.size() || col<0 || col>=matrix[0].size()){
@@ -16,6 +18,8 @@ public:
         return min({up, upLeft, upRight});
     }
     
+    // time complexity -> O(n * 3*n*m)
+    // space complexity -> O(n*m)
     int solveMem(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& dp){
         
         if(row<0 || row>=matrix.size() || col<0 || col>=matrix[0].size()){
@@ -33,16 +37,37 @@ public:
         return dp[row][col] = min({up, upLeft, upRight});
     }
     
-    int minFallingPathSum(vector<vector<int>>& matrix) {
+    int solveTab(vector<vector<int>>& matrix, int n){
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        // for row = 0 copy same values of matrix in dp
+        for(int i=0; i<n; i++){
+            dp[0][i] = matrix[0][i];
+        }
+        
+        for(int row=1; row<n; row++){
+            for(int col=0; col<n; col++){
+                
+                int up=1e8, upLeft=1e8, upRight=1e8;
+                if(row-1>=0) up = matrix[row][col] + dp[row-1][col];
+                if(row-1>=0 && col-1>=0) upLeft = matrix[row][col] + dp[row-1][col-1];
+                if(row-1>=0 && col+1<n) upRight = matrix[row][col] + dp[row-1][col+1]; 
+                
+                dp[row][col] = min({up, upLeft, upRight});
+            }
+        }
         
         int res = INT_MAX;
-        int n = matrix.size();
-        
-        vector<vector<int>> dp(n, vector<int>(n, -1));
         for(int i=0; i<n; i++){
-            res = min(res, solveMem(n-1, i, matrix, dp));
+            res = min(res, dp[n-1][i]);
         }
         
         return res;
+    }
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        return solveTab(matrix, n);
     }
 };
