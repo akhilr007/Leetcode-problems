aac8d5d566d8bf8353bfe62d7f62eb647d2ba6fc
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-14 13:04:51 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // time complexity -> 2^m
+    // space complexity -> O(n)
     int solveRec(int idx, vector<int>& nums){
         
         if(idx<0) return 0;
@@ -11,6 +13,8 @@ public:
         return max(notPick, pick);
     }
     
+    // time complexity -> O(n)
+    // space complexity -> O(n for dp cache + n for recursion stack space)
     int solveMem(int idx, vector<int>& nums, vector<int>& dp){
         
         if(idx<0) return 0;
@@ -23,14 +27,28 @@ public:
         return dp[idx] = max(notPick, pick);
     }
     
+    int solveTab(vector<int>& nums, int n){
+        
+        if(n == 1) return nums[0];
+        
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for(int i=2; i<=n; i++){
+            
+            int pick = nums[i-1] + dp[i-2];
+            int notPick = 0 + dp[i-1];
+            
+            dp[i] = max(pick, notPick);
+        }
+        
+        return dp[n];
+    }
+    
     int rob(vector<int>& nums) {
         
         int n = nums.size();
-        
-        vector<int> dp(n, -1);
-        int last = solveMem(n-1, nums, dp);
-        int secondLast = solveMem(n-2, nums, dp);
-        
-        return max(last, secondLast);
+        return solveTab(nums, n);
     }
 };
