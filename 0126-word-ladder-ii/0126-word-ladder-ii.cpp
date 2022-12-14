f423b5eba68501332e0343faa29aad9777d6cf0a
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-14 19:43:20 +0530

    Time: 19 ms (90.08%), Space: 9.2 MB (67.96%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,22 +1,23 @@
-class Solution {
-public:
+class Solution{
+public: 
     unordered_map<string, int> mp;
     vector<vector<string>> ans;
     string b;
-    
+
     void dfs(string word, vector<string>& seq){
-        
+
         if(word == b){
             reverse(seq.begin(), seq.end());
             ans.push_back(seq);
             reverse(seq.begin(), seq.end());
             return;
         }
-        
+
         int steps = mp[word];
+
         for(int i=0; i<word.length(); i++){
-            
-            char original = word[i];
+
+            char originalChar = word[i];
             for(char ch='a'; ch<='z'; ch++){
                 word[i] = ch;
                 if(mp.find(word) != mp.end() && mp[word] + 1 == steps){
@@ -25,32 +26,34 @@ public:
                     seq.pop_back();
                 }
             }
-            word[i] = original;
+
+            word[i] = originalChar;
         }
     }
-    
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        
+
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList){
+
         b = beginWord;
         unordered_set<string> st(wordList.begin(), wordList.end());
-        
+
         queue<string> q;
         q.push(beginWord);
-    
-        mp[beginWord] = 1;
+
         st.erase(beginWord);
-        
-        int wordSize = beginWord.length();
+
+        int length = beginWord.length();
+
         while(!q.empty()){
-            
+
             string word = q.front();
             q.pop();
+
             int steps = mp[word];
-            
+
             if(word == endWord) break;
-            
-            for(int i=0; i<wordSize; i++){
-                char original = word[i];
+
+            for(int i=0; i<length; i++){
+                char originalChar = word[i];
                 for(char ch='a'; ch<='z'; ch++){
                     word[i] = ch;
                     if(st.count(word)){
@@ -59,18 +62,17 @@ public:
                         st.erase(word);
                     }
                 }
-                
-                word[i] = original;
+
+                word[i] = originalChar;
             }
         }
-        
+
         if(mp.find(endWord) != mp.end()){
-            cout<<"a";
             vector<string> seq;
             seq.push_back(endWord);
             dfs(endWord, seq);
         }
-        
+
         return ans;
     }
 };
