045668f70e7a7ce2b2a9b3587e968d1b8dce9151
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-15 14:54:11 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
@@ -5,50 +5,52 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    typedef pair<int, int> pii;
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
         // Code here
-        vector<pair<int, int>> adj[n+1];
+        // build adjacency matrix
+        vector<pair<int, int>> adj[n+1]; // 1-based graph
         for(int i=0; i<m; i++){
             adj[edges[i][0]].push_back({edges[i][1], edges[i][2]});
             adj[edges[i][1]].push_back({edges[i][0], edges[i][2]});
         }
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, 1});
-        
+
         vector<int> dist(n+1, 1e9), parent(n+1);
-        dist[1] = 0;
-        
         for(int i=1; i<=n; i++) parent[i] = i;
-        
+
+
+        dist[1] = 0;
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        pq.push({0, 1}); // [distance, source_node]
+
         while(!pq.empty()){
-            
+
             int dis = pq.top().first;
             int node = pq.top().second;
             pq.pop();
-            
-            for(auto it : adj[node]){
+
+            for(auto it: adj[node]){
                 int edgeWeight = it.second;
                 int adjNode = it.first;
-                
+
                 if(dis + edgeWeight < dist[adjNode]){
                     dist[adjNode] = dis + edgeWeight;
-                    pq.push({dist[adjNode], adjNode});
+                    pq.push({ dist[adjNode], adjNode });
                     parent[adjNode] = node;
                 }
             }
         }
-        
+
         if(dist[n] == 1e9) return {-1};
-        
         int node = n;
+
         vector<int> path;
         while(parent[node] != node){
             path.push_back(node);
             node = parent[node];
         }
-        
-        path.push_back(node);
+
+        path.push_back(1);
         reverse(path.begin(), path.end());
         return path;
     }
