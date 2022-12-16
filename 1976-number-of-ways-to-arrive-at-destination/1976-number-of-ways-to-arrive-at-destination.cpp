535570581c71a319413283436bb63d5c30eaf2cf
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-16 20:31:57 +0530

    Time: 77 ms (99.23%), Space: 35.6 MB (17.26%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -3,41 +3,41 @@ public:
     int countPaths(int n, vector<vector<int>>& roads) {
         
         vector<pair<long, long>> adj[n];
-        for(auto it : roads){
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
+        for(auto it: roads){
+            adj[it[0]].push_back({ it[1], it[2] });
+            adj[it[1]].push_back({ it[0], it[2] });
         }
-        
+
         vector<long> dist(n, LONG_MAX), ways(n, 0);
-        ways[0] = 1;
         dist[0] = 0;
-        
+        ways[0] = 1;
+
         priority_queue<pair<long, long>, vector<pair<long, long>>, greater<pair<long, long>>> pq;
-        pq.push({ 0, 0}); // {dist, node}
-        
-        int mod = (long)(1e9+7);
+        pq.push({0, 0}); // [dist, node]
+
+        long mod = 1e9+7;
+
         while(!pq.empty()){
-            
-            long node = pq.top().second;
+
             long dis = pq.top().first;
+            long node = pq.top().second;
             pq.pop();
-            
-            for(auto nbr: adj[node]){
-                long adjNode = nbr.first;
-                long edgeWeight = nbr.second;
-                
-                if(dis + edgeWeight < dist[adjNode]){
-                    dist[adjNode] = dis + edgeWeight;
+
+            for(auto it : adj[node]){
+                long adjNode = it.first;
+                long edgeWt = it.second;
+
+                if(dis + edgeWt < dist[adjNode]){
+                    dist[adjNode] = dis + edgeWt;
                     pq.push({ dist[adjNode], adjNode});
                     ways[adjNode] = ways[node];
                 }
-                else if(dis + edgeWeight == dist[adjNode]){
+                else if(dis + edgeWt == dist[adjNode]){
                     ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
                 }
-                
             }
         }
-        
+
         return (int)ways[n-1] % mod;
     }
 };
