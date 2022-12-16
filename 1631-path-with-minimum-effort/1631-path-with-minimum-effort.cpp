d25b19586c59f934b7b42dff4970c455860e89ed
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-16 13:35:32 +0530

    Time: 135 ms (99.55%), Space: 19.9 MB (85.19%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,3 +1,15 @@
+struct Pair{
+    int diff;
+    int row;
+    int col;
+    
+    Pair(int diff, int row, int col){
+        this->diff = diff;
+        this->row = row;
+        this->col = col;
+    }
+};
+
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
@@ -5,40 +17,49 @@ public:
         int n = heights.size();
         int m = heights[0].size();
         
-        vector<vector<int>> efforts(n, vector<int> (m, INT_MAX));
-        efforts[0][0] = 0;
+        vector<vector<int>> dist(n, vector<int> (m, 1e9));
         
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-        pq.push(make_tuple(0, 0, 0)); // effort, row, col
+        auto lambda = [&](Pair p1, Pair p2){
+            return p1.diff > p2.diff;
+        };
+        
+        priority_queue<Pair, vector<Pair>, decltype(lambda)> pq(lambda);
+        Pair p = Pair(0, 0, 0);
+        pq.push(p); // [diff, row, col]
+        
+        dist[0][0] = 0;
         
         int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         
-        while(pq.size()>0){
+        while(!pq.empty()){
             
-            auto [currEffort, row, col] = pq.top();
+            auto it = pq.top();
             pq.pop();
             
-            if(row == n-1 and col == m-1){
-                return currEffort;
-            }
+            int row = it.row;
+            int col = it.col;
+            int diff = it.diff;
             
-            for(int d=0; d<4; d++){
-                int r = row + dir[d][0];
-                int c = col + dir[d][1];
+            if(row == n-1 && col == m-1) return diff;
+            
+            for(int i=0; i<4; i++){
                 
-                if(r >= 0 and c >= 0 and r < n and c < m){
+                int new_row = row + dir[i][0];
+                int new_col = col + dir[i][1];
+                
+                if(new_row>=0 && new_col >= 0 && new_row<n && new_col<m){
                     
-                    int newEffort = max(currEffort, 
-                                        abs(heights[r][c] - heights[row][col]));
+                    int new_effort = max(abs(heights[row][col] - heights[new_row][new_col]), diff);
                     
-                    if(newEffort < efforts[r][c]){
-                        efforts[r][c] = newEffort;
-                        pq.push(make_tuple(newEffort, r, c));
+                    if(new_effort < dist[new_row][new_col]){
+                        dist[new_row][new_col] = new_effort;
+                        Pair p = Pair(new_effort, new_row, new_col);
+                        pq.push(p);
                     }
                 }
             }
         }
         
-        return -1;
+        return 0;
     }
 };
