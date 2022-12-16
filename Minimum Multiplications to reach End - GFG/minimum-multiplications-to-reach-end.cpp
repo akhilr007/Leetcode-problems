19871254d443870657b1fd4c1f6a87d5756c6878
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-16 18:58:36 +0530

    Added solution - LeetHub

--- Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
@@ -15,30 +15,31 @@ class Solution {
         
         vector<int> dist(100000, 1e9);
         dist[start] = 0;
-        
-        int mod = 100000;
+
         queue<pair<int, int>> q;
         q.push({ start, 0});
-        
+
+        int mod = 100000;
+
         while(!q.empty()){
-            
+
             int node = q.front().first;
             int steps = q.front().second;
             q.pop();
-            
+
             if(node == end) return steps;
-            
+
             for(auto it : arr){
-                
+
                 int num = (it * node) % mod;
-                
+
                 if(dist[node] + 1 < dist[num]){
-                    dist[num] = 1 + dist[node];
-                    q.push({ num, steps + 1});
+                    dist[num] = dist[node] + 1;
+                    q.push({num, steps + 1});
                 }
             }
         }
-        
+
         return -1;
     }
 };
