4419fdea59c65e6e17d9cd152b901d6199b951e5
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-16 18:15:26 +0530

    Time: 27 ms (89.84%), Space: 13.7 MB (42.56%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,27 +2,39 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        vector<int> prices(n, INT_MAX);
-        prices[src] = 0;
+        vector<pair<int, int>> adj[n];
+        for(auto it: flights){
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
         
-        for(int i=1; i<=k+1; i++){
-            vector<int> tempPrices = prices;
-            for(int j=0; j<flights.size(); j++){
-                int s = flights[j][0]; // src
-                int d = flights[j][1]; // dest
-                int p = flights[j][2]; // price
+        queue<pair<int, pair<int, int>>> q;
+        q.push({ 0, {src, 0}}); // [steps, node, dist]
+        
+        while(!q.empty()){
+            
+            auto it = q.front();
+            q.pop();
+            
+            int steps = it.first;
+            int node = it.second.first;
+            int dis = it.second.second;
+            
+            if(steps > k) continue;
+            
+            for(auto nbr: adj[node]){
+                int adjNode = nbr.first;
+                int edgeWeight = nbr.second;
                 
-                if(prices[s] == INT_MAX) continue;
-                
-                if(prices[s] + p < tempPrices[d]){
-                    tempPrices[d] = prices[s] + p;
+                if(dis + edgeWeight < dist[adjNode]){
+                    dist[adjNode] = dis + edgeWeight;
+                    q.push({steps + 1, {adjNode,  dist[adjNode]}});
                 }
             }
-            
-            prices = tempPrices;
         }
         
-        if(prices[dst] == INT_MAX) return -1;
-        return prices[dst];
+        if(dist[dst] == 1e9) return -1;
+        return dist[dst];
     }
 };
