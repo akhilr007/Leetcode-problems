3c85277460cdef7cdeb72339a451da1bf119959f
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-22 19:46:35 +0530

    Time: 303 ms (84.72%), Space: 94.3 MB (30.34%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,54 +1,60 @@
 class Solution {
 public:
+    void buildGraph(vector<vector<int>>& edges, vector<int> adj[], int n){
+        
+        for(auto it: edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+    }
+    
+    void dfs(int node, int par, vector<int> adj[], vector<int>& childNodes){
+        
+        for(auto adjNode: adj[node]){
+            if(adjNode != par){
+                dfs(adjNode, node, adj, childNodes);
+                childNodes[node] += childNodes[adjNode];
+            }
+        }
+    }
+    
     int bfs(int src, vector<int> adj[], int n){
         
         queue<pair<int, int>> q;
-        q.push({src, 0});
-        
         vector<int> vis(n, 0);
         vis[src] = 1;
+        q.push({src, 0});
         
-        int sum=0;
+        int sum = 0;
         while(!q.empty()){
             
-            auto it = q.front();
+            int node = q.front().first;
+            int dist = q.front().second;
             q.pop();
-            int node = it.first;
-            int dist = it.second;
             
             sum += dist;
             
             for(auto adjNode: adj[node]){
                 if(!vis[adjNode]){
-                    q.push({adjNode, dist + 1});
+                    q.push({adjNode, dist+1});
                     vis[adjNode] = 1;
                 }
             }
         }
+        
         return sum;
     }
     
-    void calculateSubtree(int node, int par, vector<int> adj[], vector<int>& subtrees){
-        
-        for(auto adjNode: adj[node]){
-            if(adjNode != par){
-                calculateSubtree(adjNode, node, adj, subtrees);
-                subtrees[node] += subtrees[adjNode];
-            }
-        }
-    }
-    
-    void bfs2(int src, vector<int> adj[], vector<int>& subtrees, vector<int>& ans){
+    void bfs(vector<int> adj[], vector<int>& ans, vector<int>& childNodes){
         
         queue<int> q;
-        q.push(src);
+        q.push(0);
         
-        int n = subtrees.size();
+        int n = ans.size();
         vector<int> vis(n, 0);
-        vis[src] = 1;
+        vis[0] = 1;
         
         while(!q.empty()){
-            
             int node = q.front();
             q.pop();
             
@@ -56,7 +62,7 @@ public:
                 if(!vis[adjNode]){
                     q.push(adjNode);
                     vis[adjNode] = 1;
-                    ans[adjNode] = ans[node] - subtrees[adjNode] + ( n - subtrees[adjNode]); 
+                    ans[adjNode] = ans[node] - childNodes[adjNode] + (n - childNodes[adjNode]);
                 }
             }
         }
@@ -65,18 +71,16 @@ public:
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
         
         vector<int> adj[n];
-        for(auto it: edges){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
+        buildGraph(edges, adj, n);
         
-        vector<int> ans(n, 0);
+        vector<int> childNodes(n); // count of child nodes including itself
+        for(int i=0; i<n; i++) childNodes[i] = 1;
+        dfs(0, -1, adj, childNodes);
+        
+        vector<int> ans(n,0);
         ans[0] = bfs(0, adj, n);
         
-        vector<int> subtrees(n, 1);
-        calculateSubtree(0, -1, adj, subtrees);
-        
-        bfs2(0, adj, subtrees, ans);
+        bfs(adj, ans, childNodes);
         
         return ans;
     }
