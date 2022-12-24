b7fa9addc4c4da3740f9f837e15a3870282e5117
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-24 20:07:16 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.79%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -12,7 +12,16 @@ public:
     
     int numTilings(int n) {
         
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        vector<int> dp(n+1, 0);
+        if(n == 1 or n == 2) return n;
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i=4; i<=n; i++){
+            dp[i] = (2*dp[i-1] % mod + dp[i-3] % mod) % mod;
+        }
+        
+        return dp[n];
     }
 };
