82ee9e2fcf93a2124a804a51576fdf0ada708ab3
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-25 15:27:41 +0530

    Time: 11 ms (99.30%), Space: 13.9 MB (21.90%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+private:
+    vector<int> bruteForce(vector<int>& nums, vector<int>& queries){
         
         int n = nums.size();
         int m = queries.size();
@@ -24,6 +24,45 @@ public:
             }
         }
         
+        return ans;
+    }
+public:
+    
+    int bsearch(int low, int high, vector<int>& csum, int num){
+        
+        int result=csum.size();
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(csum[mid] > num){
+                result = mid; 
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        
+        return result;
+    }
+    
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        
+        int n = nums.size();
+        int m = queries.size();
+        sort(begin(nums), end(nums));
+        
+        vector<int> csum(n);
+        csum[0] = nums[0];
+        for(int i=1; i<n; i++){
+            csum[i] = csum[i-1] + nums[i];
+        }
+        
+        vector<int> ans(m);
+        for(int i=0; i<m; i++){
+            
+            int num = queries[i];
+            int indexGreaterThanJustNum = bsearch(0, n-1, csum, num);
+            ans[i] = indexGreaterThanJustNum;
+        }
         return ans;
     }
 };
