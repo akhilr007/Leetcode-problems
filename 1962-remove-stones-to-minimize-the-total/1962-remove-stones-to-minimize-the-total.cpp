888c4d50f76b2254c5fac3193b969e2f8500cf04
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-28 14:30:49 +0530

    Time: 560 ms (89.66%), Space: 105.2 MB (5.00%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -5,21 +5,21 @@ public:
         int n = piles.size();
         priority_queue<int> pq;
         // nlogn
-        for(int i=0; i<n; i++) pq.push(piles[i]);
+        int sum=0;
+        for(int i=0; i<n; i++) {
+            pq.push(piles[i]);
+            sum += piles[i];
+        }
         
         // klogn
         while(k-->0){
             int val = pq.top();
             pq.pop();
+            sum -= val;
             
             int tempVal = ceil(val/2.0);
+            sum += tempVal;
             pq.push(tempVal);
-        }
-        
-        int sum=0;
-        while(!pq.empty()){
-            sum += pq.top();
-            pq.pop();
         }
         
         return sum;
