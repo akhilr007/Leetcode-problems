f6c636565fd727c93500892026f99432ccb03aae
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-30 14:54:51 +0530

    Time: 524 ms (84.37%), Space: 105.3 MB (86.94%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -36,76 +36,79 @@ public:
     }
 };
 
-class Solution {
+class Solution{
 private:
     bool isValid(int r, int c, int n){
-        return r>=0 and r<n and c>=0 and c<n;
+        return r >= 0 and r < n and c >= 0 and c < n;
     }
     
 public:
-    int largestIsland(vector<vector<int>>& grid) {
-        
+    int largestIsland(vector<vector<int>>& grid){
+
         int n = grid.size();
-        UnionFind uf(n*n);
-        
+        UnionFind uf(n * n);
+
         int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        
-        // step1 -> connect all the components
+        // step 1 : connect all the 1s to make a component
         for(int row=0; row<n; row++){
             for(int col=0; col<n; col++){
-                
+
                 if(grid[row][col] == 0) continue;
                 
                 for(int d=0; d<4; d++){
-                    
+
                     int new_row = row + dir[d][0];
                     int new_col = col + dir[d][1];
-                    
+
                     if(isValid(new_row, new_col, n) && grid[new_row][new_col] == 1){
-                        
-                        int nodeNo = row *n + col;
-                        int adjNodeNo = new_row * n + new_col;
-                        
-                        // connect all the 1s to make a component
+
+                        int nodeNo = (row * n) + col;
+                        int adjNodeNo = (new_row * n) + new_col;
+
+                        // connect both the node to make a component
                         uf.unionBySize(nodeNo, adjNodeNo);
                     }
                 }
+
             }
         }
-        
+
         int mx=0;
-        // step2 -> connect all zero to find which makes big component
+        // find zero and change itself to 1 to connect with neighbors to make a big component
         for(int row=0; row<n; row++){
             for(int col=0; col<n; col++){
-                
+
                 if(grid[row][col] == 1) continue;
-                set<int> components; // to store all the parent components
+                set<int> components; // to store the cell ultimate parent
                 for(int d=0; d<4; d++){
                     int new_row = row + dir[d][0];
                     int new_col = col + dir[d][1];
-                    
+
                     if(isValid(new_row, new_col, n)){
                         if(grid[new_row][new_col] == 1){
                             
                             int adjNodeNo = (new_row * n) + new_col;
                             int findComponentParent = uf.findUltimateParent(adjNodeNo);
+
                             components.insert(findComponentParent);
                         }
                     }
                 }
-                
-                int sizeTotal=0;
+
+                // calculate size of all the components
+                int sizeTotal = 0;
                 for(auto it: components){
                     sizeTotal += uf.size[it];
                 }
-                mx = max(mx, sizeTotal + 1); // 1 for self node
+                mx = max(mx, sizeTotal + 1);
             }
         }
-        // if all the cells are 1 then
+
+        // if all the cells are 1
         for(int cellNo=0; cellNo<n*n; cellNo++){
             mx = max(mx, uf.size[uf.findUltimateParent(cellNo)]);
         }
-        
+
         return mx;
     }
 };
