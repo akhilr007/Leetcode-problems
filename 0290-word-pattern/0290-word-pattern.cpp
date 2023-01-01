02a947c68485b2131b1f84b768c3b39a48ded102
Akhil Rajan <rajanakhil07@gmail.com>
2023-01-01 13:50:16 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (19.87%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -17,22 +17,30 @@ public:
         vector<string> words = getWordsFromString(s);
         if(pattern.size() != words.size()) return false;
        
-        map<char, string> charToWord;
-        map<string, char> wordToChar;
+        unordered_map<char, string> charToWord;
+        unordered_map<string, bool> wordVisited;
+        
         for(int i=0; i<pattern.length(); i++){
             
             char ch = pattern[i];
-            // is char present in map ? if not then add it to map
-            if(charToWord.find(ch) == charToWord.end() &&
-              wordToChar.find(words[i]) == wordToChar.end()){
-                charToWord[ch] = words[i];
-                wordToChar[words[i]] = ch;
-            }
-            // if yes, then check if the word is same or not
-            else{
+            
+            // if char is present
+            if(charToWord.find(ch) != charToWord.end()){
+                
+                // then word should match 
                 if(charToWord[ch] != words[i]) return false;
             }
+            else{
+                // if char is not present
+                if(wordVisited.find(words[i]) != wordVisited.end()) return false;
+                else{
+                    charToWord[ch] = words[i];
+                    wordVisited[words[i]] = true;
+                }
+            }
         }
+        
+        return true;
         
         return true;
     }
