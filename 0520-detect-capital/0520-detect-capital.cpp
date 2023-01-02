03324fb344958d74a99bed9d78daee2ac96c2a84
Akhil Rajan <rajanakhil07@gmail.com>
2023-01-02 12:18:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (70.23%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        int n = word.length();
+        int countCapitals=0;
+        
+        bool firstLetter = false;
+        if(isupper(word[0])){
+            countCapitals++;
+            firstLetter=true;
+        }
+        
+        
+        for(int i=1; i<n; i++){
+            char ch = word[i];
+            if(ch >= 'A' && ch <= 'Z') countCapitals++;
+            
+            if(firstLetter == false && countCapitals > 1) return false;
+        }
+        
+        if(firstLetter == true && countCapitals == 1) return true;
+        return (countCapitals == n) || (countCapitals == 0);
+    }
+};
