9e045d0ab952b54b9810cdf757584c0700058a96
Akhil Rajan <rajanakhil07@gmail.com>
2023-01-04 22:12:06 +0530

    Time: 2 ms (42.22%), Space: 5.9 MB (67.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     int solveRec(int n){
         
-        if(n == 1 || n == 0) return 1;
+        if(n == 1 || n == 2) return n;
         
         int oneStep = solveRec(n-1);
         int twoStep = solveRec(n-2);
