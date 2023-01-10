3cea5ab8363e71222b9f8772daeda15f5172c11f
Akhil Rajan <rajanakhil07@gmail.com>
2023-01-10 22:51:32 +0530

    Time: 0 ms (100.00%), Space: 40 MB (62.93%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,33 +1,35 @@
 class Solution {
     
-    List<TreeNode> l1 = new ArrayList<>();
-    List<TreeNode> l2 = new ArrayList<>();
-    
-    private void preorder(TreeNode root, List<TreeNode> l){
-        
-        if(root == null){
-            l.add(null);
-            return;
-        }
-        
-        l.add(root);
-        preorder(root.left, l);
-        preorder(root.right, l);
-    
-    }
-    
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        preorder(p, l1);
-        preorder(q, l2);
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
         
-        if(l1.size() != l2.size()) return false;
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(p);
+        q2.add(q);
         
-        for(int i=0; i<l1.size(); i++){
+        while(q1.size() > 0 && q2.size() > 0){
             
-            if(l1.get(i) == null && l2.get(i) == null) continue;
-            if(l1.get(i) == null || l2.get(i) == null) return false;
-            if(l1.get(i).val != l2.get(i).val) return false;
+            TreeNode node1 = q1.remove();
+            TreeNode node2 = q2.remove();
+            
+            if(node1.val != node2.val) return false;
+            
+            if(node1.left != null && node2.left != null){
+                q1.add(node1.left);
+                q2.add(node2.left);
+            }
+            else if(node1.left != null || node2.left != null)
+                return false;
+            
+            if(node1.right != null && node2.right != null){
+                q1.add(node1.right);
+                q2.add(node2.right);
+            }
+            else if(node1.right != null || node2.right != null)
+                return false;
         }
         
         return true;
