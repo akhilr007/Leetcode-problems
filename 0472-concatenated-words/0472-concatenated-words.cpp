53b89333468bb901d2789d0a15f75f6b2fed664f
Akhil Rajan <rajanakhil07@gmail.com>
2023-01-27 14:15:50 +0530

    Time: 392 ms (46.56%), Space: 54.8 MB (51.03%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    bool dfs(string& word, unordered_set<string>& s){
+    // time complexity -> O(n * l^3)
+    bool dfs(string& word, unordered_set<string>& s, unordered_map<string, bool>& dp){
+        
+        if(dp[word] == true)
+            return dp[word];
         
         for(int i=1; i<word.length(); i++){
             
@@ -8,20 +12,23 @@ public:
             string suffix = word.substr(i, word.length()-i);
             
             if((s.find(prefix) != s.end() && s.find(suffix) != s.end()) or 
-               (s.find(prefix) != s.end() && dfs(suffix, s))){
-                return true;
+               (s.find(prefix) != s.end() && dfs(suffix, s, dp))){
+                dp[word] = true;
+                return dp[word];
             }
         }
         
-        return false;
+        return dp[word] = false;
     }
+    
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         
         unordered_set<string> s(words.begin(), words.end());
         vector<string> res;
         
+        unordered_map<string, bool> dp;
         for(auto& word: words){
-            if(dfs(word, s))
+            if(dfs(word, s, dp))
                 res.push_back(word);
         }
         return res;
