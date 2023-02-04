7f6fb6554e0729379263cfaf6ae73eb519e625be
Akhil Rajan <rajanakhil07@gmail.com>
2023-02-04 20:37:34 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (74.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    bool areEqual(vector<int>& countS1, vector<int>& countS2){
+    bool areEqual(vector<int>& count1, vector<int>& count2){
         
-        for(int i=0; i<26; i++){
-            if(countS1[i] != countS2[i]) return false;
-        }
+        for(int i=0; i<26; i++)
+            if(count1[i] != count2[i])
+                return false;
         
         return true;
     }
     
     bool checkInclusion(string s1, string s2) {
         
-        if(s1.length() > s2.length()) return false;
+        if(s1.length() > s2.length())
+            return false;
         
-        vector<int> countS1(26, 0), countS2(26, 0);
+        vector<int> count1(26, 0), count2(26, 0);
         
-        for(int i=0; i<s1.length(); i++){
-            countS1[s1[i]-'a']++;
-        }
+        for(int i=0; i<s1.length(); i++)
+            count1[s1[i] - 'a']++;
         
-        int i=0;
+        int i=0; 
         int j=0;
         
         while(j < s2.length()){
             
-            countS2[s2[j]-'a']++;
+            count2[s2[j] - 'a']++;
             
             if(j-i+1 == s1.length()){
-                if(areEqual(countS1, countS2)) return true;
+                if(areEqual(count1, count2))
+                    return true;
             }
             
-            if(j-i+1 < s1.length()) j++;
+            if(j-i+1 < s1.length())
+                j++;
             else{
-                countS2[s2[i]-'a']--;
+                count2[s2[i]-'a']--;
+                
                 i++;
                 j++;
             }
