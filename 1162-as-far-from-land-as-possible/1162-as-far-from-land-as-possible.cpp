25d3bcfe8a48e961e8c99e0af72be9553dcf1355
Akhil Rajan <rajanakhil07@gmail.com>
2023-02-10 14:28:49 +0530

    Time: 70 ms (72.74%), Space: 23 MB (33.53%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,46 +1,56 @@
 class Solution {
 public:
+    int bfs(vector<vector<int>>& grid, queue<pair<int, pair<int, int>>>& q, int n){
+        
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        
+        int res = 0;
+        
+        while(!q.empty()){
+            
+            auto it = q.front();
+            q.pop();
+            
+            int dist = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+            
+            res = max(res, dist);
+            
+            for(int d=0; d<4; d++){
+                
+                int nr = r + dir[d][0];
+                int nc = c + dir[d][1];
+                
+                if(nr>=0 && nc>=0 && nr<n && nc< n &&
+                   grid[nr][nc] == 0 && vis[nr][nc] == 0){
+                    
+                    q.push({dist+1, {nr, nc}});
+                    vis[nr][nc] = 1;
+                }
+            }
+        
+        }
+        
+        return res;
+    }
+    
     int maxDistance(vector<vector<int>>& grid) {
         
-        queue<pair<int, int>> q;
         int n = grid.size();
-
+        queue<pair<int, pair<int, int>>> q;
+        
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 1){
-                    q.push({i, j});
-                }
-            }
+            for(int j=0; j<n; j++)
+                if(grid[i][j] == 1)
+                    q.push({0, {i, j}});
+                    
         }
         
-        if(q.size() == 0 || q.size() == n*n) return -1;
-
-        int level=-1;
-        int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-
-        while(q.size()>0){
-            int size = q.size();
-            level++;
-
-            for(int k=0; k<size; k++){
-                auto rem = q.front();
-                q.pop();
-
-                int i = rem.first;
-                int j = rem.second;
-
-                for(int d=0; d<4; d++){
-                    int r = i + dir[d][0];
-                    int c = j + dir[d][1];
-
-                    if(r >= 0 && c>=0 && r<grid.size() && c<grid[0].size() && grid[r][c] == 0){
-                        q.push({r, c});
-                        grid[r][c] = 1;
-                    }
-                }
-            }
-        }
-
-        return level;
+        if(q.size() == 0 || q.size() == n*n)
+            return -1;
+        
+        return bfs(grid, q, n);
     }
 };
