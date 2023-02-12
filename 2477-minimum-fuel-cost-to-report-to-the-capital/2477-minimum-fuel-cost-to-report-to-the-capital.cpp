0afc3cae39f0599587fe5c0692b0e7af4a9128f4
Akhil Rajan <rajanakhil07@gmail.com>
2023-02-12 23:11:04 +0530

    Time: 717 ms (45.83%), Space: 200.4 MB (54.30%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-long long result=0;
-    int dfs(int node,int parent,unordered_map<int,vector<int>>&adj,int seats)
-    {
-      int passengers=0;
-      for(auto &x:adj[node])
-      {
-          if(x!=parent)
-          {
-            int p=dfs(x,node,adj,seats);
-            passengers+=p;
-            result+=(int)ceil(p*1.0/seats);
-          }
-      }
-        return passengers+1;
-    }
-
-    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        unordered_map<int,vector<int>>adj;
-        for(auto &x:roads)
-        {
-            adj[x[0]].push_back(x[1]);
-            adj[x[1]].push_back(x[0]);
+    long long res;
+    int dfs(int node, int par, vector<int> adj[], int seats){
+        
+        int passengers = 0;
+        
+        for(int& child: adj[node]){
+            if(child != par){
+                
+                int p = dfs(child, node, adj, seats);
+                
+                passengers += p;
+                
+                res += (int)ceil(p*1.0/seats);
+            }
         }
-        dfs(0,-1,adj,seats);
-        return result;
+        
+        return passengers + 1;
+    }
+    
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        
+        int n = roads.size();
+        vector<int> adj[n+1];
+        for(auto it : roads){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        res = 0;
+        dfs(0, -1, adj, seats);
+        
+        return res;
     }
 };
