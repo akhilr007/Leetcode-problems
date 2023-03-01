65a1f9980016d9f817486c078bc4eea6c5987937
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-01 22:21:48 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -26,10 +26,16 @@ class Solution
     {
         for(int i=n-1; i>=0; i--){
             
+            int didSwap = 0;
             for(int j=0; j<=i-1; j++){
-                if(arr[j] > arr[j+1])
+                if(arr[j] > arr[j+1]){
                     swap(arr[j], arr[j+1]);
+                    didSwap = 1;
+                }
+                    
             }
+            
+            if(didSwap == 0) return;
         }
     }
 };
