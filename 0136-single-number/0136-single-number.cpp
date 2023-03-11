8102175aab2b5eefaab66e630dc8aa88f43f3dbd
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-11 21:11:42 +0530

    Time: 35 ms (23.23%), Space: 20.2 MB (15.60%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -22,7 +22,25 @@ public:
         return -1;
     }
     
+    int better(vector<int>& nums){
+        
+        int n = nums.size();
+        
+        unordered_map<int, int> mpp;
+        for(auto &x : nums)
+            mpp[x]++;
+        
+        for(auto &it : mpp){
+            
+            if(it.second == 1)
+                return it.first;
+        }
+        
+        return -1;
+        
+    }
+    
     int singleNumber(vector<int>& nums) {
-        return bruteForce(nums);
+        return better(nums);
     }
 };
