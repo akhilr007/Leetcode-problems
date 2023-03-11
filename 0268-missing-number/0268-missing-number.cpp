7a9497a0849e193d7268c33fcaa5bfc9a107bdb3
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-11 20:54:34 +0530

    Time: 18 ms (80.03%), Space: 18.5 MB (9.90%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int bruteForce(vector<int>& nums){
         
         int n = nums.size();
         for(int i=0; i<=n; i++){
@@ -19,5 +19,27 @@ public:
         }
         
         return -1;
+    }
+    
+    int better(vector<int>& nums){
+        
+        int n = nums.size();
+        vector<int> hashNums(n+1, 0);
+        
+        for(int i=0; i<n; i++)
+            hashNums[nums[i]]++;
+        
+        for(int i=0; i<=n; i++){
+            
+            if(hashNums[i] == 0)
+                return i;
+        }
+        
+        return -1;
+    }
+    
+    int missingNumber(vector<int>& nums) {
+        
+        return better(nums);
     }
 };
