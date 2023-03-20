0534f3be9ca42a3d2b2cdcb4991404454daac74a
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-20 13:27:29 +0530

    Time: 7 ms (99.69%), Space: 22 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -12,12 +12,14 @@ public:
         
         for(int i=1; i<temp.size()-1; i++){
             
+            if(n == 0)
+                return true;
             if(temp[i-1] == 0 && temp[i] == 0 && temp[i+1] == 0){
                 temp[i] = 1;
                 n -= 1;
             }
         }
         
-        return n <= 0;
+        return n == 0;
     }
 };
