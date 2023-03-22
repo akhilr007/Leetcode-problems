41531dbda24c7fcf82b681ee7c292ee74a4c3697
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-22 19:56:25 +0530

    Time: 452 ms (85.54%), Space: 123.7 MB (81.39%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,48 +1,51 @@
+// TIME COMPLEXITY -> O(V+E)
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
         
-        vector<pair<int, int>> adj[n+1];
-        
-        for(auto &it: roads){
-            
-            int src = it[0];
-            int nbr = it[1];
-            int dist = it[2];
-            
-            adj[src].push_back({nbr, dist});
-            adj[nbr].push_back({src, dist});
-        }
-        
-        queue<int> q;
-        q.push(1);
-        
-        int minScore = INT_MAX;
-        vector<int> vis(n+1, 0);
-        vis[1] = 1;
-        
-        while(!q.empty()){
-            
-            int size = q.size();
-            while(size--){
-                
-                int src = q.front();
-                q.pop();
-                
-                for(auto& adjNode: adj[src]){
-                    
-                    int nbr = adjNode.first;
-                    int dist = adjNode.second;
-                    minScore = min(minScore, dist);
-                    
-                    if(vis[nbr] == 0){
-                        q.push(nbr);
-                        vis[nbr] = 1;
-                    }
-                }
+      // creating an adjacency list
+      vector<pair<int, int>> adj[n+1]; // 1-based indexing
+
+      for(auto& road: roads){
+
+        int src = road[0];
+        int nbr = road[1];
+        int dist = road[2];
+
+        adj[src].push_back({nbr, dist});
+        adj[nbr].push_back({src, dist});
+      }
+
+      int minimumScore=INT_MAX;
+      // implementing BFS
+
+      queue<int> q;
+      q.push(1); // src node -> 1
+
+      vector<int> visited(n+1, 0); // visited list
+
+      while(!q.empty()){
+
+        int size = q.size();
+        while(size--){
+
+          int node = q.front();
+          q.pop();
+
+          for(auto& adjNode: adj[node]){
+
+            int nbr = adjNode.first;
+            int dist = adjNode.second;
+
+            minimumScore = min(minimumScore, dist);
+            if(visited[nbr] == 0){
+              q.push(nbr);
+              visited[nbr] = 1;
             }
+          }
         }
-        
-        return minScore;
+      }
+
+      return minimumScore;
     }
 };
