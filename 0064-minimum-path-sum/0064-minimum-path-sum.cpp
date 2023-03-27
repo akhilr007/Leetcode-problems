9e9a902dba8bd6f404da7896cf4b2c132715ea4b
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-27 12:17:07 +0530

    Time: 15 ms (20.14%), Space: 10.1 MB (36.04%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     // time complexity ->2^(n+m)
     // space complexity -> O(n+m)
+    // for memoization, time complexity -> O(n*m) , space complexity -> O(n*m) + O(n+m)
     int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
         
         if(i<0 || j<0)
@@ -22,7 +23,27 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return f(n-1, m-1, grid, dp);
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+        
+        // base case
+        for(int i=0; i<n; i++){
+            
+            for(int j=0; j<m; j++){
+                
+                if(i == 0 and j == 0){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                
+                int upperPath = 1e8, leftPath = 1e8;
+                if(i>0) upperPath = grid[i][j] + dp[i-1][j];
+                if(j>0) leftPath = grid[i][j] + dp[i][j-1];
+                
+                dp[i][j] = min(upperPath, leftPath);
+            }
+            
+        }
+        
+        return dp[n-1][m-1];
     }
 };
