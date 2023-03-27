ba6b1222a55af56a2589e30a79d7f697c9d991ba
Akhil Rajan <rajanakhil07@gmail.com>
2023-03-27 12:19:29 +0530

    Time: 7 ms (90.29%), Space: 9.8 MB (82.48%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -18,12 +18,14 @@ public:
         return dp[i][j] = min(upperPath, leftPath);
     }
     
+    // time complexity -> O(n*m)
+    // space complexity -> O(n*m)
     int minPathSum(vector<vector<int>>& grid) {
         
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<int>> dp(n, vector<int> (m, 0));
+        vector<int> cur(m, 0), prev(m, 0);
         
         // base case
         for(int i=0; i<n; i++){
@@ -31,19 +33,21 @@ public:
             for(int j=0; j<m; j++){
                 
                 if(i == 0 and j == 0){
-                    dp[i][j] = grid[i][j];
+                    cur[j] = grid[i][j];
                     continue;
                 }
                 
                 int upperPath = 1e8, leftPath = 1e8;
-                if(i>0) upperPath = grid[i][j] + dp[i-1][j];
-                if(j>0) leftPath = grid[i][j] + dp[i][j-1];
+                if(i>0) upperPath = grid[i][j] + prev[j];
+                if(j>0) leftPath = grid[i][j] + cur[j-1];
                 
-                dp[i][j] = min(upperPath, leftPath);
+                cur[j] = min(upperPath, leftPath);
             }
+            
+            prev = cur;
             
         }
         
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
