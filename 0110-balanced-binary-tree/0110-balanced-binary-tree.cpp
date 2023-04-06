04ba5ff21de5bdb044bcc87fcd439e53698cde41
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-06 19:56:53 +0530

    Time: 8 ms (89.29%), Space: 21 MB (59.98%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -8,19 +8,15 @@ public:
         int lh = height(root->left);
         int rh = height(root->right);
         
+        if(abs(lh-rh) > 1)
+            return -1;
+        if(lh == -1 or rh == -1)
+            return -1;
+        
         return max(lh, rh) + 1;
     }
     
     bool isBalanced(TreeNode* root) {
-        
-        if(root == NULL)
-            return true;
-        
-        bool leftSubtree = isBalanced(root->left);
-        bool rightSubtree = isBalanced(root->right);
-        
-        bool ans = abs(height(root->left) - height(root->right)) <= 1;
-        
-        return leftSubtree and rightSubtree and ans;
+        return height(root) != -1;
     }
 };
