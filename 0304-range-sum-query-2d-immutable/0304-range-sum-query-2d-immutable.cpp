36df8aac979d4538d90e5509d68e7c3c45d1478c
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-13 19:42:34 +0530

    Time: 395 ms (88.51%), Space: 142.4 MB (84.95%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,51 +1,51 @@
+// question link https://leetcode.com/problems/range-sum-query-2d-immutable/
+
 class NumMatrix {
 public:
     vector<vector<int>> cum_matrix;
-    NumMatrix(vector<vector<int>>& matrix) {
-        
+    NumMatrix(vector<vector<int>> &matrix){
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        cum_matrix.resize(n, vector<int> (m, 0));
-        
+
+        cum_matrix.resize(n, vector<int>(m, 0));
+
         cum_matrix[0][0] = matrix[0][0];
-        // for 1st row -> cumulative sum
-        for(int col_ind=1; col_ind<m; col_ind++){
-            cum_matrix[0][col_ind] = cum_matrix[0][col_ind-1] + matrix[0][col_ind];
+
+        // prefix sum for 1st row
+        // row -> constant col -> variable
+        for(int col=1; col<m; col++){
+            cum_matrix[0][col] = cum_matrix[0][col-1] + matrix[0][col];
         }
-        
-        // for 1st col -> cumulative sum
-        for(int row_ind=1; row_ind<n; row_ind++){
-            cum_matrix[row_ind][0] = cum_matrix[row_ind-1][0] + matrix[row_ind][0];
+
+        // prefix sum for 1st col
+        // col -> constant row -> variable
+        for(int row=1; row<n; row++){
+            cum_matrix[row][0] = cum_matrix[row-1][0] + matrix[row][0];
         }
-        
-        for(int row_ind=1; row_ind<n; row_ind++){
-            for(int col_ind=1; col_ind<m; col_ind++){
-                
-                cum_matrix[row_ind][col_ind] = matrix[row_ind][col_ind] + cum_matrix[row_ind][col_ind-1] + cum_matrix[row_ind-1][col_ind] - cum_matrix[row_ind-1][col_ind-1];
+
+        for(int row=1; row<n; row++){
+            for(int col=1; col<m; col++){
+
+                cum_matrix[row][col] =  matrix[row][col] + cum_matrix[row-1][col] + 
+                                        cum_matrix[row][col-1] - cum_matrix[row-1][col-1];
             }
         }
     }
-    
-    int sumRegion(int start_row, int start_col, int end_row, int end_col) {
-        
-        if(start_row == 0 and start_col == 0){
-            return cum_matrix[end_row][end_col];
+
+    int sumRegion(int startRow, int startCol, int endRow, int endCol){
+
+        if(startRow == 0 and startCol == 0){
+            return cum_matrix[endRow][endCol];
         }
-        else if(start_row == 0){
-            return cum_matrix[end_row][end_col] - cum_matrix[end_row][start_col-1];
+        else if(startRow == 0){
+            return cum_matrix[endRow][endCol] - cum_matrix[endRow][startCol-1];
         }
-        else if(start_col == 0){
-            return cum_matrix[end_row][end_col] - cum_matrix[start_row-1][end_col];
+        else if(startCol == 0){
+            return cum_matrix[endRow][endCol] - cum_matrix[startRow-1][endCol];
         }
         else{
-            return cum_matrix[end_row][end_col] - cum_matrix[start_row-1][end_col] - cum_matrix[end_row][start_col-1] + cum_matrix[start_row-1][start_col-1];
+            return cum_matrix[endRow][endCol] - cum_matrix[startRow-1][endCol] -
+                    cum_matrix[endRow][startCol-1] + cum_matrix[startRow-1][startCol-1];
         }
     }
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
