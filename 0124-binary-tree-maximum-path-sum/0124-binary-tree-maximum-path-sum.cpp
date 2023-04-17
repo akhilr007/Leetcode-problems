fe3c0be9d62e36598d9d07597f2a084a17820042
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-18 00:16:27 +0530

    Time: 28 ms (51.12%), Space: 28.4 MB (15.72%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,17 +1,6 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int dfs(TreeNode* root, int &max_sum){
+    int dfs(TreeNode* root, int& max_sum){
         
         if(root == NULL)
             return 0;
@@ -19,21 +8,21 @@ public:
         int left = dfs(root->left, max_sum);
         int right = dfs(root->right, max_sum);
         
-        // mujhe neeche hi answer mil gaya [left path, root, right path]
+        // subtree path with left and right 
         int subtree_with_left_and_right = left + right + root->val;
         
-        // right ya left me se koi ek choose karo kyuki ek me se kisi me -ve val h
+        // subtree path with left or right
         int subtree_with_left_or_right = root->val + max(left, right);
         
-        // right and left dono hi -ve h to sirf root pick karo
+        // only root
         int only_root = root->val;
         
-        max_sum = max({max_sum, subtree_with_left_and_right, subtree_with_left_or_right, only_root});
+        max_sum = max({subtree_with_left_and_right, subtree_with_left_or_right, only_root, max_sum});
         
-        // subtree path ko return nai kar sakte kyuki wo ek complete path h aur hum turn ho gaye 
+        // return hum log root ya subtree with left or right kar sakte h kyuki subtree path left and right khud me ek path h jo ki turn ho chuka hai
         return max(only_root, subtree_with_left_or_right);
-        
     }
+    
     int maxPathSum(TreeNode* root) {
         
         int max_sum = INT_MIN;
