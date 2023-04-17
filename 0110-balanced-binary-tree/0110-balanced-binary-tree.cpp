ecda2c6f8ee6717ddfe0ddb1967089ed85f58496
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-17 21:48:54 +0530

    Time: 138 ms (5.17%), Space: 20.9 MB (60.12%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,22 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int height(TreeNode* root){
+    int findHeight(TreeNode* root){
         
         if(root == NULL)
             return 0;
         
-        int lh = height(root->left);
-        int rh = height(root->right);
-        
-        if(abs(lh-rh) > 1)
-            return -1;
-        if(lh == -1 or rh == -1)
-            return -1;
+        int lh = findHeight(root->left);
+        int rh = findHeight(root->right);
         
         return max(lh, rh) + 1;
     }
     
     bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
+        
+        if(root == NULL)
+            return true;
+        
+        bool leftIsBalanced = isBalanced(root->left);
+        bool rightIsBalanced = isBalanced(root->right);
+        
+        int leftHeight = findHeight(root->left);
+        int rightHeight = findHeight(root->right);
+        
+        bool absDiff = abs(leftHeight - rightHeight) <= 1;
+        
+        return leftIsBalanced and rightIsBalanced && absDiff;
     }
 };
