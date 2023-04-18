94f131874864091444ebd11ba451365e25487920
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-19 00:40:24 +0530

    Time: 7 ms (51.57%), Space: 13.8 MB (18.97%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,52 +1,52 @@
-
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
-        vector<vector<int>> ans;
-        if(root == NULL)
-            return ans;
-        /*
-            for every vertical we have have levels and for every levels we have nodes
-        
-        */
-        map<int, map<int, multiset<int>>> mpp;
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        q.push({root, {0, 0}});
-        // { root , {vertical, level}}
-        
+        vector<vector<int>> res;
+        if(root == NULL) return res;
+
+        // har horizontal distance pe levels ho sakte h and har levels pe nodes
+        map < int, map<int, multiset<int>  > > mpp;
+
+        queue < pair<TreeNode*, pair<int, int> > > q;
+        // { node , {horizontal_distance, level }}
+
+        q.push({root, {0, 0} });
+
         while(!q.empty()){
-            
-            auto p = q.front();
+
+            auto it = q.front();
             q.pop();
-            
-            auto node = p.first;
-            int vertical = p.second.first;
-            int level = p.second.second;
-            
-            mpp[vertical][level].insert(node->val);
-            
+
+            TreeNode* node = it.first;
+            int horizontal_distance = it.second.first;
+            int level = it.second.second;
+
+            mpp[horizontal_distance][level].insert(node->val);
+
             if(node->left)
-                q.push({node->left, {vertical-1, level+1}});
+                q.push( {node->left, {horizontal_distance - 1, level + 1} } );
+
             if(node->right)
-                q.push({node->right, {vertical+1, level+1}});
-                
+                q.push( {node->right, {horizontal_distance + 1, level + 1} } );
         }
-        
-        for(auto p: mpp){
-            
+
+
+        for(auto p : mpp){
+
             vector<int> cols;
-            for(auto q: p.second){
-                
+            for(auto q : p.second){
+
                 multiset<int> nodes = q.second;
-                for(auto it: nodes){
-                    cols.push_back(it);
+
+                for(auto r: nodes){
+
+                    cols.push_back(r);
                 }
             }
-            
-            ans.push_back(cols);
+
+            res.push_back(cols);
         }
-        
-        return ans;
+        return res;
     }
 };
