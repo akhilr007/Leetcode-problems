8b944c02e4f2499e8102d0d8cddcc9a517d756eb
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-19 15:18:53 +0530

    Time: 11 ms (52.50%), Space: 17.3 MB (91.97%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -5,37 +5,37 @@ public:
         if(root == NULL)
             return 0;
         
-        int ans = 0;
+        queue < pair<TreeNode*, int> > q;
+        q.push( {root, 0} );
         
-        queue<pair<TreeNode*, int>> q; // {root, index} start from 9
-        q.push({root, 0});
+        int maxWidth = 0;
         
         while(!q.empty()){
             
             int size = q.size();
             int first, last;
-            int mmin = q.front().second;
             
             for(int i=0; i<size; i++){
                 
-                long cur_id = q.front().second;
-                auto node = q.front().first;
+                long current_index = q.front().second;
+                TreeNode* node = q.front().first;
+                
                 q.pop();
                 
                 if(i == 0)
-                    first = cur_id;
-                if(i == size-1)
-                    last = cur_id;
+                    first = current_index;
+                if(i == size - 1)
+                    last = current_index;
                 
                 if(node->left)
-                    q.push({node->left, cur_id*2 + 1});
+                    q.push( {node->left, current_index * 2 + 1} );
                 if(node->right)
-                    q.push({node->right, cur_id*2 + 2});
+                    q.push( {node->right, current_index * 2 + 2} );
             }
             
-            ans = max(ans, (last - first) + 1);
+            maxWidth = max(maxWidth, (last - first) + 1);
         }
         
-        return ans;
+        return maxWidth;
     }
 };
