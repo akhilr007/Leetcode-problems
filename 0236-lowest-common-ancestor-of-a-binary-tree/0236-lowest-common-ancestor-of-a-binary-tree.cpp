84a09fc3cbcba00b30f2c5278a7a00ffa8d04c0e
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-19 13:49:36 +0530

    Time: 20 ms (41.17%), Space: 17.4 MB (9.43%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,27 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
+    bool getPath(TreeNode* root, TreeNode* target, vector<TreeNode*>& result){
+
+        if(root == NULL)
+            return false;
+        
+        result.push_back(root);
+        
+        if(root == target)
+            return true;
+
+        bool left = getPath(root->left, target, result);
+        if(left == true) return true;
+
+        bool right = getPath(root->right, target, result);
+        if(right == true) return true;
+
+        result.pop_back();
+        return false;
+    }
+    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        if(root == NULL || root == p || root == q)
-            return root;
-        
-        
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        
-        if(!left)
-            return right;
-        else if(!right)
-            return left;
-        else return root;
+        vector<TreeNode*> rootToNodeP, rootToNodeQ;
+
+        getPath(root, p, rootToNodeP);
+        getPath(root, q, rootToNodeQ);
+
+        int n = rootToNodeP.size() , m = rootToNodeQ.size();
+
+        int i;
+        for(i=0; i<n && i<m; i++){
+            if(rootToNodeP[i]->val != rootToNodeQ[i]->val)
+                break;
+        }
+
+        return rootToNodeP[i-1];
     }
 };
