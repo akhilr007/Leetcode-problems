58f98a0bc2022843e83be6d661c405b7fe2b7f62
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-20 00:51:00 +0530

    Time: 55 ms (44.53%), Space: 29.1 MB (63.59%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -1,54 +1,53 @@
 class Codec {
 public:
-    string serialize(TreeNode* root) {
-        
+    string serialize(TreeNode* root){
+
         if(root == NULL)
             return "";
-        
-        string s="";
-        
+
+        string result = "";
+
         queue <TreeNode*> q;
         q.push(root);
-        
+
         while(!q.empty()){
-            
+
             auto node = q.front();
             q.pop();
-            
+
             if(node == NULL){
-                s.append("#,");
+                result.append("#,");
                 continue;
             }
-            
-            s.append(to_string(node->val) + ',');
+
+            result.append(to_string(node->val) + ",");
             q.push(node->left);
             q.push(node->right);
         }
-        
-        return s;
-        
+
+        return result;
+
     }
 
-    TreeNode* deserialize(string data) {
-        
+    TreeNode* deserialize(string data){
+
         if(data.size() == 0)
             return NULL;
-        
+
         stringstream s(data);
         string str;
-
         getline(s, str, ',');
-        
+
         TreeNode* root = new TreeNode(stoi(str));
-        
+
         queue<TreeNode*> q;
         q.push(root);
-        
+
         while(!q.empty()){
-            
+
             auto node = q.front();
             q.pop();
-            
+
             getline(s, str, ',');
             if(str == "#"){
                 node->left = NULL;
@@ -57,7 +56,8 @@ public:
                 node->left = new TreeNode(stoi(str));
                 q.push(node->left);
             }
-            
+
+
             getline(s, str, ',');
             if(str == "#"){
                 node->right = NULL;
@@ -67,8 +67,7 @@ public:
                 q.push(node->right);
             }
         }
-        
+
         return root;
-        
     }
 };
