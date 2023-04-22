74ec6d8897496642162e8a9abeeafe19c97bc5b7
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-23 01:30:44 +0530

    Time: 38 ms (77.71%), Space: 36.9 MB (75.20%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,11 +1,12 @@
 class BSTIterator{
 private:
     stack<TreeNode*> bst;
-    bool reverse = false; // true -> before, false -> next
-    
+    bool reverse = true;
+
     void pushAll(TreeNode* root){
-        
+
         while(root){
+
             bst.push(root);
             if(reverse){
                 root = root->right;
@@ -15,61 +16,55 @@ private:
             }
         }
     }
-    
+
 public:
     BSTIterator(TreeNode* root, bool reverse){
-        
+
         this->reverse = reverse;
         pushAll(root);
-        
     }
-    
+
     int next(){
-        
+
         TreeNode* topNode = bst.top();
         bst.pop();
-        
+
         if(reverse){
             pushAll(topNode->left);
         }
         else{
             pushAll(topNode->right);
         }
-        
+
         return topNode->val;
     }
-    
+
     bool hasNext(){
         return bst.empty() == false;
     }
-
 };
 
 class Solution {
 public:
-    bool findTarget(TreeNode* root, int k) {
-        
-        BSTIterator l(root, false);
-        BSTIterator r(root, true);
-        
+    bool findTarget(TreeNode* root, int k){
+
+        BSTIterator l(root, false); // next
+        BSTIterator r(root, true); // before
+
         int i = l.next();
         int j = r.next();
-        
-        cout << i << " " << j << endl;
-        
+
         while(i < j){
-            
-            if(i + j == k)
+
+            if( i + j == k)
                 return true;
-            
-            if(i + j < k){
+
+            if(i + j < k)
                 i = l.next();
-            }
-            else{
+            else 
                 j = r.next();
-            }
         }
-        
+
         return false;
     }
 };
