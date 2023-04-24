48bfc26417e6fba503447030d80a29d1a9d6e893
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-24 23:07:51 +0530

    Time: 1 ms (98.37%), Space: 42.7 MB (96.89%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solve(int i, int j, int[][] matrix, int[][] dp){
         
-        if(i<0 || j<0 || i>=matrix.length || j>=matrix.length)
+        if(j<0 || j>=matrix.length)
             return 1000005;
         
         if(i == matrix.length - 1){
@@ -11,12 +11,9 @@ class Solution {
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        int down = 1000005, downLeft = 1000005, downRight = 1000005;
-        if(j>=1)
-            downLeft = matrix[i][j] + solve(i+1, j-1, matrix, dp);
-        if(j<matrix.length-1)
-            downRight = matrix[i][j] + solve(i+1, j+1, matrix, dp);
-        down = matrix[i][j] + solve(i+1, j, matrix, dp);
+        int downLeft = matrix[i][j] + solve(i+1, j-1, matrix, dp);
+        int downRight = matrix[i][j] + solve(i+1, j+1, matrix, dp);
+        int down = matrix[i][j] + solve(i+1, j, matrix, dp);
         
         return dp[i][j] = Math.min(downLeft, Math.min(down, downRight));
         
