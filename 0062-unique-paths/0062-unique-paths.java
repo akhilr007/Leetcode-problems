04fa9678cadaebb59f31c29c3b615cc323cc2a1a
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-24 20:36:50 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (64.00%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -21,10 +21,26 @@ class Solution {
         
         int[][] dp = new int[m][n];
         
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int up = 0, left=0;
+                if(i-1>=0)
+                    up = dp[i-1][j];
+                if(j-1>=0)
+                    left = dp[i][j-1];
+                
+                dp[i][j] = up + left;
+            }
         }
         
-        return solve(m-1, n-1, dp);
+        return dp[m-1][n-1];
     }
 }
