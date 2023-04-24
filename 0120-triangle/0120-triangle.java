a88b163debf7e4f4fc2d859e9bf404fc556ead3c
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-24 21:49:18 +0530

    Time: 6 ms (33.24%), Space: 42.3 MB (49.75%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -18,9 +18,22 @@ class Solution {
         int m = triangle.size();
         
         int[][] dp = new int[m][m];
-        for(int[] row: dp)
-            Arrays.fill(row, -1);
         
-        return solve(0, 0, triangle, dp);
+        for(int j=0; j<m; j++){
+            dp[m-1][j] = triangle.get(m-1).get(j);
+        }
+        
+        for(int i=m-2; i>=0; i--){
+            
+            for(int j=i; j>=0; j--){
+                
+                int down = triangle.get(i).get(j) + solve(i+1, j, triangle, dp);
+                int downRight = triangle.get(i).get(j) + solve(i+1, j+1, triangle, dp);
+
+                dp[i][j] = Math.min(down, downRight);
+            }
+        }
+        
+        return dp[0][0];
     }
 }
