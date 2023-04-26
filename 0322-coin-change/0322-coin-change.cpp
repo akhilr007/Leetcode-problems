aff76e4ea25d3f032320e6ebd42811d1c94dbfe3
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-27 00:05:59 +0530

    Time: 94 ms (69.63%), Space: 53.6 MB (7.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -46,12 +46,39 @@ public:
         return dp[n-1][amount];
     }
     
+    int optimal(vector<int> coins, int amount){
+        
+        int n = coins.size();
+        vector<int> prev(amount + 1, 1e8), cur(amount+1, 1e8);
+        
+        for(int a=0; a<=amount; a++){
+            if(a % coins[0] == 0)
+                prev[a] = a / coins[0];
+        }
+        
+        for(int index=1; index<n; index++){
+            for(int a=0; a<=amount; a++){
+                
+                int not_taken = prev[a];
+                int taken = 1e8;
+                if(coins[index] <= a)
+                    taken = 1 + cur[a-coins[index]];
+                
+                cur[a] = min(not_taken, taken);
+            }
+            
+            prev = cur;
+        }
+        
+        return prev[amount];
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size();
         
         vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        int ans = solve(n-1, amount, coins, dp);
+        int ans = optimal(coins, amount);
         return ans >= 1e8 ? -1 : ans;
 ;    }
 };
