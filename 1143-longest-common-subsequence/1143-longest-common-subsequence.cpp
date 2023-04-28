92bc9062ba7847901d4021f9f19945deda3e7707
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-28 20:31:36 +0530

    Time: 81 ms (31.85%), Space: 24.5 MB (25.34%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,87 +1,29 @@
 class Solution {
 public:
-    // time complexity -> O(2^n)
-    // space complexity -> O(n1 or n2)
-    int solveRec(int i1, int i2, string& text1, string& text2){
+    int solve(int i, int j, string& s1, string& s2, vector<vector<int>>& dp){
         
-        if(i1 < 0 || i2 < 0) return 0;
+        if(i < 0 || j < 0)
+            return 0;
         
-        if(text1[i1] == text2[i2]){
-            return 1 + solveRec(i1-1, i2-1, text1, text2);
-        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-        else{
-            
-            return max(solveRec(i1-1, i2, text1, text2), solveRec(i1, i2-1, text1, text2));
-            
-        }
+        int ans = 0;
         
-    }
-    
-    // time complexity -> O(N*M)
-    // space complexity -> O(N*M) + O(len of (s1) or len of (s2)) recursion stack
-    int solveMem(int i1, int i2, string& text1, string& text2, 
-                vector<vector<int>>& dp){
+        if(s1[i] == s2[j])
+            ans = 1 + solve(i-1, j-1, s1, s2, dp);
+        else
+            ans = max(solve(i-1, j, s1, s2, dp), solve(i, j-1, s1, s2, dp));
         
-        if(i1 < 0 || i2 < 0) return 0;
-        if(dp[i1][i2] != -1) return dp[i1][i2];
-        
-        if(text1[i1] == text2[i2]){
-            return dp[i1][i2] = 1 + solveMem(i1-1, i2-1, text1, text2, dp);
-        }
-        
-        else{
-            
-            return dp[i1][i2] = max(solveMem(i1-1, i2, text1, text2, dp), solveMem(i1, i2-1, text1, text2, dp));
-            
-        }
-    }
-    
-    // time complexity -> O(n*m)
-    // space complexity -> O(n*m)
-    int solveTab(string& text1, string& text2){
-        
-        int n = text1.length();
-        int m = text2.length();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                
-                if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }   
-        }
-        
-        return dp[n][m];
-    }
-    
-    // time complexity -> O(n * m)
-    // space complexity -> O(m)
-    int solveOptimal(string& text1, string& text2){
-        
-        int n = text1.length();
-        int m = text2.length();
-        vector<int> prev(m+1, 0), cur(m+1, 0);
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                
-                if(text1[i-1] == text2[j-1]){
-                    cur[j] = 1 + prev[j-1];
-                }
-                else cur[j] = max(prev[j], cur[j-1]);
-            }   
-            prev = cur;
-        }
-        
-        return cur[m];
+        return dp[i][j] = ans;
     }
     
     int longestCommonSubsequence(string text1, string text2) {
         
-        return solveTab(text1, text2);
+        int n = text1.size();
+        int m = text2.size();
+        
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return solve(n-1, m-1, text1, text2, dp);
     }
 };
