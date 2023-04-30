7f4e8898c05dbb703870830a5cf479deff38539f
Akhil Rajan <rajanakhil07@gmail.com>
2023-04-30 23:43:41 +0530

    Time: 72 ms (23.62%), Space: 42 MB (21.26%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -23,12 +23,44 @@ public:
         
     }
     
+    int tabulation(string s, string t){
+        
+        int n = s.size();
+        int m = t.size();
+        
+        vector<vector<double>> dp(n+1, vector<double> (m+1, 0));
+        
+        // if j<0 return 1;
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        // if i<0 return 0 // all the table is already 0
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                
+                if(s[i-1] == t[j-1]){
+                    
+                    double take = dp[i-1][j-1];
+                    double not_take = dp[i-1][j];
+                    
+                    dp[i][j] = take + not_take;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return (int)dp[n][m];
+    }
+    
     int numDistinct(string s, string t) {
         
         int n = s.size();
         int m = t.size();
         
         vector<vector<int>> dp(n, vector<int> (m, -1));
-        return solve(n-1, m-1, s, t, dp);
+        return tabulation(s, t);
     }
 };
