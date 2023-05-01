43e76fc7d27949fbc4bd196aa480ce15eed7de30
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-01 21:59:27 +0530

    Time: 18 ms (47.04%), Space: 8.9 MB (64.57%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -65,12 +65,47 @@ public:
         
     }
     
+    int optimal(string s, string t){
+        
+        int n = s.size();
+        int m = t.size();
+        
+        vector<int> prev(m+1, 0), cur(m+1, 0);
+        
+        // if(i<0) return j+1
+        for(int j=0; j<=m; j++)
+            prev[j] = j;
+        
+        for(int i=1; i<=n; i++){
+            cur[0] = i;
+            for(int j=1; j<=m; j++){
+                
+                if(s[i-1] == t[j-1])
+                {
+                    cur[j] = prev[j-1];
+                    continue;
+                }
+                    
+                int insert = 1 + cur[j-1];
+                int remove = 1 + prev[j];
+                int replace = 1 + prev[j-1];
+                
+                cur[j] = min({insert, remove, replace});
+            }
+            
+            prev = cur;
+        }
+        
+        return prev[m];
+        
+    }
+    
     int minDistance(string s, string t) {
         
         int n = s.size();
         int m = t.size();
         
         vector<vector<int>> dp(n, vector<int> (m, -1));
-        return tabulation(s, t);
+        return optimal(s, t);
     }
 };
