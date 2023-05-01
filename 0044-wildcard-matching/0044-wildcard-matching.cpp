1fdcd911f32ce5ee8bae3653a594e475ec676a9b
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-02 00:46:41 +0530

    Time: 69 ms (53.56%), Space: 26.9 MB (45.51%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -82,6 +82,51 @@ public:
 
         return dp[n][m];
     }
+    
+    bool optimal(string s, string p){
+
+        int n = p.size();
+        int m = s.size();
+
+       vector<bool> prev(m+1, false), cur(m+1, false);
+
+        prev[0] = true;
+
+        for(int j=1; j<=m; j++)
+            prev[j] = false;
+
+
+        for(int i=1; i<=n; i++){
+
+            bool flag = true;
+            for(int ii=1; ii<=i; ii++){
+                if(p[ii-1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            cur[0] = flag;
+
+            for(int j=1; j<=m; j++){
+
+                if(p[i-1] == s[j-1] || p[i-1] == '?'){
+                    cur[j] = prev[j-1];
+                }
+                else if(p[i-1] == '*'){
+                    cur[j] = prev[j] || cur[j-1];
+                }
+                else{
+                    cur[j] = false;
+                }
+            }
+            
+            prev = cur;
+        }
+
+        return prev[m];
+    }
 
     bool isMatch(string s, string p){
 
@@ -89,6 +134,6 @@ public:
         int m = s.size();
 
         vector<vector<int>> dp(n, vector<int> (m, -1));
-        return tabulation(s, p);
+        return optimal(s, p);
     }
 };
