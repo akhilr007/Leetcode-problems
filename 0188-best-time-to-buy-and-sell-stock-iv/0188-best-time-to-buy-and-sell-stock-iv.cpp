2a3ef2d7f3ca7e9af518e171c62944fd277e5d67
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-03 20:34:55 +0530

    Time: 7 ms (95.15%), Space: 10.8 MB (94.76%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -38,8 +38,41 @@ public:
         return ahead[1][k];
     }
     
+    int optimalT(vector<int>& prices, int k){
+        
+        int n = prices.size();
+        
+        vector<int> ahead(2*k+1, 0), cur(2*k+1, 0);
+        
+        for(int index=n-1; index>=0; index--){
+            for(int ts=2*k-1; ts>=0; ts--){
+                
+                int profit = 0;
+                if(ts%2 == 0){
+                    // buy
+                    int bought = -prices[index] + ahead[ts+1];
+                    int notBought = 0 + ahead[ts];
+
+                    profit = max(bought, notBought);
+                }
+                else{
+                    // sell
+                    int sold = prices[index] + ahead[ts+1];
+                    int notSold = 0 + ahead[ts];
+
+                    profit = max(sold, notSold);
+                }
+                
+                cur[ts] = profit;
+            }
+            ahead = cur;
+        }
+        
+        return ahead[0];
+    }
+    
     int maxProfit(int k, vector<int>& prices) {
         
-        return optimal(prices, k);
+        return optimalT(prices, k);
     }
 };
