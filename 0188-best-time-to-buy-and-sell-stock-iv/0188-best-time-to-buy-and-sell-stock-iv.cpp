00e7730e723c7454352bf31e57490af23c3b9061
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-03 20:48:13 +0530

    Time: 25 ms (46.45%), Space: 13.8 MB (60.52%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,78 +1,38 @@
 class Solution {
 public:
-    int optimal(vector<int>& prices, int k){
+    int solve(int index, int transactionNo, int k, vector<int>& prices,
+             vector<vector<int>>& dp){
         
-        int n = prices.size();
-        vector<vector<int>> ahead(2, vector<int> (k+1, 0));
-        vector<vector<int>> cur(2, vector<int> (k+1, 0));
+        if(index >= prices.size() || transactionNo == 2*k)
+            return 0;
         
-        for(int index=n-1; index>=0; index--){
+        if(dp[index][transactionNo] != -1)
+            return dp[index][transactionNo];
+        
+        int profit = 0;
+        if(transactionNo % 2 == 0){
+            // buy
+            int bought = -prices[index] + solve(index+1, transactionNo+1, k, prices, dp);
+            int notBought = 0 + solve(index+1, transactionNo, k, prices, dp);
             
-            for(int canBuy=0; canBuy<=1; canBuy++) {
-                
-                // for every cap = 0 , result will be 0 so no point of starting with it
-                for(int cap=1; cap<=k; cap++){
-                    
-                    int profit=0;
-                    if(canBuy){
-                        int bought = -prices[index] + ahead[0][cap];
-                        int notBought = 0 + ahead[1][cap];
-
-                        profit = max(bought, notBought);
-                    }
-                    else{
-                        int sold = prices[index] + ahead[1][cap-1];
-                        int notSold = 0 + ahead[0][cap];
-
-                        profit = max(sold, notSold);
-                    }
-
-                    cur[canBuy][cap] = profit;
-                    
-                }
-                
-                ahead = cur;
-            }
+            profit = max(bought, notBought);
+        }
+        else{
+            // sell
+            int sold = prices[index] + solve(index+1, transactionNo+1, k, prices, dp);
+            int notSold = 0 + solve(index+1, transactionNo, k, prices, dp);
+            
+            profit = max(sold, notSold);
         }
         
-        return ahead[1][k];
-    }
-    
-    int optimalT(vector<int>& prices, int k){
-        
-        int n = prices.size();
-        
-        vector<int> ahead(2*k+1, 0), cur(2*k+1, 0);
-        
-        for(int index=n-1; index>=0; index--){
-            for(int ts=2*k-1; ts>=0; ts--){
-                
-                int profit = 0;
-                if(ts%2 == 0){
-                    // buy
-                    int bought = -prices[index] + ahead[ts+1];
-                    int notBought = 0 + ahead[ts];
-
-                    profit = max(bought, notBought);
-                }
-                else{
-                    // sell
-                    int sold = prices[index] + ahead[ts+1];
-                    int notSold = 0 + ahead[ts];
-
-                    profit = max(sold, notSold);
-                }
-                
-                cur[ts] = profit;
-            }
-            ahead = cur;
-        }
-        
-        return ahead[0];
+        return dp[index][transactionNo] = profit;
     }
     
     int maxProfit(int k, vector<int>& prices) {
         
-        return optimalT(prices, k);
+        int n = prices.size();
+        
+        vector<vector<int>> dp(n, vector<int>(2*k, -1));
+        return solve(0, 0, k, prices, dp);
     }
 };
