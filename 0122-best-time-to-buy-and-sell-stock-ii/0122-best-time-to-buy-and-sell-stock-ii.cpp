d1232812cf25e7d306812963f09720c2dd143bd9
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-03 14:47:07 +0530

    Time: 15 ms (19.69%), Space: 17.6 MB (7.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,26 +4,25 @@ public:
     // space complexity -> O(n*2 + n)
     int solve(int index, int canBuy, vector<int>& prices, int n, vector<vector<int>>& dp){
         
-        if(index >= n)
-            return 0;
-        
+        if(index >= n) return 0;
+
         if(dp[index][canBuy] != -1)
             return dp[index][canBuy];
-        
+
         int profit = 0;
         if(canBuy){
             int bought = -prices[index] + solve(index+1, 0, prices, n, dp);
             int notBought = 0 + solve(index+1, 1, prices, n, dp);
-            
+
             profit = max(bought, notBought);
         }
         else{
             int sold = prices[index] + solve(index+1, 1, prices, n, dp);
             int notSold = 0 + solve(index+1, 0, prices, n, dp);
-            
+
             profit = max(sold, notSold);
         }
-        
+
         return dp[index][canBuy] = profit;
     }
     
@@ -139,6 +138,6 @@ public:
         
         int n = prices.size();
         vector<vector<int>> dp(n, vector<int> (2, -1));
-        return greedy(prices);
+        return solve(0, 1, prices, n, dp);
     }
 };
