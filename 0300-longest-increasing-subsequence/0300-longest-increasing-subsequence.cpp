48eaad14653a51d2dded585dacacf55ca4c8b167
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-04 20:21:34 +0530

    Time: 1514 ms (11.57%), Space: 292.3 MB (18.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // tc -> O(n*n)
+    // sc -> O(n*n) + O(n)
     int solve(int index, int prev_index, vector<int>& nums, vector<vector<int>>& dp){
         
         if(index >= nums.size())
@@ -17,6 +19,28 @@ public:
         return dp[index][prev_index+1] = max(notTake, take);
     }
     
+    int tabulation(vector<int>& nums){
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        
+        for(int index=n-1; index>=0; index--){
+            
+            for(int prev_index=index-1; prev_index>=-1; prev_index--){
+                
+                int notTake = 0 + dp[index+1][prev_index];
+        
+                int take = 0;
+                if(prev_index == -1 || nums[index] > nums[prev_index])
+                    take = 1 + dp[index+1][index];
+
+                dp[index][prev_index+1] = max(notTake, take);
+            }
+        }
+        
+        return dp[0][0];
+        
+    }
     
     int lengthOfLIS(vector<int>& nums) {
         
