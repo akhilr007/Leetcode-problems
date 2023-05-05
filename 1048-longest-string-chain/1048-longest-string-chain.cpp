ca605408ba82e01ea190e2ffe04dee7c45fcd548
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-05 14:41:20 +0530

    Time: 142 ms (44.87%), Space: 13.3 MB (83.44%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    
-    bool checkPossible(string& s1, string& s2){
+    bool isStringChain(const string& w1, const string& w2) {
         
-        if(s1.size() != s2.size() + 1) return false;
+        if(w1.size() != w2.size() + 1)
+            return false;
         
-        int i=0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         
-        while(i<s1.size()){
+        while(i<w1.size()){
             
-            if(s1[i] == s2[j]){
+            if(j<w2.size() && w1[i] == w2[j]){
                 i++;
                 j++;
             }
@@ -19,36 +19,33 @@ public:
             }
         }
         
-        if(i==s1.size() && j==s2.size()) return true;
-        
-        return false;
-    }
-    
-    static bool comp(string& s1, string& s2){
-        return s1.size() < s2.size();
+        return i == w1.size() && j == w2.size();
     }
     
     int longestStrChain(vector<string>& words) {
         
-        sort(words.begin(), words.end(), comp);
-        
         int n = words.size();
-        vector<int> dp(n, 0);
-        dp[0] = 1;
-        int maxLength=1;
         
-        for(int i=1; i<n; i++){
-            int maxi = 0;
-            for(int j=0; j<i; j++){
-                if(checkPossible(words[i], words[j])){
-                    maxi = max(maxi, dp[j]);
+        sort(begin(words), end(words), [](const string& w1, const string& w2) {
+            return w1.size() < w2.size();
+        } );
+        
+        vector<int> dp(n, 1);
+        int maxi = 0;
+        
+        for(int index=0; index<n; index++){
+            
+            for(int prev_index=0; prev_index<index; prev_index++){
+                
+                if(isStringChain(words[index], words[prev_index])){
+                    
+                    dp[index] = max(dp[index], 1+dp[prev_index]);
                 }
             }
             
-            dp[i] = maxi+1;
-            maxLength = max(dp[i], maxLength);
+            maxi = max(maxi, dp[index]);
         }
         
-        return maxLength;
+        return maxi;
     }
 };
