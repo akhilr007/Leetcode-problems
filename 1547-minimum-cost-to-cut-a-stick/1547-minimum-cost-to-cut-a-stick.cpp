7306b2734b99e2a4efde2be93419e3d12d1aec7f
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-07 00:06:08 +0530

    Time: 66 ms (70.12%), Space: 12 MB (5.83%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -19,6 +19,34 @@ public:
         return dp[i][j] = mini;
     }
     
+    
+    int tabulation(int n, vector<int>& cuts){
+        
+        int size = cuts.size()-2; // skipping 0 and n
+        
+        vector<vector<int>> dp(size+2, vector<int>(size+2, 0));
+        
+        // solve(1->size)
+        for(int i=size; i>=1; i--){
+            
+            for(int j=1; j<=size; j++){
+                
+                if(i > j) continue;
+                
+                int mini = 1e9;
+                for(int ind=i; ind<=j; ind++){
+                    
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(mini, cost);
+                }
+                
+                dp[i][j] = mini;
+            }
+        }
+        
+        return dp[1][size];
+    }
+    
     int minCost(int n, vector<int>& cuts) {
         
         int size = cuts.size();
@@ -29,6 +57,6 @@ public:
         sort(begin(cuts), end(cuts));
         
         vector<vector<int>> dp(size+1, vector<int> (size+1, -1));
-        return solve(1, size, cuts, dp);
+        return tabulation(n, cuts);
     }
 };
