846bf33aaf9448d9653f5a0c09b0d12daa793c63
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-08 00:16:42 +0530

    Time: 596 ms (17.82%), Space: 6.6 MB (63.04%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,69 +1,68 @@
 class Solution {
 public:
     bool isPalindrome(int i, int j, string& s){
-        
+
         while(i<j){
-            
             if(s[i] != s[j])
                 return false;
             
             i++;
             j--;
         }
-        
+
         return true;
     }
-    
+
     int solve(int i, int n, string& s, vector<int>& dp){
-        
+
         if(i == n)
             return 0;
-        
+
         if(dp[i] != -1)
             return dp[i];
-        
-        int minCuts = 1e9;
+
+        int min_cuts = 1e9; // max cuts for a string to be palindrome is n-1
+
         for(int j=i; j<n; j++){
-            
+
             if(isPalindrome(i, j, s)){
                 int cost = 1 + solve(j+1, n, s, dp);
-                minCuts = min(minCuts, cost);
+                min_cuts = min(min_cuts, cost);
             }
-                
         }
-        
-        return dp[i] = minCuts;
+
+        return dp[i] = min_cuts;
     }
-    
+
     int tabulation(string s){
-        
+
         int n = s.size();
+
         vector<int> dp(n+1, 0);
-        
+
         for(int i=n-1; i>=0; i--){
-            
-            int minCuts = 1e9;
-            
+
+            int min_cuts = 1e9;
+
             for(int j=i; j<n; j++){
-                
+
                 if(isPalindrome(i, j, s)){
                     int cost = 1 + dp[j+1];
-                    minCuts = min(minCuts, cost);
+                    min_cuts = min(min_cuts, cost);
                 }
             }
-            
-            dp[i] = minCuts;
+
+            dp[i] = min_cuts;
         }
-        
+
         return dp[0] - 1;
     }
-    
-    int minCut(string s) {
-        
+
+    int minCut(string s){
+
         int n = s.size();
-        
-        return tabulation(s);
+
         vector<int> dp(n, -1);
-        return solve(0, n, s, dp) - 1; // it makes cuts then go to end so -1 from ans;
+        return solve(0, n, s, dp) - 1 ; // subtract -1 from solution because at the end we are making 1 partition extra
     }
 };
