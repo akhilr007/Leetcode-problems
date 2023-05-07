82604c041c0ac0172c720adb717d6713f393d8fe
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-08 00:40:17 +0530

    Time: 19 ms (60.36%), Space: 8.6 MB (74.52%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -25,9 +25,37 @@ public:
         return dp[i] = ans;
     }
     
+    int tabulation(vector<int>& arr, int k){
+        
+        int n = arr.size();
+        vector<int> dp(n+1, 0);
+        
+        for(int i=n-1; i>=0; i--){
+            
+            int maxi = INT_MIN;
+            int ans = INT_MIN;
+            int len = 0;
+
+            for(int j=i; j<min(n, i+k); j++){
+
+                len++;
+                maxi = max(maxi, arr[j]);
+
+                int sum = len * maxi + dp[j+1];
+
+                ans = max(ans, sum);
+            }
+            
+            dp[i] = ans;
+        
+        }
+        
+        return dp[0];
+    }
+    
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         
         vector<int> dp(arr.size(), -1);
-        return solve(0, arr, k, dp);
+        return tabulation(arr, k);
     }
 };
