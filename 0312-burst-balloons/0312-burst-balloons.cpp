9e416c14242bca9f61e2c98b836afe64b34b7000
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-07 19:55:11 +0530

    Time: 455 ms (61.77%), Space: 12.6 MB (23.87%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -2,30 +2,62 @@ class Solution {
 public:
     int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp){
         
-        if(i>j)
+        if(i > j)
             return 0;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
         int maxi = 0;
-        for(int ind=i; ind<=j; ind++){
+        
+        for(int k=i; k<=j; k++){
             
-            int c1 = i-1>=0 ? nums[i-1] : 1;
-            int c2 = j+1<nums.size() ? nums[j+1] : 1;
+            int cost = nums[i-1] * nums[k] * nums[j+1] + solve(i, k-1, nums, dp) +
+                solve(k+1, j, nums, dp);
             
-            int cost = (c1 * c2 * nums[ind]) + solve(i, ind-1, nums, dp) + solve(ind+1, j, nums, dp);
-            maxi = max(cost, maxi);
+            maxi = max(maxi, cost);
         }
         
         return dp[i][j] = maxi;
+    }
+    
+    int tabulation(vector<int>& nums){
+        
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        
+        for(int i=n-2; i>=1; i--){
+            
+            for(int j=1; j<=n-2; j++){
+                
+                if(i > j) continue;
+                
+                int maxi = 0;
+                for(int k=i; k<=j; k++){
+                    
+                    int cost = nums[i-1] * nums[k] * nums[j+1] + dp[i][k-1] + 
+                        dp[k+1][j];
+                    
+                    maxi = max(maxi, cost);
+                }
+                
+                dp[i][j] = maxi;
+            }
+        }
+        
+        return dp[1][n-2];
     }
     
     int maxCoins(vector<int>& nums) {
         
         int n = nums.size();
         
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return solve(0, n-1, nums, dp);
+        nums.push_back(1);
+        nums.insert(nums.begin(), 1);
+        
+        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        return tabulation(nums);
+        
+        
     }
 };
