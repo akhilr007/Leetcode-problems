54162eb0aacfa6cd989d64afb8d6ae629cbc1b38
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-08 22:06:38 +0530

    Time: 58 ms (45.70%), Space: 15.8 MB (43.12%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,94 +1,91 @@
-class Solution {
+class Solution{
 public:
-    
     vector<int> nextSmallerLeft(vector<int>& arr){
-        
+
         int n = arr.size();
         stack<int> st;
-        
+
         vector<int> nsl(n);
-        
+
         for(int i=0; i<n; i++){
-            
+
             while(st.size()>0 && arr[st.top()] >= arr[i])
                 st.pop();
-            
+
             if(st.size() == 0)
                 nsl[i] = -1;
             else
                 nsl[i] = st.top();
-            
+
             st.push(i);
         }
-        
+
         return nsl;
     }
-    
+
     vector<int> nextSmallerRight(vector<int>& arr){
-        
+
         int n = arr.size();
         stack<int> st;
-        
+
         vector<int> nsr(n);
-        
+
         for(int i=n-1; i>=0; i--){
-            
+
             while(st.size()>0 && arr[st.top()] >= arr[i])
                 st.pop();
-            
+
             if(st.size() == 0)
                 nsr[i] = n;
             else
                 nsr[i] = st.top();
-            
+
             st.push(i);
         }
-        
+
         return nsr;
     }
-    
-    int maxAreaHistogram(vector<int>& arr){
-        
+
+    int maximumAreaHistogram(vector<int>& arr){
+
         vector<int> nsl = nextSmallerLeft(arr);
         vector<int> nsr = nextSmallerRight(arr);
-        
+
         int maxArea = 0;
         
         for(int i=0; i<arr.size(); i++){
-            
+
             int height = arr[i];
             int width = nsr[i] - nsl[i] - 1;
-            
-            maxArea = max(maxArea, height*width);
+
+            maxArea = max(maxArea, height * width);
         }
-        
+
         return maxArea;
-        
+
     }
-    
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        
+
+    int maximalRectangle(vector<vector<char>>& matrix){
+
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        vector<int> heights(m, 0);
-        
+
+        vector<int> heights(m);
+
         int maxArea = 0;
-        
+
         for(int i=0; i<n; i++){
-            
             for(int j=0; j<m; j++){
-                
-                if(matrix[i][j] == '0'){
+
+                if(matrix[i][j] == '0')
                     heights[j] = 0;
-                }
-                else{
+                else
                     heights[j] += 1;
-                }
             }
-            
-            maxArea = max(maxArea, maxAreaHistogram(heights));
+
+            maxArea = max(maxArea, maximumAreaHistogram(heights));
         }
+
         return maxArea;
     }
 };
