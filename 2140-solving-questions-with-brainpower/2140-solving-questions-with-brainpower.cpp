c5c486b464e1c68455ac14b9d7b42375f10ad89a
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-12 14:23:13 +0530

    Time: 398 ms (26.52%), Space: 118.4 MB (67.19%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -16,11 +16,31 @@ public:
         return dp[index] = max(take , notTake);
     }
     
+    long long tabulation(vector<vector<int>>& questions){
+        
+        int n = questions.size();
+        
+        vector<long long> dp(n+1, 0);
+        
+        for(int index=n-1; index>=0; index--){
+            
+            long long take = questions[index][0];
+            if(index+questions[index][1]+1 <= n)
+                take = take + dp[index+(questions[index][1])+1];
+        
+            long long notTake = 0 + dp[index+1];
+
+            dp[index] = max(take , notTake);
+        }
+        
+        return dp[0];
+    }
+    
     long long mostPoints(vector<vector<int>>& questions) {
         
         int n = questions.size();
         
         vector<long long> dp(n, -1);
-        return solve(0, questions, n, dp);
+        return tabulation(questions);
     }
 };
