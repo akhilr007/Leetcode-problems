8e9a76d87dbe54bb000ce2eeee9ec3c1c4bfbd2b
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-29 00:46:58 +0530

    Time: 1 ms (72.12%), Space: 44.7 MB (11.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -27,7 +27,59 @@ class Solution {
         }
     }
     
+    void optimal(int[][] matrix){
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        // col = row[0] -> matrix[0][..]
+        // row = col[0] -> matrix[..][0]
+        
+        int col0 = 1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                
+                if(matrix[i][j] == 0){
+                    
+                    // mark the ith row
+                    matrix[i][0] = 0;
+                    
+                    // mark the col
+                    if(j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+        
+        for(int i=1; i<n; i++){
+            
+            for(int j=1; j<m; j++){
+                
+                if(matrix[i][j] != 0){
+                    
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for(int j=0; j<m; j++)
+                matrix[0][j] = 0;
+        }
+        
+        if(col0 == 0){
+            for(int i=0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+    
     public void setZeroes(int[][] matrix) {
-        better(matrix);
+        optimal(matrix);
     }
 }
