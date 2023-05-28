fde06e1e3bef2fc22ab20b4199d5e5dec36f2a41
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-28 14:14:53 +0530

    Time: 60 ms (38.08%), Space: 41.5 MB (91.53%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,12 @@
+Array.prototype.last = function() {
+    const arr = this;
+    if(arr.length === 0)
+        return -1;
+    
+    return arr[arr.length-1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
