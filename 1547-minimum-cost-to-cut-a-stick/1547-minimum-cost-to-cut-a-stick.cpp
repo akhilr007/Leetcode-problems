451e4c2a2efe5387969e7fe53417bd978f34fc8c
Akhil Rajan <rajanakhil07@gmail.com>
2023-05-28 13:42:23 +0530

    Time: 139 ms (11.58%), Space: 10.3 MB (32.53%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,66 +1,32 @@
-class Solution{
+class Solution {
 public:
     int solve(int i, int j, vector<int>& cuts, vector<vector<int>>& dp){
-
-        if(i>j)
+        
+        if(i > j)
             return 0;
-
+        
         if(dp[i][j] != -1)
             return dp[i][j];
-
-        int mini = 1e9;
-
-        // we can make cuts from 1 to size of cuts
+        
+        int result = INT_MAX;
+        
         for(int ind=i; ind<=j; ind++){
             
             int cost = cuts[j+1] - cuts[i-1] + solve(i, ind-1, cuts, dp) + solve(ind+1, j, cuts, dp);
-            mini = min(mini, cost);
+            result = min(result, cost);
         }
-
-        return dp[i][j] = mini;
+        
+        return dp[i][j] = result;
     }
-
-    int tabulation(int n, vector<int>& cuts){
-
-        int size = cuts.size()-2; // skipping 0 and n
-
-        vector<vector<int>> dp(size+2, vector<int> (size+2, 0));
-
-        for(int i=size; i>=1; i--){
-
-            for(int j=1; j<=size; j++){
-
-                if(i > j) continue;
-
-                int mini = 1e9;
-
-                for(int ind=i; ind<=j; ind++){
-
-                    int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
-                    mini = min(mini, cost);
-                }
-
-                dp[i][j] = mini;
-            }
-        }
-
-        return dp[1][size];
-    }
-
-    int minCost(int n, vector<int>& cuts){
-
-        int size = cuts.size();
-
-        // inserting 0 and n to cuts because to make calculation easier to calculate cost of cuts
+    
+    int minCost(int n, vector<int>& cuts) {
+        
         cuts.push_back(n);
         cuts.insert(begin(cuts), 0);
-
-        // sorting because to solve the subproblems independently
-        // [1, 3, 4, 5] if we cut on 3 then [1] and [4, 5] will be easier to solve as 
-        // [1, 3, 4, 5, 2] so, [1] and [4, 5, 2] so cost calculation will be tough
+        
         sort(begin(cuts), end(cuts));
-
-        vector<vector<int>> dp(size+1, vector<int> (size+1, -1));
-        return tabulation(n, cuts);
+        
+        vector<vector<int>> dp(size(cuts), vector<int>(size(cuts), -1));
+        return solve(1, cuts.size()-2, cuts, dp);
     }
 };
