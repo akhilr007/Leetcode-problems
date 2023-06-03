79cb2091c746317fc0bbda825af9766ae349d159
Akhil Rajan <rajanakhil07@gmail.com>
2023-06-04 00:03:34 +0530

    Time: 30 ms (86.25%), Space: 6.7 MB (99.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.go
+++ 0704-binary-search/0704-binary-search.go
@@ -3,18 +3,22 @@ func search(nums []int, target int) int {
     low := 0
     high := len(nums)-1
     
-    for low <= high {
-        
-        mid := (low + high) / 2
-        
-        if target == nums[mid]{
-            return mid
-        }else if target > nums[mid] {
-            low = mid + 1
-        } else{
-            high = mid - 1
-        }
+    return binarySearch(nums, low, high, target)
+}
+
+func binarySearch(nums []int, low int, high int, target int) int {
+    
+    if low > high {
+        return -1
     }
     
-    return -1
+    mid := (low + high) >> 1
+    
+    if target == nums[mid]{
+        return mid
+    } else if target > nums[mid] {
+        return binarySearch(nums, mid + 1, high, target)
+    } else{
+        return binarySearch(nums, low, mid - 1, target)
+    }
 }
