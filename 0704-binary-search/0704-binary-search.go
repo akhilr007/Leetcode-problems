40be96ccdc56086e9b63061629dc99feb998bf68
Akhil Rajan <rajanakhil07@gmail.com>
2023-06-03 23:59:08 +0530

    Time: 35 ms (51.47%), Space: 7.2 MB (21.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.go
+++ 0704-binary-search/0704-binary-search.go
@@ -1,22 +1,20 @@
-class Solution {
-    public int search(int[] nums, int target) {
+func search(nums []int, target int) int {
+    
+    low := 0
+    high := len(nums)-1
+    
+    for low <= high {
         
-        int low = 0;
-        int high = nums.length-1;
+        mid := (low + high) / 2
         
-        while(low <= high){
-            
-            int mid = (low + high) >> 1;
-            
-            if(nums[mid] == target)
-                return mid;
-            
-            if(nums[mid] > target)
-                high = mid - 1;
-            else
-                low = mid + 1;
+        if target == nums[mid]{
+            return mid
+        }else if target > nums[mid] {
+            low = mid + 1
+        } else{
+            high = mid - 1
         }
-        
-        return -1;
     }
+    
+    return -1
 }
