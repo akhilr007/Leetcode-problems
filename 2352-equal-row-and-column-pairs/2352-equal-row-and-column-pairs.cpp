392e09dfc8a2228babd0491bf829df86a6527b9d
Akhil Rajan <rajanakhil07@gmail.com>
2023-06-13 13:17:30 +0530

    Time: 99 ms (85.41%), Space: 29.2 MB (69.49%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -2,49 +2,24 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         
-        int equalPairs = 0;
+        int n = grid.size();
+        int count = 0;
         
-        vector<vector<int>> rows;
-        vector<vector<int>> cols;
+        map<vector<int>, int> mp;
         
         for(auto row: grid)
-            rows.push_back(row);
+            mp[row]++;
         
-        for(int col=0; col<grid.size(); col++){
-            vector<int> temp;
-            for(int row=0; row<grid.size(); row++){
-                
-                temp.push_back(grid[row][col]);
+        for(int c=0; c<n; c++){
+            vector<int> col(n);
+            for(int r=0; r<n; r++){
+                col[r] = grid[r][c];
             }
-            cols.push_back(temp);
-        }
-        
-        for(auto r : rows)
-        {
-            for(auto i: r)
-            {
-                cout << i << " ";
-            }
-            cout << endl;
-        }
-        
-        for(auto r : cols)
-        {
-            for(auto i: r)
-            {
-                cout << i << " ";
-            }
-            cout << endl;
-        }
-        
-        for(int i=0; i<grid.size(); i++){
             
-            for(int j=0; j<grid.size(); j++){
-                
-                if(rows[i] == cols[j])
-                    equalPairs++;
-            }
+            if(mp.find(col) != mp.end())
+                count += mp[col];
         }
-        return equalPairs;
+        
+        return count;
     }
 };
