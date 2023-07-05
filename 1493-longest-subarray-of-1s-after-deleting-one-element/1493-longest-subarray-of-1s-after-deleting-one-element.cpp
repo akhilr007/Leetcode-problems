a7c4e7e945a3c416c54ec2d3a6eeb5faf0b1bb4c
Akhil Rajan <rajanakhil07@gmail.com>
2023-07-05 21:58:42 +0530

    Time: 52 ms (28.13%), Space: 36.5 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,38 +4,24 @@ public:
         
         int n = nums.size();
         
-        vector<int> indexes;
-        for(int i=0; i<n; i++){
-            if(nums[i]==0)
-                indexes.push_back(i);
-        }
-        
-        if(indexes.size()==0)
-            return n-1;
-        
         int result = 0;
-        for(int k=0; k<indexes.size(); k++){
-            
-            int i=indexes[k]-1;
-            int j=indexes[k]+1;
-            
-            while(i>=0){
-                
-                if(nums[i] == 0)
-                    break;
-                i--;
-            }
-            
-            while(j<n){
-                if(nums[j] == 0)
-                    break;
-                j++;
-            }
-            
-            //cout<<j<<"->"<<i<<endl;
-            result = max(result, j-i-2);
-        }
+        int zeroCount = 0;
         
+        int i=0;
+        for(int j=0; j<n; j++){
+            
+            if(nums[j] == 0)
+                zeroCount++;
+            
+            while(zeroCount > 1){
+                if(nums[i]==0)
+                    zeroCount--;
+                
+                i++;
+            }
+            
+            result = max(result, j-i);
+        }
         return result;
     }
 };
