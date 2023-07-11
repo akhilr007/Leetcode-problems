6ba25f5b263fe378d97dd02ad70ac1d264b72baa
Akhil Rajan <rajanakhil07@gmail.com>
2023-07-11 22:48:54 +0530

    Time: 3 ms (93.43%), Space: 13 MB (75.09%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -1,85 +1,94 @@
-class Solution{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
 public:
-    void markParentNode(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &nodeToParent){
-
-        queue <TreeNode*> q;
+    void mapParentNode(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& nodeToParent){
+        
+        queue<TreeNode*> q;
         q.push(root);
-
+        
         while(!q.empty()){
-
-            int size = q.size();
-            for(int i=0; i<size; i++){
-
+            
+            int size=q.size();
+            while(size--){
+                
                 auto node = q.front();
                 q.pop();
-
-                if(node->left){
-                    nodeToParent[node->left] = node;
+                
+                if(node->left)
+                {
                     q.push(node->left);
+                    nodeToParent[node->left] = node;
                 }
-
-                if(node->right){
-                    nodeToParent[node->right] = node;
+                
+                if(node->right)
+                {
                     q.push(node->right);
+                    nodeToParent[node->right] = node;
                 }
             }
         }
     }
     
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k){
-
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
         vector<int> result;
         if(root == NULL)
             return result;
-
-        unordered_map< TreeNode*, TreeNode* > nodeToParent;
-        markParentNode(root, nodeToParent);
-
-        queue < TreeNode* > q;
+        
+        unordered_map<TreeNode*, TreeNode*> nodeToParent;
+        mapParentNode(root, nodeToParent);
+        
+        queue<TreeNode*> q;
         q.push(target);
-
-        unordered_map< TreeNode*, bool > visited;
+        
+        unordered_map<TreeNode*, bool> visited;
         visited[target] = true;
-
-        int level = 0;
-
+        
+        int level=0;
+        
         while(!q.empty()){
-
+            
+            if(level == k)
+                    break;
+            level++;
+                    
             int size = q.size();
-            
-            if(level == k) break;
-            level ++;
-            
-            for(int i=0; i<size; i++){
-
+            while(size--){
+                
                 auto node = q.front();
                 q.pop();
-
-                if(node->left && visited[node->left] == false){
-                    q.push(node->left);
+                
+                if(node->left and visited[node->left]==false){
                     visited[node->left] = true;
+                    q.push(node->left);
                 }
                 
-                if(node->right && visited[node->right] == false){
-                    q.push(node->right);
+                if(node->right and visited[node->right]==false){
                     visited[node->right] = true;
+                    q.push(node->right);
                 }
-
-                if(nodeToParent[node] && visited[nodeToParent[node]] == false){
-                    q.push(nodeToParent[node]);
+                
+                if(nodeToParent[node] and visited[nodeToParent[node]]==false){
                     visited[nodeToParent[node]] = true;
+                    q.push(nodeToParent[node]);
                 }
             }
-
-            
         }
-
-        while(!q.empty())
-        {
+        
+        while(!q.empty()){
             result.push_back(q.front()->val);
             q.pop();
         }
-
+        
         return result;
     }
 };
