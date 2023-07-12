37e8652b630ebc274ea6583323e07ad63c5329b8
Akhil Rajan <rajanakhil07@gmail.com>
2023-07-12 23:07:32 +0530

    Time: 221 ms (46.17%), Space: 59.3 MB (32.96%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -16,7 +16,7 @@ public:
         return false;
     }
     
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    vector<int> dfs(vector<vector<int>>& graph){
         
         int n = graph.size();
         
@@ -36,5 +36,53 @@ public:
         }
         
         return safeNodes;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        
+        vector<int> reverseGraph[n]; 
+        vector<int> indegree(n);
+        // agar terminal node se koi edge hi nai jaa raha to kahn algo se kuch nai hoga
+        // iske liye hum graph ko reverse kar sakte hai
+        // safe se terminal = terminal se safe(reverse edge)
+        for(int i=0; i<n; i++){
+            for(int v: graph[i])
+            {
+                reverseGraph[v].push_back(i);
+                indegree[i]++;
+            }
+        }
+        
+        queue<int> q;
+        // take all indegrees with 0
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        vector<int> safe(n);
+        while(!q.empty()){
+            
+            int u = q.front();
+            q.pop();
+            safe[u]=1;
+            
+            for(int v : reverseGraph[u]){
+                indegree[v]--;
+                
+                if(indegree[v]==0)
+                    q.push(v);
+            
+            }
+        }
+        
+        vector<int> result;
+        for(int i=0; i<n; i++)
+            if(safe[i] == 1)
+                result.push_back(i);
+        
+        return result;
     }
 };
