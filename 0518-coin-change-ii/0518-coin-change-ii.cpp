0dce73a55b63e09f68b052296b98bc73372d82e5
Akhil Rajan <rajanakhil07@gmail.com>
2023-08-11 21:22:30 +0530

    Time: 41 ms (40.92%), Space: 29.6 MB (6.34%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -19,12 +19,40 @@ public:
         return dp[index][amount] = taken + notTaken;
     }
     
+    int tabulation(int amount, vector<int>& coins){
+        
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        
+        for(int a=0; a<=amount; a++)
+        {
+            if(a % coins[0] == 0)
+                dp[0][a] = 1;
+        }
+        
+        for(int index=1; index<n; index++){
+            for(int a=0; a<=amount; a++){
+                
+                int notTaken = dp[index-1][a];
+        
+                int taken=0;
+                if(coins[index] <= a){
+                    taken = dp[index][a-coins[index]];
+                }
+
+                dp[index][a] = taken + notTaken;
+            }
+        }
+        
+        return dp[n-1][amount];
+    }
+    
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
         
         vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        return solve(n-1, amount, coins, dp);
+        return tabulation(amount, coins);
         
     }
 };
